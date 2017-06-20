a03bf96046b6867d2cc56ae9a1c9fcb64367fb39
daniel martins <danielmartins7000@hotmail.com>
2017-06-20 15:51:59 -0300

    created Support class functions

--- src/AppBundle/Util/KitGenerator/Support.php
+++ src/AppBundle/Util/KitGenerator/Support.php
@@ -6,18 +6,40 @@ namespace AppBundle\Util\KitGenerator;
 
 class Support
 {
-public static function factorial($number){
-    $factorial = $number;
+    public static function factorial($number)
+    {
+        $factorial = $number;
 
-    if($number >= 1) {
-    for($i= $number-1; $i != 0; $i--){
-        $factorial *= $i;
-    }
-    }
-    else{
-        return 1;
+        if ($number >= 1) {
+            for ($i = $number - 1; $i != 0; $i--) {
+                $factorial *= $i;
+            }
+        } else {
+            return 1;
+        }
+
+        return $factorial;
     }
 
-    return $factorial;
-}
+    public static function sign($number)
+    {
+        if ($number < 0) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+
+    public static function combine($a, $b)
+    {
+        $num = $a + $b - 1; //2
+        $cont = $b; //2
+        for ($i = $num - 1; $cont > 1; $i--) {   //1
+            $num *= $i;
+            $cont -= 1;
+        }
+        return $num / self::factorial($b);
+    }
+
+
 }
--- tests/AppBundle/Util/KitGenerator/SupportTest.php
+++ tests/AppBundle/Util/KitGenerator/SupportTest.php
@@ -9,14 +9,17 @@ use AppBundle\Util\KitGenerator\Support;
  * Class SupportTest
  * @group kit_generator
  */
-class SupportTest extends WebTestCase{
+class SupportTest extends WebTestCase
+{
 
-   public function testFactorial(){
-       $this->assertEquals(120, Support::factorial(5));
-       $this->assertEquals(1, Support::factorial(0));
-       $this->assertEquals(1, Support::factorial(-5));
-   }
-
+    public function testFactorial()
+    {
+        $this->assertEquals(120, Support::factorial(5));
+        $this->assertEquals(1, Support::factorial(0));
+        $this->assertEquals(1, Support::factorial(-5));
+        $this->assertEquals(-1, Support::sign(-10)); //primeiro o numero que deve ser o resultado, depois o numero plo qual percorre o algorithimo
+        $this->assertEquals(1, Support::combine(1,2));
+    }
 
 
 }
