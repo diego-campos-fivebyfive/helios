13a8142abf1cd07717c2c449684d49e605f755e4
Claudinei M <cjchamado@gmail.com>
2017-06-20 16:22:35 -0300

    Add StructureCalculatorInterface

--- src/AppBundle/Util/KitGenerator/StructureCalculatorInterface.php
+++ src/AppBundle/Util/KitGenerator/StructureCalculatorInterface.php
@@ -1,15 +1,45 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudinei
- * Date: 20/06/17
- * Time: 16:05
- */
 
 namespace AppBundle\Util\KitGenerator;
 
-
 interface StructureCalculatorInterface
 {
+    /**
+     * Roof Types
+     */
+    //const ROOF_ROMAN            = 0;
+    //const ROOF_AMERICAN         = 0;
+    const ROOF_ROMAN_AMERICAN   = 0;
+    const ROOF_CEMENT           = 1;
+    const ROOF_FLAT_SLAB        = 2;
+    const ROOF_SHEET_METAL      = 3;
+    const ROOF_SHEET_METAL_PFM  = 4;
 
+    /**
+     * Positions
+     */
+    const POSITION_HORIZONTAL   = 'horizontal';
+    const POSITION_VERTICAL     = 'vertical';
+
+    /**
+     * @param $position
+     * @return StructureCalculatorInterface
+     */
+    public function setPosition($position);
+
+    /**
+     * @return string
+     */
+    public function getPosition();
+
+    /**
+     * @param $cellNumber
+     * @return StructureCalculatorInterface
+     */
+    public function setCellNumber($cellNumber);
+
+    /**
+     * @return int
+     */
+    public function getCellNumber();
 }
