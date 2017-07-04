9c334d0582050988be703bb3ec130c42c0e9c4cd
Rafael Kendrik <rafamikovski@gmail.com>
2017-07-04 11:33:33 -0300

    initing ces new task cli

--- cli/ces-new-task.sh
+++ cli/ces-new-task.sh
@@ -1,50 +1,17 @@
 #!/usr/bin/env bash
 
-CREDENTIALS_FILE=~/.cpf-credentials
-
-function keepCredentials {
-    printf "\n" > /dev/tty
-    read -p "Do you wanna this script to keep your credentials? in $CREDENTIALS_FILE (y/n):" keepCredentialsFlag > /dev/tty
-    if [[ $keepCredentialsFlag == "y" ]]; then
-        user=$1
-        passwd=$2
-        rm -rf  $CREDENTIALS_FILE
-        touch $CREDENTIALS_FILE
-        echo $user >> $CREDENTIALS_FILE
-        echo $passwd >> $CREDENTIALS_FILE
-    fi
-}
-
-
-function hasCredentials {
-    test -e $CREDENTIALS_FILE
-
-    [[ $? = 0 ]] && {
-        echo 1
-    }
-}
-
-function restoreLoginCredential {
-    head -n1 ~/.cpf-pr-credentials | tr -d "[:space:]"
-}
-
-function restorePasswdCredential {
-    tail -n1 ~/.cpf-pr-credentials | tr -d "[:space:]"
-}
-
 cd $CLIPP_PATH
 
 if [[ -z $1 ]] || [[ $1 == '--help' ]] || [[ $1 == '-h' ]]; then
-    printf "Creates a new task in you computer, move it on github
-    and notify everybody you are working on it.
+    printf "Iniciando uma nova tarefa.
 
     Usage:
 
-    cpf-new-task isseeId
+    cpf-new-task issue-[NUMBER]
 
     Ex:
 
-    cpf-new-task 3084\n"
+    cpf-new-task issue-3084\n"
     exit 0;
 fi
 
@@ -70,31 +37,11 @@ if [[ -n $result ]]; then
     exit 0;
 fi
 
-
-echo "Entering master to create branch from it..."
+echo "Acessando a Master..."
 git checkout master
+echo "Realizando o Pull..."
 git fetch origin
 git pull origin master
-git checkout -b $issue_name
-
-
-if [[ $(hasCredentials)  -eq 1 ]]; then
-    repositoryUser=$(restoreLoginCredential)
-    repositoryUserPassword=$(restorePasswdCredential)
-    echo "Your credentials are stored on $CREDENTIALS_FILE"
-else
-    read -p "Type your Github user :" repositoryUser
-    printf "\n"
-    read -p "Type your Github password:" -s repositoryUserPassword
-
-    $(keepCredentials $repositoryUser $repositoryUserPassword)
-fi
-
-curl -u $repositoryUser:$repositoryUserPassword https://api.github.com/repos/compufour/compufacil/issues/$issue_name/labels -d '["Stage: In Progress"]'
-
-cpf-notify-slack "$USER created local branch $issue_name"
-cpf-notify-user "A new branch $issue_name was created in your workspace"
-
-cpf-metric "compufacil.new-task" 1
-
-
+echo "Criando nova Branch..."
+git checkout -b issue-$issue_name
+echo "Done"
