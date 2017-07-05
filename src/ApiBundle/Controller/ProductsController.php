5f8815c17822300b60250a346cb7dbc5362f3f3a
Claudinei M <cjchamado@gmail.com>
2017-07-05 11:20:29 -0300

    Fix: Ajustado o nome

--- src/ApiBundle/Controller/ProductsController.php
+++ src/ApiBundle/Controller/ProductsController.php
@@ -8,47 +8,10 @@ use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
-class ProdutcsController extends FOSRestController
+class ProductsController extends FOSRestController
 {
 	public function getProductsAction(Request $request)
 	{
-		$fields = '';
-
-		$em = $this->getDoctrine()->getManager();///->getRepository(Customer::class);
-
-		$qb = $em->createQueryBuilder();
-
-		$qb->select($fields)->from(Costumer::class, 'p');
-		$qb->where('p.context = :context');
-		$qb->setParameters([
-            'context' => Customer::CONTEXT_MODULE
-        ]);
-
-        $query = $qb->getQuery();
-
-        $paginator = $this->get('knp_paginator');
-
-        $pagination = $paginator->paginate(
-            $query,
-            $request->query->getInt('page', 1),
-            $request->query->getInt('per_page', 5)
-        );
-
-        $products =  ['products' => $pagination->getItems(), 'next' => 'próxima', 'prev' => 'anterior'];
-
-        $view = View::create($products);
-
-        return $this->handleView($view);
-
-         /** @var \JMS\Serializer\Serializer $serializer */
-        $serializer = $this->get('serializer');
-
-        //dump($serializer->serialize($accounts,'json')); die;
-        return new JsonResponse($products);
-
-
-        return $this->handleView($this->view($products));
-
+	    // Waiting...
 	}
-
 }
