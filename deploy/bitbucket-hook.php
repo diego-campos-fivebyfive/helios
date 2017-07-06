212c01875eed855d17e7413cc52f5bd6bef36cca
Rafael Kendrik <rafamikovski@gmail.com>
2017-07-06 17:27:22 -0300

    doc yarn adjust and start server after frontend compile

---
+++ deploy/bitbucket-hook.php
@@ -0,0 +1,55 @@
+<?php
+
+function getPaths ()
+{
+  return (object)[
+    "git" => "/var/www",
+    "root" => "/var/www",
+  ];
+}
+
+function createLog ($commit, $branch)
+{
+  $date = date('m/d/Y h:i:s a');
+  $log = "Date: $date | Commit: $commit | Branch: $branch \n";
+  file_put_contents('deploy.log', $log , FILE_APPEND);
+}
+
+function updateHomolog ($branch = 'No Branch')
+{
+  $path = getPaths();
+
+  exec("cd $path->git && git fetch");
+  exec("cd $path->git && GIT_WORK_TREE=$path->root git checkout -f");
+
+  $commit = shell_exec("cd $path->git && git rev-parse --short HEAD");
+  createLog($commit, $branch);
+}
+
+function updateCases ($commits)
+{
+  exec("echo $commits");
+  return;
+
+  if (empty($commits)) {
+    updateHomolog();
+    return;
+  }
+
+  foreach ($commits as $commit) {
+
+    $branch = $commit->branch;
+    $branches = $commit->branches;
+
+    if ($branch || isset($branches)) {
+      updateHomolog($branch);
+      break;
+    }
+  }
+}
+
+$payload = $_POST['payload'];
+if (isset($payload)) {
+  $commits = json_decode($payload)->commits;
+  updateCases($commits);
+}
