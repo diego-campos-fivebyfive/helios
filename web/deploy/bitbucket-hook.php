e756f0bd258f5101772283566d71ebeb5b932b51
Rafael Kendrik <rafamikovski@gmail.com>
2017-07-10 13:29:27 -0300

    hooks final adjsts

--- web/deploy/bitbucket-hook.php
+++ web/deploy/bitbucket-hook.php
@@ -3,5 +3,5 @@
 exec('cd $SICES_PATH && ces-update-remote');
 
 $date = date('m/d/Y h:i:s a');
-$log = "Date: $date | Commit: 'MERGED' \n";
+$log = "Date: $date | Commit: MERGED \n";
 file_put_contents(__DIR__.'/deploy.log', $log , FILE_APPEND);
