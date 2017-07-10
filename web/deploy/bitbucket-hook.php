b3e779fb630b06d573b440db5c5f1b68c022df10
Rafael Kendrik <rafamikovski@gmail.com>
2017-07-10 11:27:03 -0300

    ces-update-remote

--- web/deploy/bitbucket-hook.php
+++ web/deploy/bitbucket-hook.php
@@ -1,14 +1,7 @@
 <?php
 
-$path = (object)[
-  "git" => "git@bitbucket.org:cjchamado/sices.git",
-  "root" => "/var/www",
-];
-
-exec("cd $path->git && git fetch");
-exec("cd $path->git && GIT_WORK_TREE=$path->root git checkout -f");
-$commit = shell_exec("cd $path->git && git rev-parse --short HEAD");
+exec("cd $SICES_PATH && ces-update-remote");
 
 $date = date('m/d/Y h:i:s a');
-$log = "Date: $date | Commit: $commit \n";
+$log = "Date: $date | Commit: 'MERGED' \n";
 file_put_contents('deploy.log', $log , FILE_APPEND);
