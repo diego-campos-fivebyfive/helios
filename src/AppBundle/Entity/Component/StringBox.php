251c900973239e6266c18d2cd178e170acc2780a
Claudinei M <cjchamado@gmail.com>
2017-07-17 15:44:55 -0300

    add string_box entity: form + manager service + unit tests

---
+++ src/AppBundle/Entity/Component/StringBox.php
@@ -0,0 +1,193 @@
+<?php
+
+/*
+ * This file is part of the SicesSolar package.
+ *
+ * (c) SicesSolar <http://sicesbrasil.com.br/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Entity\Component;
+
+use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Model as ORMBehaviors;
+
+/**
+ * StringBox
+ *
+ * @ORM\Table(name="app_component_string_box")
+ * @ORM\Entity
+ */
+class StringBox implements StringBoxInterface
+{
+    use ORMBehaviors\Timestampable\Timestampable;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="code", type="string", length=255)
+     */
+    private $code;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="inputs", type="smallint")
+     */
+    private $inputs;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="outputs", type="smallint")
+     */
+    private $outputs;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="fuses", type="smallint")
+     */
+    private $fuses;
+
+    /**
+     * @var MakerInterface
+     *
+     * @ORM\ManyToOne(targetEntity="Maker")
+     */
+    private $maker;
+
+    /**
+     * @inheritDoc
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setInputs($inputs)
+    {
+        $this->inputs = $inputs;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getInputs()
+    {
+        return $this->inputs;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setOutputs($outputs)
+    {
+        $this->outputs = $outputs;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getOutputs()
+    {
+        return $this->outputs;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setFuses($fuses)
+    {
+        $this->fuses = $fuses;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getFuses()
+    {
+        return $this->fuses;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setMaker(MakerInterface $maker)
+    {
+        $this->maker = $maker;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getMaker()
+    {
+        return $this->maker;
+    }
+}
---
+++ src/AppBundle/Entity/Component/StringBoxInterface.php
@@ -0,0 +1,101 @@
+<?php
+
+/*
+ * This file is part of the SicesSolar package.
+ *
+ * (c) SicesSolar <http://sicesbrasil.com.br/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Entity\Component;
+
+/**
+ * Interface StringBoxInterface
+ *
+ * @author Claudinei Machado <claudinei@kolinalabs.com>
+ */
+interface StringBoxInterface
+{
+    /**
+     * @return int
+     */
+    public function getId();
+
+    /**
+     * @param $code
+     * @return StringBoxInterface
+     */
+    public function setCode($code);
+
+    /**
+     * @return string
+     */
+    public function getCode();
+
+    /**
+     * @param $description
+     * @return StringBoxInterface
+     */
+    public function setDescription($description);
+
+    /**
+     * @return string
+     */
+    public function getDescription();
+
+    /**
+     * @param $inputs
+     * @return StringBoxInterface
+     */
+    public function setInputs($inputs);
+
+    /**
+     * @return int
+     */
+    public function getInputs();
+
+    /**
+     * @param $outputs
+     * @return StringBoxInterface
+     */
+    public function setOutputs($outputs);
+
+    /**
+     * @return int
+     */
+    public function getOutputs();
+
+    /**
+     * @param $fuses
+     * @return StringBoxInterface
+     */
+    public function setFuses($fuses);
+
+    /**
+     * @return int
+     */
+    public function getFuses();
+
+    /**
+     * @param MakerInterface $maker
+     * @return StringBoxInterface
+     */
+    public function setMaker(MakerInterface $maker);
+
+    /**
+     * @return MakerInterface
+     */
+    public function getMaker();
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt();
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt();
+}
