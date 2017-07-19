964130db136dae2b6af9f230f2ccb08cca4d3d86
Claudinei M <cjchamado@gmail.com>
2017-07-19 14:57:11 -0300

    create part interface+trait for context: company

--- src/AppBundle/Entity/CompanyInterface.php
+++ src/AppBundle/Entity/CompanyInterface.php
@@ -1,15 +1,43 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudinei
- * Date: 19/07/17
- * Time: 14:42
- */
 
 namespace AppBundle\Entity;
 
-
-class CompanyInterface
+interface CompanyInterface
 {
+    /**
+     * Accepted only $this->isCompany and $employee->isPerson()
+     *
+     * @param BusinessInterface $employee
+     * @return BusinessInterface
+     */
+    public function addEmployee(BusinessInterface $employee);
 
+    /**
+     * @param BusinessInterface $employee
+     * @return BusinessInterface
+     */
+    public function removeEmployee(BusinessInterface $employee);
+
+    /**
+     * @return \Doctrine\Common\Collections\ArrayCollection
+     */
+    public function getEmployees();
+
+    /**
+     * @see \AppBundle\Form\ContactType $builder->add('relatedEmployees')
+     * @param \Doctrine\Common\Collections\ArrayCollection $relatedEmployees
+     * @return BusinessInterface
+     */
+    public function setRelatedEmployees(array $relatedEmployees = []);
+
+    /**
+     * @see \AppBundle\Form\ContactType $builder->add('relatedEmployees')
+     * @return \Doctrine\Common\Collections\ArrayCollection
+     */
+    public function getRelatedEmployees();
+
+    /**
+     * @return string
+     */
+    public function getContext();
 }
--- src/AppBundle/Entity/CompanyTrait.php
+++ src/AppBundle/Entity/CompanyTrait.php
@@ -1,15 +1,75 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudinei
- * Date: 19/07/17
- * Time: 14:45
- */
 
 namespace AppBundle\Entity;
 
-
 trait CompanyTrait
 {
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     *
+     * @ORM\OneToMany(targetEntity="Customer", mappedBy="company", cascade={"persist"})
+     */
+    protected $employees;
 
+    /**
+     * @var \Doctrine\Common\Collections\ArrayCollection
+     */
+    protected $relatedEmployees;
+
+    /**
+     * @inheritDoc
+     */
+    public function addEmployee(BusinessInterface $employee)
+    {
+        if (!$this->isCompany() || !$employee->isPerson())
+            $this->unsupportedContextException();
+
+        if (!$this->employees->contains($employee)) {
+            $this->employees->add($employee);
+            $employee->setCompany($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function removeEmployee(BusinessInterface $employee)
+    {
+        if ($this->employees->contains($employee)) {
+            $this->employees->removeElement($employee);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getEmployees()
+    {
+        return $this->employees;
+    }
+
+    public function addRelatedEmployee(BusinessInterface $employee){
+        //dump($employee); die;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setRelatedEmployees(array $relatedEmployees = [])
+    {
+        /*foreach($this->getMember()->getAllowedPersons() as $person){
+            if(in_array($person->getId(), array_values($relatedEmployees))){
+                $person->setCompany($this);
+            }
+        }*/
+    }
+
+    public function getRelatedEmployees()
+    {
+        //return $this->relatedEmployees;
+    }
 }
