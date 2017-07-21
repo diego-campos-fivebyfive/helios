7a297c107afc3da25fa63bf3109dbb41548e9b6f
Claudinei M <cjchamado@gmail.com>
2017-07-21 09:40:19 -0300

    update module manager unit test

--- tests/AppBundle/Entity/ModuleManagerTest.php
+++ tests/AppBundle/Entity/ModuleManagerTest.php
@@ -2,47 +2,18 @@
 
 namespace Tests\AppBundle\Entity;
 
-use AppBundle\Entity\Component\Module;
-use Liip\FunctionalTestBundle\Test\WebTestCase;
-use Tests\AppBundle\Helpers\ObjectHelperTest;
+use Tests\AppBundle\AppTestCase;
 
 /**
  * Class ModuleManagerTest
- * @group module_manager
+ * @group module
  */
-class ModuleManagerTest extends WebTestCase
+class ModuleManagerTest extends AppTestCase
 {
-    use ObjectHelperTest;
-
     public function testDefault()
     {
-        $data = [
-            'model' => self::randomString(4),
-            'code' => self::randomString(8),
-            'cellNumber' => self::randomInt(),
-            'maxPower' => self::randomFloat(),
-            'voltageMaxPower' => self::randomFloat(),
-            'currentMaxPower' => self::randomFloat(),
-            'openCircuitVoltage' => self::randomFloat(),
-            'shortCircuitCurrent' => self::randomFloat(),
-            'efficiency' => self::randomFloat(),
-            'temperatureOperation' => self::randomFloat(),
-            'tempCoefficientMaxPower' => self::randomFloat(),
-            'tempCoefficientVoc' => self::randomFloat(),
-            'tempCoefficientIsc' => self::randomFloat(),
-            'length' => self::randomFloat(),
-            'width' => self::randomFloat(),
-            'cellType' => self::randomString(10),
-            'dataSheet' => self::randomString(15),
-            'image' => self::randomString(10),
-        ];
+        $module = $this->getFixture('module');
 
-        $module = new Module();
-        $this->fluentSettersTest($module, $data);
-
-        $this->getContainer()->get('module_manager')->save($module);
-
-        $this->assertNotNull($module->getToken());
         $this->assertNotNull($module->getCreatedAt());
     }
 }
