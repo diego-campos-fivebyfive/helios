a70d5749e81e061e45f5803ed8f2ae3515483c57
Rafael Kendrik <rafamikovski@gmail.com>
2017-07-21 15:11:29 -0300

    sendRequest renamed to getRequest

--- bundle/app/models/isquik.js
+++ bundle/app/models/isquik.js
@@ -4,16 +4,16 @@ const request = require('request-promise')
 const { app, config } = require('../config')
 const { isquik } = config
 
-const sendRequest = (uri) => Google.getAuthentication().then((auth) => (
+const getRequest = (uri) => Google.getAuthentication().then((auth) => (
   request({
     method: 'GET', uri, qs: { auth: auth.idToken }
   })
   .then((x) => JSON.parse(x))
 ))
 
-const getAccount = (id) => sendRequest(`${isquik.uri}/user/${id}`)
-const getMemorial = (id) => sendRequest(`${isquik.uri}/memorial/${id}`)
-const getProduct = (code) => sendRequest(`${isquik.uri}/product/${code}`)
+const getAccount = (id) => getRequest(`${isquik.uri}/user/${id}`)
+const getMemorial = (id) => getRequest(`${isquik.uri}/memorial/${id}`)
+const getProduct = (code) => getRequest(`${isquik.uri}/product/${code}`)
 
 module.exports = {
   getMemorial,
