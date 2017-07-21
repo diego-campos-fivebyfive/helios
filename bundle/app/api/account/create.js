ec30b97ff6495e01a51c96a1d0c7c90827eaea96
Rafael Kendrik <rafamikovski@gmail.com>
2017-07-21 13:51:34 -0300

    bundle app account_create action send sices user

--- bundle/app/api/account/create.js
+++ bundle/app/api/account/create.js
@@ -2,10 +2,23 @@
 const Sices = require('../../models/sices')
 const Isquik = require('../../models/isquik')
 
-const sendAccount = (account) => Sices.sendAccount({
-  status: account.status,
+const sendUser = (account) => Sices.sendUser({
+  name: account.name,
   owner: account.owner,
-  name: account.name
+  status: account.status
+})
+.then(status => {
+  if (status == 304) return 'Can not create User'
+})
+
+const sendAccount = (account) => Sices.sendAccount({
+  name: account.name,
+  owner: account.owner,
+  status: account.status
+})
+.then(status => {
+  if (status == 304) return 'Can not create Account'
+  sendUser(account)
 })
 
 const create = ({ object }) => Isquik.getAccount(object.id).then(sendAccount)
