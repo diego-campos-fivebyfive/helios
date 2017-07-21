acfbbe5a65e171fa5017b044b79ad05d2ecfee28
Rafael Kendrik <rafamikovski@gmail.com>
2017-07-21 11:41:30 -0300

    bundle app initing action account_approved

--- bundle/app/api/account/update.js
+++ bundle/app/api/account/update.js
@@ -9,16 +9,14 @@ const splitAccount = (object) => ({
   sices_user: object.sices_user
 })
 
-const send = ({ object }) => {
-  let isquikAccount = Isquik.getAccount(object.id)
-  isquikAccount = splitAccount(isquikAccount)
+const send = ({ id, object }) => new Promise((resolve, reject) => {
+  Isquik.getAccount(object.id).then((isquikAccount) => {
+    Sices.getAccount(isquikAccount.owner).then((sicesAccount) => {
+      Sices.getUser(sicesAccount.owner).then((sicesUser) => {
 
-  const sicesAccount = Sices.getAccount(isquikAccount.sices_account)
-  const sicesUsers = sicesAccount.users.map((userID) => Sices.getUser(userID))
-
-  return account.then((data) => new Promise((resolve) => {
-    resolve(200)
-  }))
+      })
+    })
+  })
 }
 
 module.exports = {
