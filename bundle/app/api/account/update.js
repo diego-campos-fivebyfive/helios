91922f5e45729abd56d94c67a0bd42d449f99e05
Rafael Kendrik <rafamikovski@gmail.com>
2017-07-21 13:53:35 -0300

    initing bundle app action account_approved

--- bundle/app/api/account/update.js
+++ bundle/app/api/account/update.js
@@ -2,23 +2,27 @@
 const Sices = require('../../models/sices')
 const Isquik = require('../../models/isquik')
 
-const splitAccount = (object) => ({
-  name: object.name,
-  status: object.status,
-  sices_account: object.sices_account,
-  sices_user: object.sices_user
+const sendUser = (account) => Sices.sendUser({
+  name: account.name,
+  owner: account.owner,
+  status: account.status
+})
+.then(status => {
+  if (status == 304) return 'Can not create User'
 })
 
-const send = ({ id, object }) => new Promise((resolve, reject) => {
-  Isquik.getAccount(object.id).then((isquikAccount) => {
-    Sices.getAccount(isquikAccount.owner).then((sicesAccount) => {
-      Sices.getUser(sicesAccount.owner).then((sicesUser) => {
+const sendAccount = (account) => Sices.sendAccount({
+  name: account.name,
+  owner: account.owner,
+  status: account.status
+})
+.then(status => {
+  if (status == 304) return 'Can not create Account'
+  sendUser(account)
+})
 
-      })
-    })
-  })
-}
+const update = ({ object }) => Isquik.getAccount(object.id).then(sendAccount)
 
 module.exports = {
-  send
+  update
 }
