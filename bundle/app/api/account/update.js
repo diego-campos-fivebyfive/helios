341c417f104e36bd43698108949fc8db0d5092db
Rafael Kendrik <rafamikovski@gmail.com>
2017-07-24 11:08:58 -0300

    api account update action adjusts

--- bundle/app/api/account/update.js
+++ bundle/app/api/account/update.js
@@ -8,7 +8,9 @@ const sendUser = account => Sices.sendUser({
   owner: account.owner,
   status: account.status
 })
-  .then(status => ((status !== 304) ? 'Can not create User' : 'User and Account created successfully'))
+  .then(status => (
+    (status !== 304) ? 'Can not update User' : 'User and Account updated successfully'
+  ))
 
 const sendAccount = account => Sices.sendAccount({
   name: account.name,
@@ -16,15 +18,19 @@ const sendAccount = account => Sices.sendAccount({
   status: account.status
 })
   .then(status => {
-    if (status !== 304) return 'Can not create Account'
+    if (status !== 304) return 'Can not update Account'
     sendUser(account)
   })
 
-const create = ({ object }) => Isquik.getAccount(object.id).then(isquikAccount => {
-  const sicesUser = Sices.getUser(isquikAccount.owner)
-  const sicesAccount = Sices.getAccount(sicesUser.owner)
+
+const update = ({ object }) => Isquik.getAccount(object.id).then(isquikAccount => {
+  Sices.getUser(isquikAccount.owner).then(sicesUser => {
+    Sices.getAccount(sicesUser.account).then(sicesAccount => {
+      console.log(sicesUser, sicesAccount)
+    })
+  })
 })
 
 module.exports = {
-  create
+  update
 }
