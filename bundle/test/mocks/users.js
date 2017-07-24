4a71198da784b2c9423dc8a98cd3c84502916690
Rafael Kendrik <rafamikovski@gmail.com>
2017-07-24 11:30:24 -0300

    removing useless data

--- bundle/test/mocks/users.js
+++ bundle/test/mocks/users.js
@@ -16,31 +16,6 @@ const users = [
     postcode: '80254369',
     owner: 36,
     status: 1
-  },
-    {
-    id: 2,
-    name: 'Name DEF',
-    status: 0,
-    owner: 101,
-    dangerousField: 'Mal'
-  },
-  {
-    id: 3,
-    name: 'Name HIJ',
-    status: 0,
-    owner: 102
-  },
-  {
-    id: 4,
-    name: 'Name KLM',
-    status: 0,
-    owner: 103
-  },
-  {
-    id: 5,
-    name: 'Name NOP',
-    status: 1,
-    owner: 104
   }
 ]
 
