581986610c9f4accdda69eab051a0cd66e9e8e47
Rafael Kendrik <rafamikovski@gmail.com>
2017-07-28 14:47:13 -0300

    update request function

--- bundle/app/models/sices.js
+++ bundle/app/models/sices.js
@@ -5,7 +5,12 @@ const { config } = require('../config')
 
 const { sices } = config
 
-const getRequest = uri => request({ method: 'GET', uri }).then(x => JSON.parse(x))
+const getRequest = uri => request({
+  method: 'GET',
+  uri
+})
+  .then(x => JSON.parse(x))
+
 const postRequest = (uri, data) => request({
   headers: { 'Content-Type': 'application/json' },
   method: 'POST',
@@ -13,23 +18,31 @@ const postRequest = (uri, data) => request({
   uri
 })
 
+const putRequest = (uri, data) => request({
+  headers: { 'Content-Type': 'application/json' },
+  method: 'PUT',
+  json: data,
+  uri
+})
+
+const updateAccount = (id, account) => putRequest(`${sices.uri}/accounts/${id}`, account)
+const updateUser = (id, user) => putRequest(`${sices.uri}/users/${id}`, user)
 const sendInveter = inverter => postRequest(`${sices.uri}/inverters`, inverter)
 const sendStructure = structure => postRequest(`${sices.uri}/structures`, structure)
 const sendModule = module => postRequest(`${sices.uri}/modules`, module)
 const sendMemorial = memorial => postRequest(`${sices.uri}/memorials`, memorial)
 const sendAccount = account => postRequest(`${sices.uri}/accounts`, account)
 const sendUser = user => postRequest(`${sices.uri}/users`, user)
-
-const getAccount = id => getRequest(`${sices.uri}/accounts/${id}`)
 const getUser = id => getRequest(`${sices.uri}/users/${id}`)
 
 module.exports = {
+  updateAccount,
+  updateUser,
   sendInveter,
   sendStructure,
   sendModule,
   sendMemorial,
   sendAccount,
   sendUser,
-  getAccount,
   getUser
 }
