49e21aeffe8828d68f08eefeb0a0748bbbde9a68
Fabio Dukievicz <fabiojd47@gmail.com>
2017-08-01 18:54:27 -0300

    Ajustes de codigo

--- src/AppBundle/Controller/Component/StringBoxController.php
+++ src/AppBundle/Controller/Component/StringBoxController.php
@@ -42,7 +42,7 @@ class StringBoxController extends AbstractController
             $request->query->getInt('page', 1),
             'grid' == $request->query->get('display', 'grid') ? 8 : 20
         );
-        //dump($pagination);die;
+
         return $this->render('Stringbox.index', array(
             //'stringBoxes' => $stringBoxes,
             'pagination' => $pagination,
