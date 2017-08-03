518baf462049cc0cf54d08ef7971a3b6631d23fc
Rafael Kendrik <rafamikovski@gmail.com>
2017-08-03 14:58:10 -0300

    using real data

--- bundle/app/api/memorial/create.js
+++ bundle/app/api/memorial/create.js
@@ -3,12 +3,25 @@
 const Sices = require('../../models/sices')
 const Isquik = require('../../models/isquik')
 
-const sendMemorial = memorial => Sices.sendMemorial({
-  version: memorial.version,
-  status: memorial.status,
-  start_at: memorial.start_at,
-  end_at: memorial.end_at,
-  range: memorial.products
+const sendMemorial = ({ Dados }) => Sices.sendMemorial({
+  version: Dados.Versao,
+  status: Dados.Status,
+  start_at: new Date(),
+  end_at: new Date(),
+  range: Dados.Produtos.map(x => ({
+    code: x.Codigo,
+    description: '',
+    family: '',
+    markups: x.Faixas.map(y => ({
+      code: y.Id,
+      initial: y.De,
+      final: y.Ate,
+      markup: y.Niveis.map(z => ({
+        price: z.PrecoVenda,
+        description: z.Descricao
+      }))
+    }))
+  }))
 })
 
 const create = ({ object }) => Isquik.getMemorial(object.id).then(sendMemorial)
