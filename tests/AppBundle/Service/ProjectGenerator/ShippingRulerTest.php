299e9080ccfa2fedfc2d646ddc05c2c5eee9eb27
Claudinei M <cjchamado@gmail.com>
2017-08-08 10:42:37 -0300

    add unit tests

--- tests/AppBundle/Service/ProjectGenerator/ShippingRulerTest.php
+++ tests/AppBundle/Service/ProjectGenerator/ShippingRulerTest.php
@@ -1,15 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudinei
- * Date: 07/08/17
- * Time: 17:59
- */
 
 namespace Tests\AppBundle\Service\ProjectGenerator;
 
+use AppBundle\Service\ProjectGenerator\ShippingRuler;
+use Liip\FunctionalTestBundle\Test\WebTestCase;
 
-class ShippingRulerTest
+/**
+ * @group shipping_ruler
+ */
+class ShippingRulerTest extends WebTestCase
 {
+    /**
+     * Test default behaviors with multiple definitions
+     */
+    public function testDefaultRulerHandling()
+    {
+        $rule = [
+            'price' => 50000,
+            'region' => ShippingRuler::REGION_MIDWEST,
+            'kind' => 'interior',
+            'power' => 50
+        ];
 
+        ShippingRuler::apply($rule);
+
+        $this->assertEquals('mlt', $rule['company']);
+        $this->assertEquals(.031, $rule['percent']);
+        $this->assertEquals(.1, $rule['markup']);
+        $this->assertEquals(1705, $rule['shipping']);
+    }
 }
