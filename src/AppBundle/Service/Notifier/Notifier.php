08591ccda0bd8d291d8809a66b60fd870166e826
Rafael Kendrik <rafamikovski@gmail.com>
2017-08-09 17:36:53 -0300

    dinamic entrypoint

--- src/AppBundle/Service/Notifier/Notifier.php
+++ src/AppBundle/Service/Notifier/Notifier.php
@@ -7,25 +7,21 @@ namespace AppBundle\Service\Notifier;
 
 class Notifier
 {
-    public function notify(array $data){
-
+    public function notify(array $notification, $entrypoint){
         $host = getenv('CES_ISQUIK_HOST');
         $port = getenv('CES_ISQUIK_PORT');
 
         $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_URL, "$host:$port/notifications");
+        curl_setopt($ch, CURLOPT_URL, "$host:$port/isquik-dev/$entrypoint");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_TIMEOUT, 3);
         curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($notification));
 
         $output = curl_exec($ch);
         $info = curl_getinfo($ch);
 
         curl_close($ch);
-
-        //dump($output);die;
     }
-
 }
