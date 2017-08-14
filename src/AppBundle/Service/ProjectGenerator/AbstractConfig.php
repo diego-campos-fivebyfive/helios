3e53163404134af037d5d49ff91f7721720758d9
Claudinei M <cjchamado@gmail.com>
2017-08-14 14:04:14 -0300

    change maxInverters + add static method for allowed types

--- src/AppBundle/Service/ProjectGenerator/AbstractConfig.php
+++ src/AppBundle/Service/ProjectGenerator/AbstractConfig.php
@@ -4,5 +4,29 @@ namespace AppBundle\Service\ProjectGenerator;
 
 abstract class AbstractConfig
 {
-    public static $maxInverters = 15;
+    public static $maxInverters = 10;
+
+    const PHASE_NUMBER_MONOPHASIC = 'Monophasic';
+    const PHASE_NUMBER_BIPHASIC = 'Biphasic';
+    const PHASE_NUMBER_TRIPHASIC = 'Triphasic';
+
+    /**
+     * @return array
+     */
+    public static function getPhaseNumbers()
+    {
+        return [
+            self::PHASE_NUMBER_MONOPHASIC,
+            self::PHASE_NUMBER_BIPHASIC,
+            self::PHASE_NUMBER_TRIPHASIC
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public static function getVoltages()
+    {
+        return ['127/220', '220/380'];
+    }
 }
