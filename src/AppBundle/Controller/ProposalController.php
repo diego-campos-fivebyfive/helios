24364db85e1dec229f0a2591f446ab5be5e19691
Fabio Dukievicz <fabiojd47@gmail.com>
2017-08-15 15:06:43 -0300

    Rotas do gerador de pdf

--- src/AppBundle/Controller/ProposalController.php
+++ src/AppBundle/Controller/ProposalController.php
@@ -5,8 +5,11 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Component\Project;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use APY\BreadcrumbTrailBundle\Annotation\Breadcrumb;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 
 /**
@@ -48,6 +51,66 @@ class ProposalController extends AbstractController
             [
                 'project' => $project
             ]);
+    }
+
+    /**
+     * @Route("/{id}/generator", name="proposal_pdf_generator")
+     */
+    public function generatorAction($id)
+    {
+        $snappy = $this->get('knp_snappy.pdf');
+        $snappy->setOption('viewport-size', '1280x1024');
+        $snappy->setOption('margin-top', 0);
+        $snappy->setOption('margin-bottom', 0);
+        $snappy->setOption('margin-left', 0);
+        $snappy->setOption('margin-right', 0);
+        $snappy->setOption('zoom', 1.25);
+
+
+        $dir = $this->get('kernel')->getRootDir() . '/../storage/';
+        $filename = md5(uniqid(time())) . '.pdf';
+        $file = $dir.$filename;
+
+        $url = $this->generateUrl('files_pdf',['id'=>$id], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        try {
+            $snappy->generate($url, $file);
+            if(file_exists($file)){
+                return $this->json(
+                    ['filename' => $filename],
+                    Response::HTTP_OK
+                );
+            }else{
+                return $this->json(
+                    ['error' => 'File not found.'],
+                    Response::HTTP_NOT_FOUND);
+            }
+        }
+        catch(\Exception $error) {
+            return $this->json(
+                [
+                    'error' => 'Could not generate PDF.',
+                    'filename' => $filename
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * @Route("/display/{filename}", name="proposal_display_pdf")
+     */
+    public function displayAction($filename)
+    {
+        $dir = $this->get('kernel')->getRootDir() . '/../storage/';
+        $file = $dir.$filename;
+        if(file_exists($file)){
+            return new BinaryFileResponse($file);
+        }else{
+            return $this->json(
+                ['error' => 'File not found.'],
+                Response::HTTP_NOT_FOUND);
+        }
+
     }
 
     /**
