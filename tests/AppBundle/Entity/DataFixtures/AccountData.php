fa27310a671d964e62c888f72cba55a611d6a50c
Claudinei M <cjchamado@gmail.com>
2017-08-15 15:14:11 -0300

    add account data fixture

--- tests/AppBundle/Entity/DataFixtures/AccountData.php
+++ tests/AppBundle/Entity/DataFixtures/AccountData.php
@@ -1,15 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudinei
- * Date: 15/08/17
- * Time: 15:11
- */
 
 namespace Tests\AppBundle\Entity\DataFixtures;
 
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Tests\AppBundle\Helpers\ObjectHelperTest;
+use AppBundle\Entity\Customer as Account;
 
-class AccountData
+class AccountData extends AbstractFixture implements OrderedFixtureInterface
 {
+    use ObjectHelperTest;
 
+    /**
+     * @inheritDoc
+     */
+    public function load(ObjectManager $manager)
+    {
+        $account = new Account();
+
+        $account
+            ->setContext(Account::CONTEXT_ACCOUNT)
+            ->setFirstname(self::randomString(15))
+            ->setEmail(sprintf('%s@%s.com', self::randomString(5), self::randomString(5)))
+        ;
+
+        $manager->persist($account);
+        $manager->flush();
+
+        $this->addReference('account', $account);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getOrder()
+    {
+        return 1;
+    }
 }
