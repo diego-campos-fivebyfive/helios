87480b9252984bf8e635c8bc8826288f4ecafe57
Claudinei M <cjchamado@gmail.com>
2017-08-15 15:14:37 -0300

    add order grouping test

--- tests/AppBundle/Service/Order/OrderGrouperTest.php
+++ tests/AppBundle/Service/Order/OrderGrouperTest.php
@@ -1,15 +1,58 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudinei
- * Date: 15/08/17
- * Time: 14:53
- */
 
 namespace Tests\AppBundle\Service\Order;
 
+use AppBundle\Entity\Order\Element;
+use Tests\AppBundle\AppTestCase;
 
-class OrderGrouperTest
+/**
+ * Class OrderGrouperTest
+ * @group order_grouper
+ */
+class OrderGrouperTest extends AppTestCase
 {
+    public function testDefaultGrouper()
+    {
+        $childrens = $this->createOrders(5);
+        $total = 0;
+        foreach ($childrens as $children){
+            $total += $children->getTotal();
+        }
 
+        $grouper = $this->getContainer()->get('order_transformer');
+
+        $order = $grouper->transformFromChildrens($childrens);
+
+        $this->assertEquals(5, $order->getChildrens()->count());
+        $this->assertEquals($total, $order->getTotal());
+        $this->assertNotNull($order->getId());
+        $this->assertNotNull($order->getAccount());
+    }
+
+    private function createOrders($number)
+    {
+        $manager = $this->manager('order');
+        $account = $this->getFixture('account');
+
+        $orders = [];
+        for($i= 0; $i < $number; $i++){
+            $order = $manager->create();
+
+            $element = new Element();
+            $element
+                ->setCode('ABCD')
+                ->setDescription('The Element')
+                ->setUnitPrice(1000)
+                ->setQuantity($i+1)
+            ;
+
+            $order
+                ->setAccount($account)
+                ->addElement($element);
+
+            $orders[] = $order;
+        }
+
+        return $orders;
+    }
 }
