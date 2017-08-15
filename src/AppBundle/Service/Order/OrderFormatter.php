2c72d6685c4cde7afb9c3638cffcd8e9e4caf9da
Claudinei M <cjchamado@gmail.com>
2017-08-15 09:59:49 -0300

    add order formatter

--- src/AppBundle/Service/Order/OrderFormatter.php
+++ src/AppBundle/Service/Order/OrderFormatter.php
@@ -1,15 +1,37 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudinei
- * Date: 15/08/17
- * Time: 09:27
- */
 
 namespace AppBundle\Service\Order;
 
+use AppBundle\Entity\Order\OrderInterface;
 
-class OrderFormatter
+abstract class OrderFormatter
 {
+    public static function format(OrderInterface $order)
+    {
+        $data = [];
 
+        if(null != $account = $order->getAccount()){
+            $data['account_id'] = $account->getId();
+        }
+
+        $data = [
+            'description' => $order->getDescription(),
+            'elements' => []
+        ];
+
+        $elements = [];
+        foreach ($order->getElements() as $element){
+
+            $elements[] = [
+                'code' => $element->getCode(),
+                'description' => $element->getDescription(),
+                'quantity' => $element->getQuantity(),
+                'unitPrice' => $element->getUnitPrice()
+            ];
+        }
+
+        $data['products'] = $elements;
+
+        return array_filter($data);
+    }
 }
