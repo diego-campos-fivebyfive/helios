f3728508508c302484248c4c012d4aa839026c00
Claudinei M <cjchamado@gmail.com>
2017-08-16 09:23:29 -0300

    apply unit tests

--- tests/AppBundle/Service/Order/ElementResolverTest.php
+++ tests/AppBundle/Service/Order/ElementResolverTest.php
@@ -1,15 +1,36 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudinei
- * Date: 16/08/17
- * Time: 08:55
- */
 
 namespace Tests\AppBundle\Service\Order;
 
+use AppBundle\Entity\Order\Element;
+use AppBundle\Service\Order\ElementResolver;
+use Liip\FunctionalTestBundle\Test\WebTestCase;
 
-class ElementResolverTest
+/**
+ * @group element_resolver
+ */
+class ElementResolverTest extends WebTestCase
 {
+    public function testResolveElementAndData()
+    {
+        $firstArg = null;   //  Instância desconhecida
+        $secondArg = null;  //  Instância desconhecida
 
+        $element = ElementResolver::resolve($firstArg, $secondArg);
+        $this->assertInstanceOf(Element::class, $element);
+
+        $firstArg = [];
+        $secondArg = null;
+
+        $element = ElementResolver::resolve($firstArg, $secondArg);
+        $this->assertInstanceOf(Element::class, $element);
+
+        $firstArg = new Element();
+        $firstArg->setCode('ABC');
+        $secondArg = [];
+
+        $element = ElementResolver::resolve($firstArg, $secondArg);
+        $this->assertInstanceOf(Element::class, $firstArg);
+        $this->assertEquals($element->getCode(), $firstArg->getCode());
+    }
 }
