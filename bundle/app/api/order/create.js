1c65642088a880c3d14364733a0e3eceb11bfc61
Rafael Kendrik <rafamikovski@gmail.com>
2017-08-17 11:56:26 -0300

    order split and join methods

--- bundle/app/api/order/create.js
+++ bundle/app/api/order/create.js
@@ -3,29 +3,37 @@
 const Sices = require('../../models/sices')
 const Isquik = require('../../models/isquik')
 
-const level = {
-  'BLACK': 'black',
-  'PLATINUM': 'platinum',
-  'PREMIUM': 'premium',
-  'PARCEIRO OURO': 'gold',
-  'PROMOCIONAL': 'promotional'
-}
+const splitOrder = ({ Dado: order }) => ({
+  isquik_id: order.IdIntegrador,
+  version: memorial.Versao,
+  status: memorial.FlagPublicado,
+  range: memorial.Produtos.map(range => ({
+    code: range.Codigo,
+    markups: range.Faixas.map(markup => ({
+      initial: markup.De,
+      final: markup.Ate,
+      levels: markup.Niveis.map(level => ({
+        price: level.PrecoVenda,
+        level: getLevel(level.Descricao)
+      }))
+    }))
+  }))
+})
 
-const getLevel = type => level[type]
+const joinOrders = ({ itens: children, ...master }) => ([
+  ...children,
+  master
+])
 
-const sendOrder = ({ Dado: order }) =>
-  Sices
-    .sendOrder({
-      extraDocument: order.InscricaoEstadual,
-      level: getLevel(order.NivelDesconto.Descricao),
-      status: true,
-      isquik_id: order.IdIntegrador
-    })
+const sendOrders = orders =>
+  orders.reduce(Sices.sendOrder)
 
 const createOrder = ({ notification }) =>
   Isquik
     .getOrder(notification.id)
-    .then(sendOrder)
+    .then(splitOrder)
+    .then(joinOrders)
+    .then(sendOrders)
 
 module.exports = {
   create: createOrder
