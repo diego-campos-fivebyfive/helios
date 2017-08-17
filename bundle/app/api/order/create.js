d2901cad6ba526469dea260a28358070675a8551
Rafael Kendrik <rafamikovski@gmail.com>
2017-08-17 10:28:56 -0300

    order module init

---
+++ bundle/app/api/order/create.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const Sices = require('../../models/sices')
+const Isquik = require('../../models/isquik')
+
+const level = {
+  'BLACK': 'black',
+  'PLATINUM': 'platinum',
+  'PREMIUM': 'premium',
+  'PARCEIRO OURO': 'gold',
+  'PROMOCIONAL': 'promotional'
+}
+
+const getLevel = type => level[type]
+
+const sendOrder = ({ Dados: order }) =>
+  Sices
+    .sendOrder({
+      extraDocument: order.InscricaoEstadual,
+      level: getLevel(order.NivelDesconto.Descricao),
+      status: true,
+      isquik_id: order.IdIntegrador
+    })
+
+const createOrder = ({ notification }) =>
+  Isquik
+    .getOrder(notification.id)
+    .then(sendOrder)
+
+module.exports = {
+  create: createOrder
+}
---
+++ bundle/app/api/order/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const { create } = require('./create')
+//const { update } = require('./update')
+
+const order = {
+  create,
+  //update
+}
+
+module.exports = order
