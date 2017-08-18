ccedb17a95b9e66a30a5bc96da7d61d1a6fb2322
Claudinei M <cjchamado@gmail.com>
2017-08-18 13:05:27 -0300

    set not required fields

--- src/AppBundle/Form/Extra/PreRegisterType.php
+++ src/AppBundle/Form/Extra/PreRegisterType.php
@@ -1,16 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Fabio
- * Date: 22/06/2017
- * Time: 11:15
- */
 
 namespace AppBundle\Form\Extra;
 
-use AppBundle\Entity\Extra\AccountRegister;
 use AppBundle\Util\Validator\Constraints\ContainsCnpj;
-use AppBundle\Util\Validator\Constraints\ContainsCnpjValidator;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -25,7 +17,9 @@ class PreRegisterType extends AbstractType
             ->add('document',TextType::class, array(
                 'constraints' => new ContainsCnpj()
             ))
-            ->add('extraDocument',TextType::class)
+            ->add('extraDocument',TextType::class, [
+                'required' => false
+            ])
             ->add('lastname',TextType::class)
             ->add('firstname',TextType::class)
             ->add('postcode',TextType::class)
@@ -33,7 +27,9 @@ class PreRegisterType extends AbstractType
             ->add('city',TextType::class)
             ->add('district',TextType::class)
             ->add('street',TextType::class)
-            ->add('number',TextType::class)
+            ->add('number',TextType::class, [
+                'required' => false
+            ])
 
             ->add('contact',TextType::class)
             ->add('email',EmailType::class)
