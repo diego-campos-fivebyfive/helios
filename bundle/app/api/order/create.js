7500ab686e68351e4834dc17f13223fb1903e053
Rafael Kendrik <rafamikovski@gmail.com>
2017-08-22 15:38:31 -0300

    reduce adjusts

--- bundle/app/api/order/create.js
+++ bundle/app/api/order/create.js
@@ -8,13 +8,15 @@ const getStatus = status => status === 'Aprovado'
 const splitOrder = ({ Dado: order }) => ({
   account_id: order.Integrador.IdSicesSolar,
   isquik_id: order.Integrador.IdIntegrador,
+  description: '',
   note: '',
   status: getStatus(order.Status.Descricao),
   items: order.itens.map(item => ({
     description: item.DescricaoSistema,
     note: '',
-    parent_id:  order.IdSicesSolar,
+    parent_id: 40,
     code: item.CodigoProduto,
+    status: getStatus(order.Status.Descricao),
     products: item.subItens.map(product => ({
       code: product.CodigoProduto,
       description: product.Descricao,
@@ -31,7 +33,7 @@ const joinOrders = ({ items: children, ...master }) => ([
 ])
 
 const sendOrders = orders =>
-  orders.reduce((y, x) => Sices.sendOrder(x))
+  orders.reduce((y, x) => Sices.sendOrder(x), {})
 
 const createOrder = ({ notification }) =>
   Isquik
