2856df33fa3a8a4f4b3d6d75762363c0344b6143
Rafael Kendrik <rafamikovski@gmail.com>
2017-08-24 09:23:43 -0300

    delete method

--- bundle/app/models/sices.js
+++ bundle/app/models/sices.js
@@ -18,7 +18,7 @@ const getAuthentication = () => request({
     statusCode
   }))
 
-const getRequest = uri => getAuthentication().then(auth => (
+const get = uri => getAuthentication().then(auth => (
   request({
     resolveWithFullResponse: true,
     headers: {
@@ -34,7 +34,7 @@ const getRequest = uri => getAuthentication().then(auth => (
     }))
 ))
 
-const postRequest = (uri, data) => getAuthentication().then(auth => (
+const post = (uri, data) => getAuthentication().then(auth => (
   request({
     resolveWithFullResponse: true,
     headers: {
@@ -51,7 +51,7 @@ const postRequest = (uri, data) => getAuthentication().then(auth => (
     }))
 ))
 
-const putRequest = (uri, data) => getAuthentication().then(auth => (
+const put = (uri, data) => getAuthentication().then(auth => (
   request({
     resolveWithFullResponse: true,
     headers: {
@@ -68,32 +68,53 @@ const putRequest = (uri, data) => getAuthentication().then(auth => (
     }))
 ))
 
-const updateAccount = (id, account) => putRequest(`${sices.uri}/accounts/${id}`, account)
-const updateUser = (id, user) => putRequest(`${sices.uri}/users/${id}`, user)
-const updateOrder = (id, order) => putRequest(`${sices.uri}/orders/${id}`, order)
-const sendInverter = inverter => postRequest(`${sices.uri}/inverters`, inverter)
-const sendStructure = structure => postRequest(`${sices.uri}/structures`, structure)
-const sendModule = module => postRequest(`${sices.uri}/modules`, module)
-const sendStringbox = stringbox => postRequest(`${sices.uri}/stringboxes`, stringbox)
-const sendVariety = variety => postRequest(`${sices.uri}/varieties`, variety)
-const sendMemorial = memorial => postRequest(`${sices.uri}/memorials`, memorial)
-const sendAccount = account => postRequest(`${sices.uri}/accounts`, account)
-const sendOrder = order => postRequest(`${sices.uri}/orders`, order)
-const sendUser = user => postRequest(`${sices.uri}/users`, user)
-const getUser = id => getRequest(`${sices.uri}/users/${id}`)
+const del = uri => getAuthentication().then(auth => (
+  request({
+    resolveWithFullResponse: true,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${auth.access_token}`
+    },
+    method: 'DELETE',
+    uri
+  })
+    .then(({ body, statusCode }) => ({
+      ...JSON.parse(body),
+      statusCode
+    }))
+))
+
+const sendAccount = account => post(`${sices.uri}/accounts`, account)
+const updateAccount = (id, account) => put(`${sices.uri}/accounts/${id}`, account)
+
+const getUser = id => get(`${sices.uri}/users/${id}`)
+const sendUser = user => post(`${sices.uri}/users`, user)
+const updateUser = (id, user) => put(`${sices.uri}/users/${id}`, user)
+
+const sendOrder = order => post(`${sices.uri}/orders`, order)
+const updateOrder = (id, order) => put(`${sices.uri}/orders/${id}`, order)
+const deleteOrder = id => del(`${sices.uri}/orders/${id}`)
+
+const sendMemorial = memorial => post(`${sices.uri}/memorials`, memorial)
+const sendInverter = inverter => post(`${sices.uri}/inverters`, inverter)
+const sendStructure = structure => post(`${sices.uri}/structures`, structure)
+const sendModule = module => post(`${sices.uri}/modules`, module)
+const sendStringbox = stringbox => post(`${sices.uri}/stringboxes`, stringbox)
+const sendVariety = variety => post(`${sices.uri}/varieties`, variety)
 
 module.exports = {
+  sendAccount,
   updateAccount,
+  getUser,
+  sendUser,
   updateUser,
+  sendOrder,
   updateOrder,
+  deleteOrder,
+  sendMemorial,
   sendInverter,
   sendStructure,
   sendModule,
   sendStringbox,
-  sendVariety,
-  sendMemorial,
-  sendAccount,
-  sendOrder,
-  sendUser,
-  getUser
+  sendVariety
 }
