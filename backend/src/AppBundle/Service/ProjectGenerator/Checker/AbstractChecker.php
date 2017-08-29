4b8f87faa708c6550e666f76a35cc83c95581002
Claudinei M <cjchamado@gmail.com>
2017-08-29 15:42:55 -0300

    update component fixtures

---
+++ backend/src/AppBundle/Service/ProjectGenerator/Checker/AbstractChecker.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudinei
+ * Date: 29/08/17
+ * Time: 15:40
+ */
+
+namespace AppBundle\Service\ProjectGenerator\Checker;
+
+
+class AbstractChecker
+{
+
+}
