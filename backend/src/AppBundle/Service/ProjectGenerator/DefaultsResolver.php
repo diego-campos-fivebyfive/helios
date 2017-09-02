56ecf9526dd5bbbad662645c4707f45ff7df401c
Claudinei M <cjchamado@gmail.com>
2017-09-02 12:00:03 -0300

    create entity filter

---
+++ backend/src/AppBundle/Service/ProjectGenerator/DefaultsResolver.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudinei
+ * Date: 02/09/17
+ * Time: 10:12
+ */
+
+namespace AppBundle\Service\ProjectGenerator;
+
+
+class GeneratorDefaults
+{
+
+}
