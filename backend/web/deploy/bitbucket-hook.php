e25d829427e96c3b09a7495a14560cb54b4ae018
Rafael Kendrik <rafamikovski@gmail.com>
2017-09-02 10:13:11 -0300

    removing php deploy module

--- backend/web/deploy/bitbucket-hook.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-exec('$CLI_PATH/ces-app-deploy --homolog');
-
-$date = date('m/d/Y h:i:s a');
-$log = "Date: $date | Commit: MERGED \n";
-file_put_contents(__DIR__.'/deploy.log', $log , FILE_APPEND);
