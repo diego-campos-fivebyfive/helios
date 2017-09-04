e7d877e15db5b352847404a6fdb49f28787271c3
Claudinei M <cjchamado@gmail.com>
2017-09-04 16:00:43 -0300

    add insurance_test class

--- backend/tests/AppBundle/Service/Pricing/InsuranceTest.php
+++ backend/tests/AppBundle/Service/Pricing/InsuranceTest.php
@@ -1,15 +1,38 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudinei
- * Date: 04/09/17
- * Time: 15:01
- */
 
 namespace Tests\AppBundle\Service\Pricing;
 
+use AppBundle\Entity\Component\Project;
+use AppBundle\Entity\Order\Element;
+use AppBundle\Entity\Order\Order;
+use AppBundle\Service\Pricing\Insurance;
+use Tests\AppBundle\AppTestCase;
 
-class InsuranceTest
+/**
+ * Class InsuranceTest
+ * @group insurance
+ */
+class InsuranceTest extends AppTestCase
 {
+    public function testCalculationFromProject()
+    {
+        $project = new Project();
+        $project->setCostPrice(10000);
 
+        $order = new Order();
+        $element = new Element();
+        $element
+            ->setUnitPrice(1000)
+            ->setQuantity(20)
+            ->setOrder($order)
+        ;
+
+        Insurance::insure($project);
+        Insurance::insure($order);
+
+        $this->assertEquals(65, $project->getInsurance());
+
+        $this->assertEquals(20000, $order->getTotal());
+        $this->assertEquals(130, $order->getInsurance());
+    }
 }
