15eec7d68fc024fc46470f315e88a0032080b83d
Claudinei M <cjchamado@gmail.com>
2017-09-04 16:01:22 -0300

    add insurance calculator

--- backend/src/AppBundle/Service/Pricing/InsurableInterface.php
+++ backend/src/AppBundle/Service/Pricing/InsurableInterface.php
@@ -1,15 +1,34 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudinei
- * Date: 04/09/17
- * Time: 15:14
+
+/*
+ * This file is part of the SicesSolar package.
+ *
+ * (c) SicesSolar <http://sicesbrasil.com.br/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace AppBundle\Service\Pricing;
 
-
+/**
+ * @author Claudinei Machado <claudinei@kolinalabs.com>
+ */
 interface InsurableInterface
 {
+    /**
+     * @return float
+     */
+    public function getInsuranceQuota();
 
+    /**
+     * @param float $insurance
+     * @return $this
+     */
+    public function setInsurance($insurance);
+
+    /**
+     * @return float
+     */
+    public function getInsurance();
 }
--- backend/src/AppBundle/Service/Pricing/Insurance.php
+++ backend/src/AppBundle/Service/Pricing/Insurance.php
@@ -1,15 +1,45 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudinei
- * Date: 04/09/17
- * Time: 15:07
+
+/*
+ * This file is part of the SicesSolar package.
+ *
+ * (c) SicesSolar <http://sicesbrasil.com.br/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace AppBundle\Service\Pricing;
 
-
+/**
+ * This class provide a default methods from insurable entities
+ *
+ * @author Claudinei Machado <claudinei@kolinalabs.com>
+ */
 class Insurance
 {
+    /**
+     * Percent off base price
+     */
+    const PERCENT = 0.0065;
 
+    /**
+     * @param InsurableInterface $insurable
+     */
+    public static function insure(InsurableInterface $insurable)
+    {
+        $quota = $insurable->getInsuranceQuota();
+
+        $insurable->setInsurance(self::calculate($quota));
+    }
+
+    /**
+     * @param $quota
+     * @param float $percent
+     * @return float
+     */
+    public static function calculate($quota, $percent = self::PERCENT)
+    {
+        return ($quota * $percent);
+    }
 }
