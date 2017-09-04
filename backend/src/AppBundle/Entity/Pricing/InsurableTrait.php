b0472c504271b8c87e9003dbaa78b82d145453c5
Claudinei M <cjchamado@gmail.com>
2017-09-04 16:01:44 -0300

    add insurance calculator trait support

--- backend/src/AppBundle/Entity/Pricing/InsurableTrait.php
+++ backend/src/AppBundle/Entity/Pricing/InsurableTrait.php
@@ -1,6 +1,15 @@
 <?php
 
-namespace AppBundle\Entity\Common;
+/*
+ * This file is part of the SicesSolar package.
+ *
+ * (c) SicesSolar <http://sicesbrasil.com.br/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Entity\Pricing;
 
 use Doctrine\ORM\Mapping as ORM;
 
