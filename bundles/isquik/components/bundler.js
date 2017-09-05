a3dcd26cd2df827d19984cc45906c53f6f2a6518
Rafael Kendrik <rafamikovski@gmail.com>
2017-09-05 10:33:03 -0300

    encoding exception message

--- bundles/isquik/components/bundler.js
+++ bundles/isquik/components/bundler.js
@@ -32,7 +32,7 @@ const bundler = actions => (request, response) => {
         .end()
 
       if (process.env.CES_AMBIENCE === 'development') {
-        throw new Error(`Internal error: ${message}`)
+        throw new Error(`Internal error: ${JSON.stringify(message)}`)
       } else {
         /*eslint-disable */
         exec(`$CLI_PATH/ces-slack-notify --backlog-api \'${JSON.stringify(message)}\'`)
