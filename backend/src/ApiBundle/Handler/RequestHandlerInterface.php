f4397d7b88fee61b5e61e075c2ed6e35d1b7dedd
Claudinei M <cjchamado@gmail.com>
2017-09-12 15:55:52 -0300

    add form request handler

--- backend/src/ApiBundle/Handler/RequestHandlerInterface.php
+++ backend/src/ApiBundle/Handler/RequestHandlerInterface.php
@@ -1,15 +1,50 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudinei
- * Date: 12/09/17
- * Time: 13:07
- */
 
-namespace ApiBundle\Request;
+namespace ApiBundle\Handler;
 
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
 
-interface HandlerInterface
+interface RequestHandlerInterface
 {
+    /**
+     * @param Request $request
+     * @return RequestHandlerInterface
+     */
+    public function setRequest(Request $request);
 
+    /**
+     * @return Request
+     */
+    public function getRequest();
+
+    /**
+     * @param FormInterface $form
+     * @return RequestHandlerInterface
+     */
+    public function setForm(FormInterface $form);
+
+    /**
+     * @return FormInterface
+     */
+    public function getForm();
+
+    /**
+     * @param Request|null $request
+     * @param FormInterface|null $form
+     * @return bool
+     */
+    public function handle(Request $request = null, FormInterface $form = null);
+
+    /**
+     * @return \FOS\RestBundle\View\View
+     */
+    public function view();
+
+    /**
+     * @param Request|null $request
+     * @param FormInterface|null $form
+     * @return RequestHandlerInterface
+     */
+    public static function create(Request $request = null, FormInterface $form = null);
 }
