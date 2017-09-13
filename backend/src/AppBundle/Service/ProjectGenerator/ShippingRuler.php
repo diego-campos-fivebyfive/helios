398971d458d7d2185ec70f2cda2c32d8e2ac29fd
joaozaqueu <joaozaqueuchereta@gmail.com>
2017-09-13 15:11:19 -0300

    modificações no service ShippingRuller

--- backend/src/AppBundle/Service/ProjectGenerator/ShippingRuler.php
+++ backend/src/AppBundle/Service/ProjectGenerator/ShippingRuler.php
@@ -30,7 +30,7 @@ abstract class ShippingRuler
      */
     public static function apply(&$rule)
     {
-        if('self' == $rule['type']){
+        if('self' == $rule['type'] || 0 == $rule['price']){
             self::defaults($rule);
         }else {
             self::definitions($rule);
