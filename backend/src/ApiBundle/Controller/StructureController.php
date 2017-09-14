51e81fe34b49ed1e187f2a4476cc56612edfadfc
joaozaqueu <joaozaqueuchereta@gmail.com>
2017-09-14 14:19:44 -0300

    modificações dos metodos de structure na api

--- backend/src/ApiBundle/Controller/StructureController.php
+++ backend/src/ApiBundle/Controller/StructureController.php
@@ -2,47 +2,17 @@
 
 namespace ApiBundle\Controller;
 
+use ApiBundle\Form\StructureType;
+use ApiBundle\Handler\RequestHandler;
 use AppBundle\Entity\Component\Structure;
-use AppBundle\Entity\Customer;
-use Doctrine\Common\Cache\VoidCache;
-use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\View\View;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
-class StructureController extends FOSRestController
+class StructureController extends AbstractApiController
 {
-    public function postStructuresAction(Request $request)
-    {
-        $data = json_decode($request->getContent(), true);
-
-        $structureManager = $this->get('structure_manager');
-
-        /** @var Structure $structure */
-        $structure = $structureManager->create();
-        $structure
-            ->setCode($data['code'])
-            ->setDescription($data['description'])
-            ->setAvailable($data['available'])
-            ->setStatus(false);
-
-        try {
-            $structureManager->save($structure);
-            $status = Response::HTTP_CREATED;
-            $data = $this->get('api_formatter')->format($structure, ['maker' => 'id']);
-        }catch (\Exception $exception){
-            $status = Response::HTTP_UNPROCESSABLE_ENTITY;
-            $data = $exception;
-        }
-
-        $view = View::create($data)->setStatusCode($status);
-
-        return $this->handleView($view);
-    }
-
     public function getStructureAction(Structure $structure)
     {
         $data = $this->get('api_formatter')->format($structure, ['maker' => 'id']);
@@ -53,27 +23,49 @@ class StructureController extends FOSRestController
     }
 
     /**
-     * @Route("/{code}")
-     * @ParamConverter("code", class="AppBundle:Component\Structure", options={"mapping":{"code" : "code"}})
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function putStructureAction(Request $request, Structure $code)
+    public function getStructuresAction(Request $request)
     {
-        $data = json_decode($request->getContent(), true);
+        $data = $this->get('api_handler')->handleRequest($request, ['maker' => 'id']);
 
-        $structureManager = $this->get('structure_manager');
-        $code->setPromotional($data['promotional']);
+        $view = View::create($data);
 
-        try {
-            $structureManager->save($code);
-            $status = Response::HTTP_CREATED;
-            $data = $this->get('api_formatter')->format($code, ['maker' => 'id']);
+        return $this->handleView($view);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function postStructuresAction(Request $request)
+    {
+        $manager = $this->manager('structure');
+
+        $structure = $manager->create();
+
+        return $this->applyRequest($request, $structure);
+    }
+
+    /**
+     * @param Request $request
+     * @param Structure $structure
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    private function applyRequest(Request $request, Structure $structure)
+    {
+        $manager = $this->manager('structure');
+
+        $form = $this->form(StructureType::class, $structure);
+
+        $handler = RequestHandler::create($request, $form);
+
+        if ($handler->handle()) {
+            $manager->save($structure);
         }
-        catch (\Exception $exception) {
-            $status = Response::HTTP_UNPROCESSABLE_ENTITY;
-            $data = $exception;
-        }
 
-        $view = View::create($data)->setStatusCode($status);
+        $view = $handler->view();
 
         return $this->handleView($view);
     }
