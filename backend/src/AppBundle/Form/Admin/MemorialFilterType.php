98aab10623c7fe828ff9d2b621c66f1c5c9b3228
Claudinei M <cjchamado@gmail.com>
2017-09-19 09:10:54 -0300

    add admin memorial form type

--- backend/src/AppBundle/Form/Admin/MemorialFilterType.php
+++ backend/src/AppBundle/Form/Admin/MemorialFilterType.php
@@ -1,15 +1,53 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudinei
- * Date: 15/09/17
- * Time: 12:39
- */
 
 namespace AppBundle\Form\Admin;
 
+use AppBundle\Entity\Pricing\Memorial;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
-class MemorialFilterType
+class MemorialFilterType extends AbstractType
 {
+    /**
+     * @inheritDoc
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        /** @var Memorial $memorial */
+        //$memorial = $options['memorial'];
+        $levels = $options['levels'];
 
+        $builder
+            ->add('level', ChoiceType::class, [
+                'choices' => array_combine($levels, $levels)
+            ])
+            ->add('components', ChoiceType::class, [
+                'choices' => [
+                    'module' => 'Módulos',
+                    'inverter' => 'Inversores',
+                    'string_box' => 'String Box',
+                    'structure' => 'Estrutura',
+                    'variety' => 'Variedades'
+                ],
+                'expanded' => true,
+                'multiple' => true,
+                'required' => false
+            ])
+        ;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver
+            ->setRequired('memorial')
+            ->setDefaults([
+                'levels' => []
+            ])
+        ;
+    }
 }
