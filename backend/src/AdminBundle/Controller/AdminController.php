087a58ec2f240ea27b6104f5650a7aa1905017f4
Claudinei M <cjchamado@gmail.com>
2017-09-19 09:06:47 -0300

    add distinct manager method

---
+++ backend/src/AdminBundle/Controller/AdminController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudinei
+ * Date: 15/09/17
+ * Time: 10:13
+ */
+
+namespace AdminBundle\Controller;
+
+
+class AdminController
+{
+
+}
---
+++ backend/src/AppBundle/Form/Admin/MemorialFilterType.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudinei
+ * Date: 15/09/17
+ * Time: 12:39
+ */
+
+namespace AppBundle\Form\Admin;
+
+
+class MemorialFilterType
+{
+
+}
