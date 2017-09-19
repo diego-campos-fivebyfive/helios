9514b847d90a2f4b8d704a6cd0041dfb51f9d6f2
Rafael Kendrik <rafamikovski@hotmail.com>
2017-09-19 09:45:43 -0300

    github agent validate

--- devops/hook/api/github/post.js
+++ devops/hook/api/github/post.js
@@ -2,7 +2,15 @@
 
 const { exec } = require('child_process')
 
+const allowOrigin = (request, agent) =>
+  request.headers['user-agent'].includes(agent)
+
 const postHook = (request, response) => {
+  const githubAgent = 'GitHub-Hookshot/ab46a57'
+  if (!allowOrigin(request, githubAgent)) {
+    return
+  }
+
   response
     .status(200)
     .json({ message: 'Hook Recieved' })
