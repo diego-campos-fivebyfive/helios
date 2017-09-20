718882b29d6f7e56cacea99a404d877790d7b8a7
Claudinei M <cjchamado@gmail.com>
2017-09-20 18:10:36 -0300

    disable event listener for doctrine

--- backend/src/AppBundle/EventListener/DoctrineListener.php
+++ backend/src/AppBundle/EventListener/DoctrineListener.php
@@ -30,52 +30,5 @@ class DoctrineListener implements EventSubscriber
      */
     public function getSubscribedEvents()
     {
-        return [
-            Events::preUpdate,
-            Events::postRemove
-        ];
-    }
-
-    /**
-     * @param LifecycleEventArgs | \Doctrine\ORM\Event\PreUpdateEventArgs $args
-     */
-    public function preUpdate(LifecycleEventArgs $args)
-    {
-        $entity = $args->getEntity();
-
-
-        /**
-         * Change component status to featured if one or more fields changed
-         */
-        if($entity instanceof ComponentInterface
-            && $entity->isPrivate()){
-
-            $changeSet = $args->getEntityChangeSet();
-            foreach($changeSet as $property => $values){
-                if('status' != $property && $values[0] != $values[1]){
-                    $entity->setStatus(ComponentInterface::STATUS_FEATURED);
-                    break;
-                }
-            }
-        }
-    }
-
-    /**
-     * @param LifecycleEventArgs $args
-     */
-    public function postRemove(LifecycleEventArgs $args)
-    {
-        $entity = $args->getEntity();
-
-        if($entity instanceof ComponentInterface){
-
-            $maker = $entity->getMaker();
-
-            $components = $maker->isMakerModule() ? $maker->getModules() : $maker->getInverters() ;
-
-            if((0 == $components->count()) && MakerInterface::REMOVE_ZERO_CHILD){
-                $this->container->get('app.maker_manager')->delete($maker);
-            }
-        }
     }
 }
