9410e9b1a41e6c2327d71cfccc64a64c5b92fd37
Claudinei M <cjchamado@gmail.com>
2017-09-20 12:03:56 -0300

    update memorial loader

--- backend/src/AppBundle/Service/Pricing/MemorialLoader.php
+++ backend/src/AppBundle/Service/Pricing/MemorialLoader.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Service\Pricing;
 
+use AppBundle\Entity\Pricing\Memorial;
 use AppBundle\Manager\Pricing\MemorialManager;
 
 class MemorialLoader
@@ -27,9 +28,15 @@ class MemorialLoader
         $qb = $this->manager->createQueryBuilder();
 
         $qb->where('m.status = :status')
-            ->andWhere('m.endAt is null')
-            ->setParameters(['status' => 1]);
+            ->andWhere('m.publishedAt is not null')
+            ->andWhere('m.expiredAt is null')
+            ->setMaxResults(1)
+            ->setParameters([
+                'status' => Memorial::STATUS_ENABLED
+            ]);
 
-        return $qb->getQuery()->getOneOrNullResult();
+        $memorial =$qb->getQuery()->getOneOrNullResult();
+
+        return $memorial;
     }
 }
