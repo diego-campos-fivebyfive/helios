efa058405f4a4fe4417a4321d968d878a4d28aff
Claudinei M <cjchamado@gmail.com>
2017-09-21 17:52:19 -0300

    update memorials management views: field mask, prefix, suffix

---
+++ backend/src/AppBundle/Service/Pricing/MemorialCloner.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudinei
+ * Date: 21/09/17
+ * Time: 15:14
+ */
+
+namespace AppBundle\Service\Pricing;
+
+
+class MemorialCloner
+{
+
+}
---
+++ backend/tests/AppBundle/Service/Pricing/MemorialClonerTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudinei
+ * Date: 21/09/17
+ * Time: 14:50
+ */
+
+namespace Tests\AppBundle\Service\Pricing;
+
+
+class MemorialClonerTest
+{
+
+}
