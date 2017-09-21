c3eb8cfbc9fc0e5f1866614521eb38fa7c40398e
Claudinei M <cjchamado@gmail.com>
2017-09-21 17:54:10 -0300

    change memorial cloner property read

--- backend/src/AppBundle/Service/Pricing/MemorialCloner.php
+++ backend/src/AppBundle/Service/Pricing/MemorialCloner.php
@@ -1,15 +1,69 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudinei
- * Date: 21/09/17
- * Time: 15:14
- */
 
 namespace AppBundle\Service\Pricing;
 
+use AppBundle\Entity\Pricing\Memorial;
+use AppBundle\Entity\Pricing\MemorialInterface;
+use AppBundle\Entity\Pricing\Range;
+use Symfony\Component\PropertyAccess\PropertyAccess;
 
 class MemorialCloner
 {
+    /**
+     * @var array
+     */
+    private $rangeProperties = [
+        'initialPower',
+        'finalPower',
+        'costPrice',
+        'markup',
+        'level',
+        'price',
+        'code',
+        'tax'
+    ];
 
+    /**
+     * @var \Symfony\Component\PropertyAccess\PropertyAccessor
+     */
+    private $accessor;
+
+    /**
+     * MemorialCloner constructor.
+     */
+    function __construct()
+    {
+        $this->accessor = PropertyAccess::createPropertyAccessor();
+    }
+
+    /**
+     * @param MemorialInterface $source
+     * @return Memorial
+     */
+    public function execute(MemorialInterface $source)
+    {
+        $memorial = new Memorial();
+
+        $memorial
+            ->setStatus(Memorial::STATUS_PENDING)
+            ->setName(sprintf('%s [clone:%s]', $source->getName(), $source->getId()))
+        ;
+
+        /** @var Range $sourceRange */
+        foreach ($source->getRanges() as $sourceRange){
+
+            $range = new Range();
+
+            foreach ($this->rangeProperties as $property){
+
+                $value = $this->accessor->getValue($sourceRange, $property);
+
+                $this->accessor->setValue($range, $property, $value);
+            }
+
+            $memorial->addRange($range);
+        }
+
+        return $memorial;
+    }
 }
