fb1179711ddaaae92cefd1d2946a930b7670b42e
Fabio Dukievicz <fabiojd47@gmail.com>
2017-09-21 10:41:29 -0300

    UsersController

--- backend/src/AdminBundle/Controller/UsersController.php
+++ backend/src/AdminBundle/Controller/UsersController.php
@@ -1,18 +1,34 @@
 <?php
 
-namespace AppBundle\Controller;
+namespace AdminBundle\Controller;
 
+use AdminBundle\Form\MemberType;
+use AdminBundle\Form\UserType;
+use AppBundle\Controller\AbstractController;
+use AppBundle\Entity\AccountInterface;
+use AppBundle\Entity\BusinessInterface;
+use AppBundle\Entity\Customer;
+use AppBundle\Entity\MemberInterface;
+use AppBundle\Entity\UserInterface;
 use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
+use APY\BreadcrumbTrailBundle\Annotation\Breadcrumb;
+use Symfony\Component\HttpFoundation\Response;
+
 
 /**
  * User controller.
  *
  * @Route("user")
+ * @Security("has_role('ROLE_ADMIN')")
+ *
+ * @Breadcrumb("Sices")
  */
-class UserController extends Controller
+class UsersController extends AbstractController
 {
     /**
      * Lists all user entities.
@@ -22,38 +38,64 @@ class UserController extends Controller
      */
     public function indexAction()
     {
-        $em = $this->getDoctrine()->getManager();
+        /** @var Customer $manager */
+        $manager = $this->manager('customer');
 
-        $users = $em->getRepository('AppBundle:User')->findAll();
+        /** @var MemberInterface $member */
+        $members = $manager->findBy([
+            'context' => 'member'
+        ]);
+        $data = [];
+        foreach ($members as $member) {
+            if (($member->isPlatformAdmin() || $member->isPlatformCommercial()) && !$member->isDeleted())
+                $data[] = $member;
+        }
 
-        return $this->render('user/index.html.twig', array(
-            'users' => $users,
+        return $this->render('admin/user/index.html.twig', array(
+            'members' => $data,
         ));
     }
 
     /**
      * Creates a new user entity.
      *
-     * @Route("/new", name="user_new")
+     * @Route("/create", name="create_user")
      * @Method({"GET", "POST"})
      */
-    public function newAction(Request $request)
+    public function createAction(Request $request)
     {
-        $user = new User();
-        $form = $this->createForm('AppBundle\Form\UserType', $user);
+        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+        $userManager = $this->get('fos_user.user_manager');
+
+        $memberManager = $this->manager('customer');
+
+        /** @var AccountInterface $accountManager */
+        $accountManager = $this->get('account_manager');
+        $account = $accountManager->find(634);
+
+        $user = $userManager->createUser();
+        $user->setCreatedAt(new \DateTime('now'));
+
+        $member = $memberManager->create();
+        $member
+            ->setContext(Customer::CONTEXT_MEMBER)
+            ->setAccount($account)
+            ->setUser($user);
+
+        $form = $this->createForm(MemberType::class, $member);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
 
-            return $this->redirectToRoute('user_show', array('id' => $user->getId()));
+            $user->addRole(UserInterface::ROLE_PLATFORM_COMMERCIAL);
+
+            $memberManager->save($member);
+
+            return $this->redirectToRoute('user_index');
         }
 
-        return $this->render('user/new.html.twig', array(
-            'user' => $user,
-            'form' => $form->createView(),
+        return $this->render('admin/user/form.html.twig', array(
+           'form' => $form->createView(),
         ));
     }
 
@@ -63,74 +105,60 @@ class UserController extends Controller
      * @Route("/{id}", name="user_show")
      * @Method("GET")
      */
-    public function showAction(User $user)
+    public function showAction(Customer $member)
     {
-        $deleteForm = $this->createDeleteForm($user);
-
-        return $this->render('user/show.html.twig', array(
-            'user' => $user,
-            'delete_form' => $deleteForm->createView(),
+        return $this->render('admin/user/show.html.twig', array(
+            'member' => $member,
+            'is_admin' => $member->isPlatformAdmin()
         ));
     }
 
     /**
      * Displays a form to edit an existing user entity.
      *
-     * @Route("/{id}/edit", name="user_edit")
+     * @Route("/update/{id}", name="user_update")
      * @Method({"GET", "POST"})
      */
-    public function editAction(Request $request, User $user)
+    public function updateAction(Request $request, Customer $member)
     {
-        $deleteForm = $this->createDeleteForm($user);
-        $editForm = $this->createForm('AppBundle\Form\UserType', $user);
-        $editForm->handleRequest($request);
+        $memberManager = $this->manager('customer');
 
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+        $form = $this->createForm(MemberType::class, $member);
+        $form->handleRequest($request);
 
-            return $this->redirectToRoute('user_edit', array('id' => $user->getId()));
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $member->getUser()->addRole(UserInterface::ROLE_PLATFORM_COMMERCIAL);
+
+            $memberManager->save($member);
+
+            return $this->redirectToRoute('user_index');
         }
 
-        return $this->render('user/edit.html.twig', array(
-            'user' => $user,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
+        return $this->render('admin/user/form.html.twig', array(
+            'form' => $form->createView(),
         ));
     }
 
     /**
-     * Deletes a user entity.
      *
-     * @Route("/{id}", name="user_delete")
-     * @Method("DELETE")
+     * @Route("/{id}/delete", name="user_delete")
+     * @Method("delete")
      */
-    public function deleteAction(Request $request, User $user)
+    public function deleteAction(Customer $member)
     {
-        $form = $this->createDeleteForm($user);
-        $form->handleRequest($request);
+        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+        $userManager = $this->get('fos_user.user_manager');
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($user);
-            $em->flush();
+        try {
+            $this->manager('customer')->delete($member);
+            $userManager->deleteUser($member->getUser());
+            $status = Response::HTTP_OK;
+        } catch (\Exception $exception) {
+            $status = Response::HTTP_NOT_FOUND;
         }
 
-        return $this->redirectToRoute('user_index');
-    }
+        return $this->json([], $status);
 
-    /**
-     * Creates a form to delete a user entity.
-     *
-     * @param User $user The user entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(User $user)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('user_delete', array('id' => $user->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
     }
 }
