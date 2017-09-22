9c7c44477ce94fbbe442867a50b7a4bdca090835
Fabio Dukievicz <fabiojd47@gmail.com>
2017-09-22 16:55:32 -0300

    configuração de email

--- backend/src/AppBundle/Service/Mailer.php
+++ backend/src/AppBundle/Service/Mailer.php
@@ -76,7 +76,7 @@ class Mailer
             'account' => $account
         ]);
 
-        $this->sendEmailMessage('Plataforma Sices Solar - Registro', $rendered, self::FROM_EMAIL, $account->getEmail());
+        $this->sendEmailMessage('Plataforma Sices Solar - Cadastro', $rendered, self::FROM_EMAIL, $account->getEmail());
 
         return $rendered;
     }
@@ -97,7 +97,7 @@ class Mailer
             'account' => $account
         ]);
 
-        $this->sendEmailMessage('Plataforma Sices Solar - Registro', $rendered, self::FROM_EMAIL, $account->getEmail());
+        $this->sendEmailMessage('Plataforma Sices Solar - Verificação', $rendered, self::FROM_EMAIL, $account->getEmail());
 
         return $rendered;
     }
