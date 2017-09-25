fbacbd9e4f1d413587882cc9fbd091d5b4650259
Claudinei M <cjchamado@gmail.com>
2017-09-25 09:18:17 -0300

    add unit tests: ensure correct user type and account context

--- backend/tests/AppBundle/Entity/AccountManagerTest.php
+++ backend/tests/AppBundle/Entity/AccountManagerTest.php
@@ -2,16 +2,114 @@
 
 namespace Tests\AppBundle\Entity;
 
-use Liip\FunctionalTestBundle\Test\WebTestCase;
+use AppBundle\Entity\Customer;
+use AppBundle\Entity\User;
+use AppBundle\Entity\UserInterface;
+use Tests\AppBundle\AppTestCase;
 
-class AccountManagerTest extends WebTestCase
+/**
+ * @group account
+ */
+class AccountManagerTest extends AppTestCase
 {
-    public function testFirstTesting()
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInvalidRoleAgentReference()
     {
-        $manager = $this->getContainer()->get('account_manager');
+        $manager = $this->manager('account');
 
         $account = $manager->create();
 
-        $this->assertNull($account->getId());
+        $account->setContext(Customer::CONTEXT_ACCOUNT); // valid account context
+
+        $member = $this->createMember();
+
+        $member->setContext(Customer::CONTEXT_MEMBER);
+
+        $account->setAgent($member);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInvalidContextAccountReference()
+    {
+        $manager = $this->manager('account');
+
+        $account = $manager->create();
+
+        $account->setContext(Customer::CONTEXT_COMPANY); // invalid account context
+
+        $member = $this->createMember();
+
+        $member->getUser()->addRole(UserInterface::ROLE_PLATFORM_ADMIN); // valid user role
+
+        $account->setAgent($member);
+    }
+
+    public function testSuccessAgentToAccountReference()
+    {
+        $account  = $this->createAccount();
+        $member = $this->createMember();
+
+        $member->setAccount($account);
+
+        $this->assertCount(1, $account->getMembers()->toArray());
+
+        $agent = $this->createMember();
+        $agent->getUser()->addRole(User::ROLE_PLATFORM_COMMERCIAL);
+
+        $account->setAgent($agent);
+
+        $this->assertNotNull($account->getAgent());
+    }
+
+    private function createMember()
+    {
+        $manager = $this->manager('customer');
+
+        $this->assertNotNull($manager);
+
+        $member = $manager->create();
+        $member->setContext(Customer::CONTEXT_MEMBER);
+
+        $user = $this->createUser();
+
+        $member->setUser($user);
+
+        return $member;
+    }
+
+    private function createUser()
+    {
+        $manager = $this->getContainer()->get('fos_user.user_manager');
+
+        /** @var UserInterface $user */
+        $user = $manager->createUser();
+
+        $user->addRole(UserInterface::ROLE_OWNER);
+
+        return $user;
+    }
+
+    /**
+     * @return mixed|object|Customer
+     */
+    private function createAccount()
+    {
+        $manager = $this->manager('account');
+
+        $account = $manager->create();
+
+        $account
+            ->setContext(Customer::CONTEXT_ACCOUNT)
+            ->setFirstname('Account')
+            ->setEmail('testaccount@testaccount.com')
+        ;
+
+        $manager->save($account);
+
+        return $account;
     }
 }
