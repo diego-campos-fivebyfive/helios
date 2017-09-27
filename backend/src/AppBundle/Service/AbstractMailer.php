7f5a909a4f84657548c31edfd6b777ea7c12a2df
Claudinei M <cjchamado@gmail.com>
2017-09-27 15:00:09 -0300

    add order status templates

---
+++ backend/src/AppBundle/Service/AbstractMailer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudinei
+ * Date: 27/09/17
+ * Time: 09:56
+ */
+
+namespace AppBundle\Service;
+
+
+class AbstractMailer
+{
+
+}
---
+++ backend/src/AppBundle/Service/Order/OrderMailer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudinei
+ * Date: 27/09/17
+ * Time: 09:54
+ */
+
+namespace AppBundle\Service\Order;
+
+
+class OrderMailer
+{
+
+}
