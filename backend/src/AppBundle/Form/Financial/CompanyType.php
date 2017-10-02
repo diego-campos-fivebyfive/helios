12a1a34fc0b38b4d549d44fde7ee4722033cb19d
joaozaqueu <joaozaqueuchereta@gmail.com>
2017-10-02 15:02:19 -0300

    ajuste de código no formtype de Company

--- backend/src/AppBundle/Form/Financial/CompanyType.php
+++ backend/src/AppBundle/Form/Financial/CompanyType.php
@@ -12,29 +12,13 @@ class CompanyType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name', TextType::class, [
-            'required' => false
-        ]);
-        $builder->add('contact', null, [
-            'required' => false
-        ]);
-        $builder->add('phone', null, [
-            'required' => false
-        ]);
-        $builder->add('email', EmailType::class, [
-            'required' => false
-        ]);
-        $builder->add('address', null, [
-            'required' => false
-        ]);
-        $builder->add('postcode', null, [
-            'required' => false
-        ]);
-        $builder->add('city', null, [
-            'required' => false
-        ]);
-        $builder->add('state', null, [
-            'required' => false
-        ]);
+        $builder->add('name', TextType::class, ['required' => false]);
+        $builder->add('contact', null, ['required' => false]);
+        $builder->add('phone', null, ['required' => false]);
+        $builder->add('email', EmailType::class, ['required' => false]);
+        $builder->add('address', null, ['required' => false]);
+        $builder->add('postcode', null, ['required' => false]);
+        $builder->add('city', null, ['required' => false]);
+        $builder->add('state', null, ['required' => false]);
     }
 }
