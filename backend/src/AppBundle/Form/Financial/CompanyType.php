9988de1674eaf11434c64f6dc26f2c087d26b20d
joaozaqueu <joaozaqueuchereta@gmail.com>
2017-10-02 14:33:54 -0300

    FormType com campos a serem salvos da transportadora

--- backend/src/AppBundle/Form/Financial/CompanyType.php
+++ backend/src/AppBundle/Form/Financial/CompanyType.php
@@ -1,15 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 02/10/17
- * Time: 08:41
- */
 
 namespace AppBundle\Form\Financial;
 
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
 
-class CompanyType
+class CompanyType extends AbstractType
 {
 
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', TextType::class, [
+            'required' => false
+        ]);
+        $builder->add('contact', null, [
+            'required' => false
+        ]);
+        $builder->add('phone', null, [
+            'required' => false
+        ]);
+        $builder->add('email', EmailType::class, [
+            'required' => false
+        ]);
+        $builder->add('address', null, [
+            'required' => false
+        ]);
+        $builder->add('postcode', null, [
+            'required' => false
+        ]);
+        $builder->add('city', null, [
+            'required' => false
+        ]);
+        $builder->add('state', null, [
+            'required' => false
+        ]);
+    }
 }
