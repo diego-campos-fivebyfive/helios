577a1efd6952605de3e013753a1b36711e9046d2
Rafael Kendrik <rafamikovski@gmail.com>
2017-10-04 16:51:03 -0300

    cleanning file logic

--- backend/src/AppBundle/Controller/ProposalController.php
+++ backend/src/AppBundle/Controller/ProposalController.php
@@ -22,7 +22,6 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
  */
 class ProposalController extends AbstractController
 {
-
     /**
      * @Route("/{id}/save", name="proposal_save")
      */
@@ -185,46 +184,38 @@ class ProposalController extends AbstractController
     {
         $theme = $this->resolveTheme($project);
 
-        $status = Response::HTTP_CONFLICT;
-        $filename = null;
-
-        if($theme) {
-
-            $snappy = $this->get('knp_snappy.pdf');
-            $snappy->setOption('viewport-size', '1280x1024');
-            $snappy->setOption('margin-top', 0);
-            $snappy->setOption('margin-bottom', 0);
-            $snappy->setOption('margin-left', 0);
-            $snappy->setOption('margin-right', 0);
-            $snappy->setOption('zoom', 2);
-
-            $dir = $this->get('kernel')->getRootDir() . '/../storage/';
-            $filename = md5(uniqid(time())) . '.pdf';
-            $file = $dir . $filename;
-
-            $url = $this->generateUrl('proposal_pdf', ['id' => $theme->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
-
-            try {
-
-                $snappy->generate($url, $file);
-
-                if (file_exists($file)) {
-                    $status = Response::HTTP_OK;
-
-                    if (!$project->getIssuedAt()) {
-                        $manager = $this->manager('project');
-                        $project->setIssuedAt(new \DateTime('now'));
-                        $manager->save($project);
-                    }
-                }
-
-            } catch (\Exception $error) {
-            }
+        if(!$theme) {
+            return $this->json([], Response::HTTP_NO_CONTENT);
         }
 
-        return $this->json([
-            'filename' => $filename
-        ], $status);
+        $snappy = $this->get('knp_snappy.pdf');
+        $snappy->setOption('viewport-size', '1280x1024');
+        $snappy->setOption('margin-top', 0);
+        $snappy->setOption('margin-bottom', 0);
+        $snappy->setOption('margin-left', 0);
+        $snappy->setOption('margin-right', 0);
+        $snappy->setOption('zoom', 2);
+
+        $PATH = "{$this->get('kernel')->getRootDir()}/../";
+        $tempFileName = "${md5(uniqid())}.pdf";
+        $tempFilePath = "{$PATH}/.uploads/{$tempFileName}";
+
+        $absoluteUrl = UrlGeneratorInterface::ABSOLUTE_URL;
+        $snappyUrl = $this->generateUrl('proposal_pdf', [ 'id' => $theme->getId() ], $absoluteUrl);
+
+        $snappy->generate($snappyUrl, $tempFilePath);
+
+        if (!file_exists($tempFilePath)) {
+            return $this->json([ 'filename' => $tempFileName ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        if (!$project->getIssuedAt()) {
+            $manager = $this->manager('project');
+            $project->setIssuedAt(new \DateTime('now'));
+            $manager->save($project);
+        }
+
+        return $this->json([ 'filename' => $tempFileName ], Response::HTTP_OK);
     }
 
     /**
