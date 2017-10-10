483ed39b517e5bb4a2b0323ff0d726c21149f647
Claudinei M <cjchamado@gmail.com>
2017-10-10 15:09:09 -0300

    remove null reference checker

---
+++ backend/src/AppBundle/Service/Order/OrderReference.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudinei
+ * Date: 10/10/17
+ * Time: 14:30
+ */
+
+namespace AppBundle\Service\Order;
+
+
+class OrderReference
+{
+
+}
---
+++ backend/tests/AppBundle/Service/Order/OrderReferenceTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudinei
+ * Date: 10/10/17
+ * Time: 14:24
+ */
+
+namespace Tests\AppBundle\Service\Order;
+
+
+class OrderReference
+{
+
+}
