b18fd53f77521f00788860c5060e530375c941d2
Claudinei M <cjchamado@gmail.com>
2017-10-10 19:09:36 -0300

    add like field for filter type: admin

--- backend/src/AdminBundle/Form/Order/FilterType.php
+++ backend/src/AdminBundle/Form/Order/FilterType.php
@@ -2,9 +2,21 @@
 
 namespace AdminBundle\Form\Order;
 
+use Symfony\Component\Form\FormBuilderInterface;
 use AppBundle\Form\Order\FilterType as AppFilterType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class FilterType extends AppFilterType
 {
+    /**
+     * @inheritDoc
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
 
+        $builder->add('like', TextType::class, [
+            'required' => false
+        ]);
+    }
 }
