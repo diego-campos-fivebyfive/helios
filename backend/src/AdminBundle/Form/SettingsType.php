d73872b93400bb276ee2e04b7c96fbc2c6e0359b
Fabio Dukievicz <fabiojd47@gmail.com>
2017-10-16 14:42:49 -0200

    nome e email para financeiro e data final de promocional

--- backend/src/AdminBundle/Form/SettingsType.php
+++ backend/src/AdminBundle/Form/SettingsType.php
@@ -3,7 +3,9 @@
 namespace AdminBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -36,8 +38,24 @@ class SettingsType extends AbstractType
                         ->create('admin', FormType::class)
                         ->add('name', TextType::class)
                         ->add('email', EmailType::class)
+                )->add(
+                    $builder
+                        ->create('financial', FormType::class)
+                        ->add('name', TextType::class)
+                        ->add('email', EmailType::class)
                 )
                 ->add('enable_promo', CheckboxType::class)
+                ->add('promo_end_at', TextType::class)
         );
+        $builder->get('parameters')
+            ->get('promo_end_at')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($endAt) {
+                    return (new \DateTime($endAt['date']))->format('d/m/Y');
+                },
+                function ($endAt) {
+                    return new \DateTime(implode('-', array_reverse(explode('/', $endAt))));
+                }
+            ));
     }
 }
