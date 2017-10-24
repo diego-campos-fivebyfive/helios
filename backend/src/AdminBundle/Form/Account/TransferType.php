5cce0f979957cbe084ecd8b8e9938205c72c3c8b
Fabio Dukievicz <fabiojd47@gmail.com>
2017-10-24 17:25:10 -0200

    form de tranferencia de conta

--- backend/src/AdminBundle/Form/Account/TransferType.php
+++ backend/src/AdminBundle/Form/Account/TransferType.php
@@ -1,12 +1,39 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 24/10/17
- * Time: 15:14
- */
-class TransferType
-{
+namespace AdminBundle\Form\Account;
 
+use AppBundle\Entity\Customer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use AppBundle\Entity\MemberInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class TransferType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $account = $options['account'];
+
+        $members = $account->getMembers()->filter(function (MemberInterface $member){
+            return $member->isPlatformCommercial();
+        });
+
+        $builder
+            ->add('source', EntityType::class,[
+                'choices' => $members,
+                'class' => 'AppBundle:Customer'
+            ])
+            ->add('target', EntityType::class,[
+                'choices' => $members,
+                'class' => 'AppBundle:Customer'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'account' => null
+        ));
+    }
 }
