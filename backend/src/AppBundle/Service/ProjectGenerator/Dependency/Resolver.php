f0f04da2de8eff539f1c467d3c7a86f4716dc8bb
Claudinei M <cjchamado@gmail.com>
2017-10-25 16:07:04 -0200

    add mapping resolver

--- backend/src/AppBundle/Service/ProjectGenerator/Dependency/Resolver.php
+++ backend/src/AppBundle/Service/ProjectGenerator/Dependency/Resolver.php
@@ -1,15 +1,55 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudinei
- * Date: 25/10/17
- * Time: 15:06
- */
 
 namespace AppBundle\Service\ProjectGenerator\Dependency;
 
-
+/**
+ * Class Resolver
+ * @package AppBundle\Service\ProjectGenerator\Dependency
+ */
 class Resolver
 {
+    /**
+     * @var Loader
+     */
+    private $loader;
 
+    /**
+     * @var Accumulator
+     */
+    private $accumulator;
+
+    /**
+     * Resolver constructor.
+     * @param Loader $loader
+     */
+    function __construct(Loader $loader)
+    {
+        $this->loader = $loader;
+        $this->accumulator = Accumulator::create();
+    }
+
+    /**
+     * @param array $mapping
+     * @return array
+     */
+    public function fromMapping(array $mapping)
+    {
+        $types = [];
+        foreach ($mapping as $config){
+            if(null != $component = $this->loader->load($config['id'], $config['type'])){
+                $this->accumulator->add($component, $config['quantity']);
+                $types[$config['type']] = $this->accumulator->get($config['type']);
+            }
+        }
+
+        return $types;
+    }
+
+    /**
+     * @return Resolver
+     */
+    public static function create(Loader $loader)
+    {
+        return new self($loader);
+    }
 }
