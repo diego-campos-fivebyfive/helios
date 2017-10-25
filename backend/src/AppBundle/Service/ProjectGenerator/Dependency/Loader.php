6dc118744dbb7865035ba8bfa1f8c31e2bbc4b16
Claudinei M <cjchamado@gmail.com>
2017-10-25 16:06:43 -0200

    add component loader class

--- backend/src/AppBundle/Service/ProjectGenerator/Dependency/Loader.php
+++ backend/src/AppBundle/Service/ProjectGenerator/Dependency/Loader.php
@@ -1,15 +1,70 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudinei
- * Date: 25/10/17
- * Time: 14:52
- */
 
 namespace AppBundle\Service\ProjectGenerator\Dependency;
 
+use AppBundle\Manager\AbstractManager;
+use AppBundle\Entity\Component\ComponentInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class Resolver
+class Loader
 {
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
 
+    /**
+     * @var array
+     */
+    private $managers = [];
+
+    /**
+     * @var array
+     */
+    private $components = [];
+
+    /**
+     * Loader constructor.
+     * @param ContainerInterface $container
+     */
+    function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param $id
+     * @param $type
+     * @return null|ComponentInterface
+     */
+    public function load($id, $type)
+    {
+        $manager = $this->manager($type);
+
+        if(!array_key_exists($type, $this->components))
+            $this->components[$type] = [];
+
+        if(!array_key_exists($id, $this->components[$type])){
+
+            $component = $manager->find($id);
+
+            if($component instanceof ComponentInterface) {
+                $this->components[$type][$id] = $component;
+            }
+        }
+
+        return array_key_exists($id, $this->components[$type])  ? $this->components[$type][$id] : null ;
+    }
+
+    /**
+     * @param $type
+     * @return AbstractManager|object
+     */
+    private function manager($type)
+    {
+        if(!array_key_exists($type, $this->managers))
+            $this->managers[$type] = $this->container->get(sprintf('%s_manager', $type));
+
+        return $this->managers[$type];
+    }
 }
