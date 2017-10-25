458289d17d09bd63d46cbd34d71b8e8ce4a8295c
joaozaqueu <joaozaqueuchereta@gmail.com>
2017-10-25 08:16:53 -0200

    pré implementação do serviço de transferencia de contas

--- backend/src/AppBundle/Service/Util/AccountTransfer.php
+++ backend/src/AppBundle/Service/Util/AccountTransfer.php
@@ -1,15 +1,43 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 24/10/17
- * Time: 17:14
- */
 
 namespace AppBundle\Service\Util;
 
+use AppBundle\Entity\AccountInterface;
+use AppBundle\Manager\AccountManager;
+use AppBundle\Manager\CustomerManager;
 
 class AccountTransfer
 {
+
+    /**
+     * @var AccountManager
+     */
+    private $manager;
+
+    function __construct(CustomerManager $customerManager)
+    {
+        $this->customerManager = $customerManager;
+    }
+
+    public function transfer($source, $target)
+    {
+        /** @var AccountInterface $accountManager */
+        $accountManager = $this->customerManager;
+
+        /** @var AccountInterface $accounts */
+        $accounts = $accountManager->findBy([
+            'context' => 'account',
+            'agent' => $source
+        ]);
+
+        foreach ($accounts as $account) {
+
+            if ($account->getAgent() == $source) {
+                $account->setAgent($target);
+
+                $accountManager->save($account);
+            }
+        }
+    }
 
 }
