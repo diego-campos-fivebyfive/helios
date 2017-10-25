6afef12c5bef67af62b3506e35c367126e7432a0
Claudinei M <cjchamado@gmail.com>
2017-10-25 14:18:29 -0200

    project dependency: add accumulator unit test

--- backend/tests/AppBundle/Service/ProjectGenerator/Dependency/AccumulatorTest.php
+++ backend/tests/AppBundle/Service/ProjectGenerator/Dependency/AccumulatorTest.php
@@ -3,15 +3,16 @@
 namespace Tests\AppBundle\Service\ProjectGenerator\Dependency;
 
 use AppBundle\Entity\Component\VarietyInterface;
+use AppBundle\Service\ProjectGenerator\Dependency\Accumulator;
 use Tests\AppBundle\AppTestCase;
 use Tests\AppBundle\Helpers\ObjectHelperTest;
 
 /**
  * Class CollectionTest
  * @group project_generator
- * @group dependency_collection
+ * @group dependency_accumulator
  */
-class CollectionTest extends AppTestCase
+class AccumulatorTest extends AppTestCase
 {
     use ObjectHelperTest;
 
@@ -19,9 +20,37 @@ class CollectionTest extends AppTestCase
     {
         $varieties = $this->createVarieties(10);
 
-        
+        /**
+         * [index, quantity]
+         */
+        $indexToQuantityMapping = [
+            [1, 1],
+            [2, 3],
+            [1, 2],
+            [5, 5],
+            [1, 5]
+        ];
 
-        //$collection new Collection();
+        $accumulator = Accumulator::create();
+        $data = [];
+        foreach ($indexToQuantityMapping as $mapping){
+            $variety = $varieties[$mapping[0]];
+            $accumulator->add($variety, $mapping[1]);
+
+            if(!array_key_exists($variety->getId(), $data))
+                $data[$variety->getId()] = 0;
+
+            $data[$variety->getId()] += $mapping[1];
+        }
+
+        foreach ($accumulator->get(Accumulator::VARIETY) as $item){
+            $this->assertEquals($data[$item['component']->getId()], $item['quantity']);
+        }
+
+        $items = $accumulator->get(Accumulator::VARIETY);
+        $item = $items[$varieties[1]->getId()];
+
+        $this->assertEquals(8, $item['quantity']);
     }
 
     /**
