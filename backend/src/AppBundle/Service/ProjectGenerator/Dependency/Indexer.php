4c45dbbb6afd77ab2876932cecc8ff5fc2205207
Claudinei M <cjchamado@gmail.com>
2017-10-26 14:55:16 -0200

    add abstract indexer class

--- backend/src/AppBundle/Service/ProjectGenerator/Dependency/Indexer.php
+++ backend/src/AppBundle/Service/ProjectGenerator/Dependency/Indexer.php
@@ -1,15 +1,46 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudinei
- * Date: 26/10/17
- * Time: 14:33
+
+/*
+ * This file is part of the SicesSolar package.
+ *
+ * (c) SicesSolar <http://sicesbrasil.com.br/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace AppBundle\Service\ProjectGenerator\Dependency;
 
-
-class Indexer
+/**
+ * Class Indexer
+ * This class indexes dependencies by solving repetitions and grouping by types
+ *
+ * @author Claudinei Machado <claudinei@kolinalabs.com>
+ */
+abstract class Indexer
 {
+    /**
+     * @param array $dependencies
+     * @return array
+     */
+    public static function process(array $dependencies)
+    {
+        $output = [];
+        foreach ($dependencies as $dependency){
 
+            $type = $dependency['type'];
+            $id = $dependency['id'];
+
+            if(!array_key_exists($type, $output))
+                $output[$type] = [];
+
+            if(!array_key_exists($id, $output[$type])) {
+                $output[$type][$id] = $dependency['ratio'];
+            }else{
+                $output[$type][$id] += $dependency['ratio'];
+            }
+        }
+
+        return $output;
+    }
 }
