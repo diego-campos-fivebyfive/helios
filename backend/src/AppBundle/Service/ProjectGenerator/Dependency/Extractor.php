ad5a62cba13c5fb58a1613c4943df8c5ba787d4e
Claudinei M <cjchamado@gmail.com>
2017-10-26 18:05:43 -0200

    add extractor class unit tests

---
+++ backend/src/AppBundle/Service/ProjectGenerator/Dependency/Extractor.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudinei
+ * Date: 26/10/17
+ * Time: 15:00
+ */
+
+namespace AppBundle\Service\ProjectGenerator\Dependency;
+
+
+class Extractor
+{
+
+}
