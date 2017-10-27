a1697be550326af05af9d3680c8b170b439f489e
Claudinei M <cjchamado@gmail.com>
2017-10-27 16:39:48 -0200

    add component dependency management views

---
+++ backend/src/AdminBundle/Controller/ComponentDependencyController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudinei
+ * Date: 27/10/17
+ * Time: 12:24
+ */
+
+namespace AdminBundle\Controller;
+
+
+class ComponentDependencyController
+{
+
+}
---
+++ backend/src/AdminBundle/Form/Component/DependencyType.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudinei
+ * Date: 27/10/17
+ * Time: 12:30
+ */
+
+namespace AdminBundle\Form\Component;
+
+
+class DependencyType
+{
+
+}
