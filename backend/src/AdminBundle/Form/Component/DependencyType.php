ab9aaea888bb763d7b6eb1cf0970ed9f05c9269e
Claudinei M <cjchamado@gmail.com>
2017-10-27 16:40:30 -0200

    add component dependency form type

--- backend/src/AdminBundle/Form/Component/DependencyType.php
+++ backend/src/AdminBundle/Form/Component/DependencyType.php
@@ -1,15 +1,78 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudinei
- * Date: 27/10/17
- * Time: 12:30
- */
 
 namespace AdminBundle\Form\Component;
 
+use AppBundle\Entity\Component\ComponentInterface;
+use AppBundle\Entity\Component\Variety;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class DependencyType
+class DependencyType extends AbstractType
 {
+    /**
+     * @inheritDoc
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $component = $options['component'];
+        $dependencyIds = $this->filterDependencyIds($component->getDependencies());
 
+        $builder
+            ->add('type', HiddenType::class, [
+                'data' => 'variety'
+            ])
+            ->add('id', EntityType::class, [
+                'class' => Variety::class,
+                'group_by' => 'type',
+                'query_builder' => function(EntityRepository $er) use($dependencyIds){
+
+                    $qb = $er->createQueryBuilder('c');
+
+                    if(!empty($dependencyIds)) {
+                        $qb->andWhere($qb->expr()->notIn('c.id', $dependencyIds));
+                    }
+
+                    return $qb;
+                }
+            ])
+            ->add('ratio')
+        ;
+
+        $builder->get('id')->addModelTransformer(
+            new CallbackTransformer(
+                function($value){
+                    return $value;
+                },
+                function(ComponentInterface $component){
+                    return $component->getId();
+                }
+            )
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'component' => null
+        ]);
+    }
+
+    /**
+     * @param array $dependencies
+     * @return array
+     */
+    private function filterDependencyIds(array $dependencies)
+    {
+        return array_map(function($dependency){
+            return $dependency['id'];
+        }, $dependencies);
+    }
 }
