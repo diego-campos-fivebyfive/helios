33282d95dc30810cee6a96c84a746748afc0bbb4
Claudinei M <cjchamado@gmail.com>
2017-10-27 10:39:15 -0200

    dependency resolver class

--- backend/src/AppBundle/Service/ProjectGenerator/Dependency/Resolver.php
+++ backend/src/AppBundle/Service/ProjectGenerator/Dependency/Resolver.php
@@ -1,10 +1,31 @@
 <?php
 
+/*
+ * This file is part of the SicesSolar package.
+ *
+ * (c) SicesSolar <http://sicesbrasil.com.br/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace AppBundle\Service\ProjectGenerator\Dependency;
+
+use AppBundle\Entity\Component\ProjectVariety;
+use AppBundle\Entity\Component\ComponentInterface;
+use AppBundle\Entity\Component\ProjectInterface;
+use Doctrine\Common\Inflector\Inflector;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\PropertyAccess\PropertyAccess;
 
 /**
  * Class Resolver
- * @package AppBundle\Service\ProjectGenerator\Dependency
+ * This class resolve dependencies from a project,
+ * - Check promotional
+ * - Resolve accumulated relationships
+ * - Creates new dependency associations
+ *
+ * @author Claudinei Machado <claudinei@kolinalabs.com>
  */
 class Resolver
 {
@@ -14,42 +35,80 @@ class Resolver
     private $loader;
 
     /**
-     * @var Accumulator
+     * @var bool
      */
-    private $accumulator;
+    private $strictPromotional = true;
 
     /**
      * Resolver constructor.
      * @param Loader $loader
      */
-    function __construct(Loader $loader)
+    function __construct(ContainerInterface $container)
     {
-        $this->loader = $loader;
-        $this->accumulator = Accumulator::create();
+        $this->loader = Loader::create($container);
     }
 
     /**
-     * @param array $mapping
-     * @return array
+     * @param ProjectInterface $project
      */
-    public function fromMapping(array $mapping)
+    public function resolve(ProjectInterface $project)
     {
-        $types = [];
-        foreach ($mapping as $config){
-            if(null != $component = $this->loader->load($config['id'], $config['type'])){
-                $this->accumulator->add($component, $config['quantity']);
-                $types[$config['type']] = $this->accumulator->get($config['type']);
+        $dependencies = Extractor::create()->fromProject($project);
+        $collection = $this->loader->load(self::normalize($dependencies));
+        $checkPromotional = $project->isPromotional() && $this->strictPromotional;
+
+        $accessor = PropertyAccess::createPropertyAccessor();
+
+        foreach ($collection as $type => $components){
+
+            /** @var ComponentInterface $component */
+            foreach ($components as $component) {
+
+                if($checkPromotional && !$component->isPromotional()) continue;
+
+                $quantity = $dependencies[$type][$component->getId()];
+
+                $id = $component->getId();
+                $related = sprintf('project' . ucfirst($type));
+                $getter = Inflector::pluralize($related);
+
+                $projectComponent = $accessor->getValue($project, $getter)->filter(function($related) use($id, $type, $accessor){
+                    return $id === $accessor->getValue($related, $type)->getId();
+                })->first();
+
+                if($projectComponent){
+                    $quantity = $accessor->getValue($projectComponent, 'quantity') + $quantity;
+                }else {
+
+                    $projectComponent = new ProjectVariety();
+                    $accessor->setValue($projectComponent, 'project', $project);
+                    $accessor->setValue($projectComponent, $type, $component);
+                }
+
+                $accessor->setValue($projectComponent, 'quantity', $quantity);
             }
         }
+    }
 
-        return $types;
+    /**
+     * @param array $extracted
+     * @return array
+     */
+    private static function normalize(array $extracted = [])
+    {
+        $normalized = [];
+        foreach ($extracted as $type => $dependencies) {
+            $normalized[$type] = array_keys($dependencies);
+        }
+
+        return $normalized;
     }
 
     /**
      * @return Resolver
      */
-    public static function create(Loader $loader)
+    public static function create(ContainerInterface $container)
     {
-        return new self($loader);
+        return new self($container);
     }
 }
