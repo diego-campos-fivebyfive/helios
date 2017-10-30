7dd68dfacc27720554047f72e31106c058507cce
Claudinei M <cjchamado@gmail.com>
2017-10-30 14:16:31 -0200

    update app test case: add structure data fixture

--- backend/tests/AppBundle/AppTestCase.php
+++ backend/tests/AppBundle/AppTestCase.php
@@ -31,6 +31,7 @@ class AppTestCase extends WebTestCase
             Fixtures\Component\InverterData::class,
             Fixtures\Component\StringBoxData::class,
             Fixtures\Component\VarietyData::class,
+            Fixtures\Component\StructureData::class,
             Fixtures\LoadProjectData::class,
             Fixtures\LoadProjectModuleData::class,
             Fixtures\LoadProjectInverterData::class,
