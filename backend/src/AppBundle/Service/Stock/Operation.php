1d2b96b5eca26bbe4bbe14e37adc45d495722c4a
Claudinei M <cjchamado@gmail.com>
2017-10-31 14:00:46 -0200

    add operation class

--- backend/src/AppBundle/Service/Stock/Operation.php
+++ backend/src/AppBundle/Service/Stock/Operation.php
@@ -1,15 +1,62 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudinei
- * Date: 31/10/17
- * Time: 13:44
- */
 
 namespace AppBundle\Service\Stock;
 
+use AppBundle\Entity\Stock\ProductInterface;
 
+/**
+ * Class Operation
+ * @package AppBundle\Service\Stock
+ */
 class Operation
 {
+    /**
+     * @var ProductInterface
+     */
+    private $product;
 
+    /**
+     * @var int
+     */
+    private $amount;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * Operation constructor.
+     * @param ProductInterface $product
+     * @param $amount
+     * @param $description
+     */
+    function __construct(ProductInterface $product, $amount, $description)
+    {
+        if(!is_int($amount)) throw new \InvalidArgumentException('Invalid amount value type');
+
+        $this->product = $product;
+        $this->amount = $amount;
+        $this->description = $description;
+    }
+
+    /**
+     * @param $name
+     * @return mixed|string|object|int
+     */
+    function __get($name)
+    {
+        return $this->$name;
+    }
+
+    /**
+     * @param ProductInterface $product
+     * @param $amount
+     * @param $description
+     * @return Operation
+     */
+    public static function create(ProductInterface $product, $amount, $description)
+    {
+        return new self($product, $amount, $description);
+    }
 }
