27a952f40347ff94bf28734ae00366c792aa24a2
Claudinei M <cjchamado@gmail.com>
2017-11-01 11:57:11 -0200

    update component stock docblock

--- backend/src/AppBundle/Service/Stock/Component.php
+++ backend/src/AppBundle/Service/Stock/Component.php
@@ -1,11 +1,26 @@
 <?php
 
+/*
+ * This file is part of the SicesSolar package.
+ *
+ * (c) SicesSolar <http://sicesbrasil.com.br/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace AppBundle\Service\Stock;
 
 use AppBundle\Entity\Component\ComponentInterface;
 use AppBundle\Entity\Stock\ProductInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
+/**
+ * Class Component
+ * This class process stock transactions for components
+ *
+ * @author Claudinei Machado <claudinei@kolinalabs.com>
+ */
 class Component
 {
     /**
