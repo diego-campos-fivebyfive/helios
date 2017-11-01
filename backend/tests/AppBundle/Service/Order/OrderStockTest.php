9b318eda86655b827e82058f3f5d408ccb44a3d9
Claudinei M <cjchamado@gmail.com>
2017-11-01 11:54:51 -0200

    update order stock unit tests: test via process

--- backend/tests/AppBundle/Service/Order/OrderStockTest.php
+++ backend/tests/AppBundle/Service/Order/OrderStockTest.php
@@ -5,6 +5,7 @@ namespace Tests\AppBundle\Service\Order;
 use AppBundle\Entity\Order\Element;
 use AppBundle\Service\Order\ElementResolver;
 use AppBundle\Service\Order\OrderStock;
+use AppBundle\Service\Stock\Identity;
 use Tests\AppBundle\AppTestCase;
 use Tests\AppBundle\Helpers\ObjectHelperTest;
 
@@ -16,31 +17,67 @@ class OrderStockTest extends AppTestCase
 {
     use ObjectHelperTest;
 
-    public function testDebitElements()
+    /**
+     * Component transactions via Order instance
+     */
+    public function testTransactElements()
     {
         $manager = $this->manager('order');
         $order = $manager->create();
-        $components = $this->createComponents();
+        $components = $this->createComponents(25);
+        $stocks = [];
 
-        foreach ($components as $component){
+        foreach ($components as $key => $component){
 
             $element = new Element();
 
             ElementResolver::resolve($element, $component);
 
+            // Determine identity
+            $identity = Identity::create($component);
+            if(!array_key_exists($identity, $stocks))
+                $stocks[$identity] = 0;
+
+            $amount = ($key + 1);
+
+            $element->setQuantity($amount);
             $element->setOrder($order);
+
+            $stocks[$identity] += $amount;
         }
+
+        $manager->save($order);
+
+        $this->service('order_reference')->generate($order);
 
         // Test elements is added
         $this->assertCount(count($components), $order->getElements()->toArray());
 
-        //$orderStock = $this->service('order_stock');
-        $orderStock = new OrderStock(
-            $this->service('component_collector'),
-            $this->service('stock_converter')
-        );
+        $orderStock = $this->service('order_stock');
 
+        // Check stock 0
+        foreach ($components as $key => $component){
+            $this->assertEquals(0, $component->getStock());
+        }
+
+        // Process debit
         $orderStock->debit($order);
+
+        // Check stock updated
+        foreach ($components as $key => $component){
+            // Determine identity
+            $identity = Identity::create($component);
+            // Check stock
+            $this->assertEquals(($stocks[$identity] * -1), $component->getStock());
+        }
+
+        // Process credit
+        $orderStock->credit($order);
+
+        // Check stock return to 0
+        foreach ($components as $key => $component){
+            $this->assertEquals(0, $component->getStock());
+        }
     }
 
     /**
@@ -50,26 +87,37 @@ class OrderStockTest extends AppTestCase
     private function createComponents($count = 10)
     {
         $components = [];
-        $families = ['inverter', 'module', 'stringBox', 'structure', 'variety'];
+
+        $families = [
+            'inverter' => true,
+            'module' => false,
+            'stringBox' => false,
+            'structure' => false,
+            'variety' => false
+        ];
 
         $fixturesNamespace = 'Tests\\AppBundle\\Entity\\DataFixtures\\Component\\%sData';
 
-        foreach ($families as $family){
+        foreach ($families as $family => $enabled){
 
-            $manager = $this->manager($family);
-            $fixtureClass = sprintf($fixturesNamespace, ucfirst($family));
+            if($enabled) {
+                $manager = $this->manager($family);
+                $fixtureClass = sprintf($fixturesNamespace, ucfirst($family));
 
-            for ($i = 0; $i < $count; $i++) {
+                for ($i = 0; $i < $count; $i++) {
 
-                $component = $manager->create();
+                    $component = $manager->create();
 
-                $data = $fixtureClass::getData();
+                    $data = $fixtureClass::getData();
 
-                self::fluentSetters($component, $data);
+                    self::fluentSetters($component, $data);
 
-                $manager->save($component);
+                    $component->setCode(md5(uniqid(time())));
 
-                $components[] = $component;
+                    $manager->save($component);
+
+                    $components[] = $component;
+                }
             }
         }
 
