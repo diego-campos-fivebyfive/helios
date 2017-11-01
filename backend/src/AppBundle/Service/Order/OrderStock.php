8ba77a49c62e1b8b48ac5bdb114fba7c7c77ec89
Claudinei M <cjchamado@gmail.com>
2017-11-01 11:54:08 -0200

    update order stock service: add process option

--- backend/src/AppBundle/Service/Order/OrderStock.php
+++ backend/src/AppBundle/Service/Order/OrderStock.php
@@ -1,55 +1,105 @@
 <?php
 
+/*
+ * This file is part of the SicesSolar package.
+ *
+ * (c) SicesSolar <http://sicesbrasil.com.br/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace AppBundle\Service\Order;
 
 use AppBundle\Entity\Order\OrderInterface;
-use AppBundle\Service\Stock\Converter;
-use AppBundle\Service\Stock\Identity;
-use Doctrine\ORM\EntityManagerInterface;
+use AppBundle\Service\Stock\Component as Stock;
 
+/**
+ * Class OrderStock
+ * This class processes incoming and outgoing inventory for orders
+ *
+ * @author Claudinei Machado <claudinei@kolinalabs.com>
+ */
 class OrderStock
 {
+    const MODE_DEBIT = -1;
+    const MODE_CREDIT = 1;
+    const MODE_STATUS = 0;
+
     /**
      * @var ComponentCollector
      */
     private $collector;
+
     /**
-     * @var Converter
+     * @var Stock
      */
-    private $converter;
+    private $stock;
 
     /**
      * OrderStock constructor.
      * @param ComponentCollector $collector
+     * @param Stock $stock
      */
-    function __construct(ComponentCollector $collector, Converter $converter)
+    function __construct(ComponentCollector $collector, Stock $stock)
     {
         $this->collector = $collector;
-        $this->converter = $converter;
+        $this->stock = $stock;
     }
 
+    /**
+     * @param OrderInterface $order
+     * @param $mode
+     */
+    public function process(OrderInterface $order, $mode)
+    {
+        $transactions = $this->mappingTransactions($order, $mode);
+
+        $this->stock->transact($transactions);
+    }
+
+    /**
+     * @param OrderInterface $order
+     */
+    public function credit(OrderInterface $order)
+    {
+        $this->process($order, self::MODE_CREDIT);
+    }
+
+    /**
+     * @param OrderInterface $order
+     */
     public function debit(OrderInterface $order)
     {
-        $components = $this->mappingComponents($order);
-        $products = $this->converter->transform($components);
-
-        dump($components); die;
+        $this->process($order, self::MODE_DEBIT);
     }
 
     /**
      * @param OrderInterface $order
      * @return array
      */
-    private function mappingComponents(OrderInterface $order)
+    private function mappingTransactions(OrderInterface $order, $mode)
     {
-        $components = [];
+        $description = sprintf(
+            'Orçamento %s - %s',
+            $order->getReference(),
+            (new \DateTime())->format('d/m/Y H:i')
+        );
+
+        $transactions = [];
         foreach ($order->getElements() as $element){
+
             $component = $this->collector->fromCode($element->getCode());
+
             if($component) {
-                $components[] = $component;
+                $transactions[] = [
+                    'component' =>  $component,
+                    'amount' => ($element->getQuantity() * $mode),
+                    'description' => $description
+                ];
             }
         }
 
-        return $components;
+        return $transactions;
     }
 }
