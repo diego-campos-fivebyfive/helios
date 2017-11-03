a52c60240da366635051cde15217738c86029dcd
joaozaqueu <joaozaqueuchereta@gmail.com>
2017-11-03 17:27:30 -0200

    Form de transações de entrada e saida de estoque

--- backend/src/AdminBundle/Form/Stock/TransactionType.php
+++ backend/src/AdminBundle/Form/Stock/TransactionType.php
@@ -1,15 +1,31 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 03/11/17
- * Time: 13:38
- */
 
 namespace AdminBundle\Form\Stock;
 
+use AppBundle\Entity\Stock\Transaction;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class TransactionType
+class TransactionType extends AbstractType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('description');
+        $builder->add('amount');
+        $builder->add('mode', ChoiceType::class, [
+            'choices' => [
+                '1' => 'credit',
+                '-1' => 'debit'
+            ]
+        ]);
 
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array( ));
+    }
 }
