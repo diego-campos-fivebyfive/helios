e1d2c9e1e5b9cf49b357673e10dfc909686600b3
Fabio Dukievicz <fabiojd47@gmail.com>
2017-11-03 16:15:09 -0200

    rotas para gestao de estoque

---
+++ backend/src/AdminBundle/Controller/StockController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace AdminBundle\Controller;
+
+use AppBundle\Entity\Component\Structure;
+use APY\BreadcrumbTrailBundle\Annotation\Breadcrumb;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Security("has_role('ROLE_PLATFORM_ADMIN')")
+ *
+ * @Route("stock")
+ * @Breadcrumb("Gestão de estoque")
+ */
+class StockController extends AbstractController
+{
+    /**
+     * @Route("/", name="stock")
+     */
+    public function stockAction()
+    {
+
+        return $this->render('admin/stock/index.html.twig');
+    }
+
+    /**
+     * @Route("/components", name="components_stock")
+     */
+    public function componentsAction(Request $request)
+    {
+        // TODO: codigo para teste
+        $manager = $this->manager('structure');
+        $paginator = $this->getPaginator();
+
+        $qb = $manager->getEntityManager()->createQueryBuilder();
+        $qb->select('s')
+            ->from(Structure::class, 's')
+            ->leftJoin('s.maker', 'm', 'WITH')
+            ->orderBy('m.name', 'asc')
+            ->addOrderBy('s.description', 'asc');
+
+        if(!$this->user()->isAdmin() && !$this->user()->isPlatformMaster()) {
+            $qb->where('s.status = :status');
+            $qb->andWhere('s.available = :available');
+            $qb->setParameters([
+                'status' => 1,
+                'available' => 1
+            ]);
+        }
+
+        $this->overrideGetFilters();
+
+        $pagination = $paginator->paginate(
+            $qb->getQuery(),
+            $request->query->getInt('page', 1),
+            'grid' == $request->query->get('display', 'grid') ? 8 : 10,
+            ['distinct' => false]
+        );
+
+        return $this->render('admin/stock/components_content.html.twig',[
+            'pagination' => $pagination
+        ]);
+    }
+
+    /**
+     * @Route("/{component}/transaction", name="transaction_stock")
+     */
+    public function transactionAction($component)
+    {
+        // TODO: adaptar para pegar a familia do componente
+        $manager = $this->manager('structure');
+
+        $component = $manager->find($component);
+
+        return $this->render('admin/stock/transaction.html.twig',[
+            'component' => $component
+        ]);
+    }
+
+    /**
+     * @Route("/transaction/{component}/list", name="list_transactions_stock")
+     */
+    public function listTransactionAction(Request $request, $component)
+    {
+        // TODO: codigo de teste
+        $manager = $this->manager('structure');
+
+        $paginator = $this->getPaginator();
+
+        $qb = $manager->getEntityManager()->createQueryBuilder();
+        $qb->select('s')
+            ->from(Structure::class, 's')
+            ->leftJoin('s.maker', 'm', 'WITH')
+            ->orderBy('m.name', 'asc')
+            ->addOrderBy('s.description', 'asc');
+
+        if(!$this->user()->isAdmin() && !$this->user()->isPlatformMaster()) {
+            $qb->where('s.status = :status');
+            $qb->andWhere('s.available = :available');
+            $qb->setParameters([
+                'status' => 1,
+                'available' => 1
+            ]);
+        }
+
+        $this->overrideGetFilters();
+
+        $pagination = $paginator->paginate(
+            $qb->getQuery(),
+            $request->query->getInt('page', 1),
+            'grid' == $request->query->get('display', 'grid') ? 8 : 10,
+            ['distinct' => false]
+        );
+
+        return $this->render('admin/stock/transaction_content.html.twig',[
+            'pagination' => $pagination
+        ]);
+    }
+}
