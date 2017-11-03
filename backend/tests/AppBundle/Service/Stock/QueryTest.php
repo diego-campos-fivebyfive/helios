864b3b7e0964dfd4055c01175098f8ee8149d59b
Claudinei M <cjchamado@gmail.com>
2017-11-03 09:41:33 -0200

    add transaction query service unit tests

--- backend/tests/AppBundle/Service/Stock/QueryTest.php
+++ backend/tests/AppBundle/Service/Stock/QueryTest.php
@@ -3,47 +3,162 @@
 namespace Tests\AppBundle\Service\Stock;
 
 use AppBundle\Entity\Stock\ProductInterface;
+use AppBundle\Entity\Stock\Transaction;
 use AppBundle\Service\Stock\Operation;
 use AppBundle\Service\Stock\Provider;
-use AppBundle\Service\Stock\QueryTransaction;
-use Doctrine\ORM\Query;
+use AppBundle\Service\Stock\Query;
+use Doctrine\ORM\Query as DoctrineQuery;
 use Doctrine\ORM\QueryBuilder;
+use Knp\Bundle\PaginatorBundle\Pagination\SlidingPagination;
 use Tests\AppBundle\AppTestCase;
 use Tests\AppBundle\Helpers\ObjectHelperTest;
 
 /**
- * Class QueryTransactionTest
+ * Class QueryTest
  * @group stock
- * @group stock_query_transaction
+ * @group stock_query
  */
-class QueryTransactionTest extends AppTestCase
+class QueryTest extends AppTestCase
 {
     use ObjectHelperTest;
 
-    public function testTransactions()
+    /**
+     * Test return types
+     */
+    public function testReturnInstancesOrTypes()
     {
         $products = $this->createProducts();
         $product = $products[0];
-        $this->createTransactions($product);
 
-        $query = new QueryTransaction($this->getProvider());
+        $this->createTransactions($product, 25);
 
-        $query->between(
-            new \DateTime('-1 month'),
-            new \DateTime()
-        );
+        $query = new Query($this->getProvider());
 
-        $output = $query->get('qb');
+        $output = $query->qb();
         $this->assertInstanceOf(QueryBuilder::class, $output);
 
-        $output = $query->get('query');
-        $this->assertInstanceOf(Query::class, $output);
+        $output = $query->query();
+        $this->assertInstanceOf(DoctrineQuery::class, $output);
 
-        $output = $query->get('result');
+        $output = $query->result();
         $this->assertArrayHasKey(0, $output);
         $this->assertEquals(count($output), $product->getTransactions()->count());
 
+        $output = $query->pagination();
+        $this->assertInstanceOf(SlidingPagination::class, $output);
 
+        $output = $query->count();
+        $this->assertEquals(25, $output);
+    }
+
+    /**
+     * Test return based product
+     */
+    public function testReturnByProduct()
+    {
+        $products = $this->createProducts();
+
+        $count = 20;
+        $product = $products[0];
+        $productWithoutTransactions = $products[1];
+
+        $this->createTransactions($product, $count);
+
+        $query = new Query($this->getProvider());
+
+        $query->product($product);
+        $this->assertCount($count, $query->get('result'));
+
+        $query->product($productWithoutTransactions);
+        $this->assertCount(0, $query->get('result'));
+    }
+
+    /**
+     * Test return based transaction dates
+     */
+    public function testReturnByDateIntervals()
+    {
+        $products = $this->createProducts();
+
+        $count = 10;
+        $product = $products[0];
+
+        $this->createTransactions($product, $count);
+
+        $manager = $this->manager('stock_product');
+
+        $connection = $manager->getConnection();
+        $table = $manager->getObjectManager()->getClassMetadata(Transaction::class)->table['name'];
+
+        $date = new \DateTime('-1 month');
+        $days = 2;
+
+        /** @var Transaction $transaction */
+        foreach ($product->getTransactions() as $key => $transaction){
+            if(0 == $key % 2) {
+
+                $interval = new \DateInterval(sprintf('P%sD', $days));
+                $date->add($interval);
+
+                $dateFormat = $date->format('Y-m-d H:i:s');
+
+                $connection->update(
+                    $table,
+                    ['created_at' => $dateFormat, 'updated_at' => $dateFormat],
+                    ['id' => $transaction->getId()]
+                );
+            }
+        }
+
+        $query = new Query($this->getProvider());
+
+        $query->between(
+            new \DateTime('-2 days'),
+            new \DateTime('-1 day')
+        );
+
+        $this->assertCount(0, $query->get('result'));
+
+        $query->between(
+            new \DateTime('-1 day'),
+            new \DateTime()
+        );
+
+        $this->assertCount($count / 2, $query->get('result'));
+    }
+
+    /**
+     * Test return based transaction like search
+     */
+    public function testReturnBySearch()
+    {
+        $this->markTestSkipped('Like filter is processed by paginator!');
+
+        $products = $this->createProducts(2);
+
+        $product = $products[0];
+
+        $operations = [
+            Operation::create($product, 100, 'Foo Transaction'),
+            Operation::create($product, 100, 'Bar Transaction'),
+            Operation::create($product, 100, 'Foo Bar'),
+            Operation::create($product, 100, 'Foo'),
+            Operation::create($product, 100, 'Bar'),
+            Operation::create($product, 100, 'Transaction'),
+        ];
+
+        $this->service('stock_control')->process($operations);
+
+        $this->assertCount(count($operations), $product->getTransactions()->toArray());
+
+        /** @var Query $query */
+        $query = $this->service('stock_query');
+
+        $query->like('transact');
+        $this->assertCount(3, $query->result());
+
+        $query->like('foo');
+        $this->assertCount(3, $query->result());
     }
 
     /**
@@ -78,36 +193,16 @@ class QueryTransactionTest extends AppTestCase
      *
      * @param ProductInterface $product
      */
-    private function createTransactions(ProductInterface $product)
+    private function createTransactions(ProductInterface $product, $count = 50)
     {
         $operations = [];
-        for ($i = 0; $i < 50; $i++){
+        for ($i = 0; $i < $count; $i++){
 
             $amount = 0 == $i % 2 ? ($i + 10) : ($i - 10);
             $operations[] = Operation::create($product, $amount, 'Test');
         }
 
         $this->service('stock_control')->process($operations);
-
-        $this->assertCount(count($operations), $product->getTransactions()->toArray());
-
-        // $manager = $this->manager('stock_product');
-
-        /*$inc = 0;
-        foreach ($product->getTransactions() as  $key => $transaction){
-            if(0 == $key % 2){
-
-                $date = new \DateTime( $inc . ' days');
-
-                dump($date);
-                $transaction->setCreatedAt($date);
-                $transaction->setUpdatedAt($date);
-
-                $inc += 10;
-            }
-        }*/
-
-        // $manager->save($product);
     }
 
     /**
