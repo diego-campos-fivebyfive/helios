341d9c362add1f0bd937b122e1341aee1b8759c7
joaozaqueu <joaozaqueuchereta@gmail.com>
2017-11-07 12:03:12 -0200

    implementação do Manager de OrderMessage

--- backend/src/AppBundle/Manager/OrderMessageManager.php
+++ backend/src/AppBundle/Manager/OrderMessageManager.php
@@ -1,15 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 07/11/17
- * Time: 11:09
- */
 
 namespace AppBundle\Manager;
 
-
-class OrderMessageManager
+class OrderMessageManager extends AbstractManager
 {
 
 }
