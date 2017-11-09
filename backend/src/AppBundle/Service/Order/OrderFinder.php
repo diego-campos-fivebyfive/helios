b9b7f6fcb139621282d0dc31eda352443dc9afcb
Fabio Dukievicz <fabiojd47@gmail.com>
2017-11-09 11:48:26 -0200

    listagem de order para user de logistica

--- backend/src/AppBundle/Service/Order/OrderFinder.php
+++ backend/src/AppBundle/Service/Order/OrderFinder.php
@@ -165,6 +165,10 @@ class OrderFinder
             throw new \InvalidArgumentException('Invalid agent type');
 
         $includeStatus = [];
+        if($agent->isPlatformLogistic()){
+            $includeStatus = [Order::STATUS_INSERTED, Order::STATUS_AVAILABLE, Order::STATUS_COLLECTED];
+        }
+
         if($agent->isPlatformAfterSales()){
             $includeStatus = [Order::STATUS_DONE, Order::STATUS_INSERTED];
         }
