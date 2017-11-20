279461bdc9f39335064bd37d9100043a379288ec
joaozaqueu <joaozaqueuchereta@gmail.com>
2017-11-20 14:21:47 -0200

    implementação da controller de Catalog, ou gerador de lista de sistemas

--- backend/src/AppBundle/Controller/CatalogController.php
+++ backend/src/AppBundle/Controller/CatalogController.php
@@ -1,15 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 20/11/17
- * Time: 10:14
- */
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use APY\BreadcrumbTrailBundle\Annotation\Breadcrumb;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
 
-class CatalogController
+/**
+ * @Route("catalog")
+ * @Breadcrumb("Lista de Sistemas")
+ */
+class CatalogController extends AbstractController
 {
-
+    /**
+     * @Route("/", name="index_catalog")
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('catalog/index.html.twig', array(
+        ));
+    }
 }
