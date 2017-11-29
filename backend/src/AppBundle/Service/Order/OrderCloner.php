ed73aaf47812dddfefece73363f22cb07068e770
Fabio Dukievicz <fabiojd47@gmail.com>
2017-11-29 17:10:29 -0200

    ajuste na proforma

---
+++ backend/src/AppBundle/Service/Order/OrderCloner.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kolinalabs
+ * Date: 28/11/17
+ * Time: 17:08
+ */
+
+namespace AppBundle\Service\Order;
+
+
+class OrderCloner
+{
+
+}
---
+++ backend/tests/AppBundle/Service/Order/OrderClonerTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kolinalabs
+ * Date: 29/11/17
+ * Time: 09:36
+ */
+
+namespace Tests\AppBundle\Service\Order;
+
+
+class OrderCloner
+{
+
+}
