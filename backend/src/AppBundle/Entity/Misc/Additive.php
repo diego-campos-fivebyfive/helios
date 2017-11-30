89ced42be66552cf940ebeaa847b8b9476644407
Fabio Dukievicz <fabiojd47@gmail.com>
2017-11-30 09:40:52 -0200

    Mapeamento de nova entidade Additive

---
+++ backend/src/AppBundle/Entity/Misc/Additive.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace AppBundle\Entity\Misc;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Additive
+ *
+ * @ORM\Table(name="misc_additive")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\Misc\AdditiveRepository")
+ */
+class Additive implements AdditiveInterface
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="type", type="smallint")
+     */
+    private $type;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="target", type="smallint")
+     */
+    private $target;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="value", type="float")
+     */
+    private $value;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set type
+     *
+     * @param integer $type
+     *
+     * @return Additive
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return int
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Additive
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Additive
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set target
+     *
+     * @param integer $target
+     *
+     * @return Additive
+     */
+    public function setTarget($target)
+    {
+        $this->target = $target;
+
+        return $this;
+    }
+
+    /**
+     * Get target
+     *
+     * @return int
+     */
+    public function getTarget()
+    {
+        return $this->target;
+    }
+
+    /**
+     * Set value
+     *
+     * @param float $value
+     *
+     * @return Additive
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return float
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
+
---
+++ backend/src/AppBundle/Entity/Misc/AdditiveInterface.php
@@ -0,0 +1,81 @@
+<?php
+/*
+ * This file is part of the SicesSolar package.
+ *
+ * (c) SicesSolar <http://sicesbrasil.com.br/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Entity\Misc;
+
+use Doctrine\ORM\Mapping as ORM;
+
+interface AdditiveInterface
+{
+    const TARGET_FIXED = 1;
+    const TARGET_PERCENT = 2;
+
+    const TYPE_INSURANCE = 1;
+
+    /**
+     * @return int
+     */
+    public function getId();
+
+    /**
+     * @param $type
+     * @return AdditiveInterface
+     */
+    public function setType($type);
+
+    /**
+     * @return int
+     */
+    public function getType();
+
+    /**
+     * @param $name
+     * @return AdditiveInterface
+     */
+    public function setName($name);
+
+    /**
+     * @return string
+     */
+    public function getName();
+
+    /**
+     * @param $description
+     * @return AdditiveInterface
+     */
+    public function setDescription($description);
+
+    /**
+     * @return string
+     */
+    public function getDescription();
+
+    /**
+     * @param $target
+     * @return AdditiveInterface
+     */
+    public function setTarget($target);
+
+    /**
+     * @return int
+     */
+    public function getTarget();
+
+    /**
+     * @param $value
+     * @return AdditiveInterface
+     */
+    public function setValue($value);
+
+    /**
+     * @return float
+     */
+    public function getValue();
+}
