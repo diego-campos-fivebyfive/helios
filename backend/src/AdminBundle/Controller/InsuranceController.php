ae799013fae625514796f0a2c01377b7f7258d2c
Fabio Dukievicz <fabiojd47@gmail.com>
2017-11-30 18:12:59 -0200

    form e controller de seguro

--- backend/src/AdminBundle/Controller/InsuranceController.php
+++ backend/src/AdminBundle/Controller/InsuranceController.php
@@ -1,15 +1,89 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 30/11/17
- * Time: 10:16
- */
 
 namespace AdminBundle\Controller;
 
+use AppBundle\Controller\AbstractController;
+use AppBundle\Entity\Misc\Additive;
+use AppBundle\Entity\Misc\AdditiveInterface;
+use AdminBundle\Form\Misc\InsuranceType;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use APY\BreadcrumbTrailBundle\Annotation\Breadcrumb;
 
-class InsuranceController
+/**
+ * @Security("has_role('ROLE_PLATFORM_ADMIN')")
+ *
+ * @Breadcrumb("Seguro")
+ *
+ * @Route("/insurance")
+ */
+class InsuranceController extends AbstractController
 {
+    /**
+     * @Route("/", name="insurance_index")
+     */
+    public function indexAction(Request $request)
+    {
+        $manager = $this->manager('additive');
 
+        return $this->render('admin/insurance/index.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="insurance_create")
+     */
+    public function createAction(Request $request)
+    {
+        $manager = $this->manager('additive');
+
+        /** @var AdditiveInterface $insurance */
+        $insurance = $manager->create();
+
+        $form = $this->createForm(InsuranceType::class, $insurance);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $manager->save($insurance);
+
+            return $this->json([]);
+        }
+
+        return $this->render('admin/insurance/form.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/update", name="insurance_update")
+     */
+    public function updateAction(Request $request, Additive $insurance)
+    {
+        $form = $this->createForm(InsuranceType::class, $insurance);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $this->manager('additive')->save($insurance);
+
+            return $this->json([]);
+        }
+
+        return $this->render('admin/insurance/form.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/delete", name="insurance_delete")
+     * @Method("delete")
+     */
+    public function deleteAction(Additive $insurance)
+    {
+        $this->manager('additive')->delete($insurance);
+
+        return $this->json([]);
+    }
 }
--- backend/src/AdminBundle/Form/Misc/InsuranceType.php
+++ backend/src/AdminBundle/Form/Misc/InsuranceType.php
@@ -1,12 +1,49 @@
 <?php
+namespace AdminBundle\Form\Misc;
 
-/**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 30/11/17
- * Time: 11:16
- */
-class InsuranceType
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use AppBundle\Entity\Misc\Additive;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InsuranceType extends AbstractType
 {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('type', ChoiceType::class, [
+                'required' => true,
+                'choices' => Additive::getTypes()
+            ])
+            ->add( 'name', TextType::class, [
+                'required' => true
+            ])
+            ->add('description', TextType::class, [
+                'required' => false,
+            ])
+            ->add('target', ChoiceType::class, [
+                'required' => true,
+                'choices' => Additive::getTargets()
+            ])
+            ->add('value', TextType::class, [
+                'required' => true
+            ]);
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Misc\Additive'
+        ));
+    }
 
 }
