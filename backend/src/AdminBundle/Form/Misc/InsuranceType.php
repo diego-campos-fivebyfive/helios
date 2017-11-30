ba7c28de41e1c6a3bcc341730017756a85f2da58
Fabio Dukievicz <fabiojd47@gmail.com>
2017-11-30 18:10:55 -0200

    adição de linhas

---
+++ backend/src/AdminBundle/Form/Misc/InsuranceType.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kolinalabs
+ * Date: 30/11/17
+ * Time: 11:16
+ */
+class InsuranceType
+{
+
+}
---
+++ backend/src/AppBundle/Manager/AdditiveManager.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kolinalabs
+ * Date: 30/11/17
+ * Time: 10:57
+ */
+
+namespace AppBundle\Manager;
+
+
+class InsuranceManager extends AbstractManager
+{
+
+}
