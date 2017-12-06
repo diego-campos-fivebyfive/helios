1840efd64a23c84bf511af5646747bf7e5332a25
joaozaqueu <joaozaqueuchereta@gmail.com>
2017-12-06 12:06:38 -0200

    criação e mapeamento da trait AdditiveRelationTrait

--- backend/src/AppBundle/Entity/Misc/AdditiveRelationTrait.php
+++ backend/src/AppBundle/Entity/Misc/AdditiveRelationTrait.php
@@ -12,7 +12,110 @@ namespace AppBundle\Entity\Misc;
 
 use Doctrine\ORM\Mapping as ORM;
 
-trait AdditiveAssocTrait
+/**
+ * Trait AdditiveRelationTrait
+ * @package AppBundle\Entity\Misc
+ *
+ * @ORM\Entity
+ */
+trait AdditiveRelationTrait
 {
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
 
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="quantity", type="integer")
+     */
+    private $quantity;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="total", type="float")
+     */
+    private $total;
+
+    /**
+     * @var AdditiveInterface
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Misc\Additive")
+     * @ORM\JoinColumn(name="additive_id", referencedColumnName="id")
+     */
+    private $additive;
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct()
+    {
+        $this->quantity = 1;
+        $this->total = 0;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = (int) $quantity;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setTotal($total)
+    {
+        $this->total = (float) $total;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getTotal()
+    {
+        return $this->total;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setAdditive(AdditiveInterface $additive)
+    {
+        $this->additive = $additive;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getAdditive()
+    {
+        return $this->additive;
+    }
 }
