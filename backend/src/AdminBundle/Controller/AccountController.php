74fdc4fa379fc884e218d62ca748b7f9b97c7769
Fabio Dukievicz <fabiojd47@gmail.com>
2017-12-15 17:20:55 -0200

    ajuste em opções de filtro de level

--- backend/src/AdminBundle/Controller/AccountController.php
+++ backend/src/AdminBundle/Controller/AccountController.php
@@ -101,6 +101,7 @@ class AccountController extends AdminController
         $levels = Memorial::getDefaultLevels();
 
         unset($levels[Memorial::LEVEL_PROMOTIONAL]);
+        unset($levels[Memorial::LEVEL_FINAME]);
 
         $states = $this->getStates($expanseStates);
 
