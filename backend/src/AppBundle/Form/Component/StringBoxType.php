a404bdf15f635e1a6ae63bf35498c42a6e8f0fc5
joaozaqueu <joaozaqueuchereta@gmail.com>
2017-12-15 15:11:20 -0200

    alteração na consulta do select de fabricantes

--- backend/src/AppBundle/Form/Component/StringBoxType.php
+++ backend/src/AppBundle/Form/Component/StringBoxType.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Form\Component;
 
+use AppBundle\Entity\Component\MakerInterface;
 use AppBundle\Entity\Pricing\Memorial;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -17,13 +18,33 @@ class StringBoxType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $builder->add('maker', 'entity', array(
+                'required' => true,
+                'multiple' => false,
+                'property' => 'name',
+                'class' => 'AppBundle\Entity\Component\Maker',
+                'query_builder' => function (\Doctrine\ORM\EntityRepository $er){
+
+                    $parameters = ['context' => MakerInterface::CONTEXT_STRING_BOX];
+
+                    $qb = $er
+                        ->createQueryBuilder('m')
+                        ->where('m.context = :context')
+                        ->orderBy('m.name', 'ASC');
+
+                    $qb->setParameters($parameters);
+
+                    return $qb;
+                }
+            )
+        );
+
         $builder
             ->add('code', null, ['required' => true])
             ->add('description', null, ['required' => true])
             ->add('inputs', null, ['required' => true])
             ->add('outputs', null, ['required' => true])
             ->add('fuses', null, ['required' => true])
-            ->add('maker', null, ['required' => true])
             ->add('position', NumberType::class, [
                 'required' => false
             ])
