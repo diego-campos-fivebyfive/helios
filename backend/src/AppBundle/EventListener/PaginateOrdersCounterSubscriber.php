052a012cd4500386b34fe90677305cdbb8dc8bbe
Claudinei M <cjchamado@gmail.com>
2017-12-19 10:17:10 -0200

    Corrigida referência de busca para usuário SICES

---
+++ backend/src/AppBundle/EventListener/PaginateOrdersCounterSubscriber.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudinei
+ * Date: 18/12/17
+ * Time: 18:22
+ */
+
+namespace AppBundle\EventListener;
+
+
+class PaginateOrdersCounterSubscriber
+{
+
+}
