4d21ef5c757473ee0ce323d4719ae2b6d93bbc40
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-01-12 16:00:25 -0200

    integração do metodo de upload e save de templates, e rota de listagem de templates

--- backend/src/AppBundle/Controller/TemplateController.php
+++ backend/src/AppBundle/Controller/TemplateController.php
@@ -4,12 +4,15 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Component\Project;
 use AppBundle\Entity\Component\ProjectInterface;
+use AppBundle\Entity\ThemeInterface;
 use AppBundle\Service\Order\ComponentExtractor;
 use APY\BreadcrumbTrailBundle\Annotation\Breadcrumb;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use PhpOffice\PhpWord\TemplateProcessor;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * @Route("/template")
@@ -23,6 +26,53 @@ class TemplateController extends AbstractController
     public function indexAction()
     {
         return $this->render('projects/templates/index.html.twig');
+    }
+
+    /**
+     * @Route("/upload", name="template_upload")
+     */
+    public function templateUploadAction(Request $request)
+    {
+        $file = $request->files->get('file');
+
+        if (!$file instanceof UploadedFile) {
+            return $this->render('projects/templates/upload.html.twig');
+        }
+
+        $filename = substr(md5(uniqid(rand(1,6))), 0, 8) . '.docx';
+
+        $options = [
+            'filename' => $filename,
+            'root' => 'proposal',
+            'type' => 'template',
+            'access' => 'private'
+        ];
+
+        $originalFilename = $file->getClientOriginalName();
+
+        $this->saveTheme($filename, $originalFilename);
+
+        $location = $this->container->get('app_storage')->location($options);
+
+        $path = str_replace($filename, '', $location);
+
+        $file->move($path, $filename);
+
+        return $this->json([ 'name' => $filename ]);
+    }
+
+    /**
+     * @Route("/templatesList", name="templates_list")
+     */
+    public function templatesList()
+    {
+        $themes = $this->manager('theme')->findBy([
+            'accountId' => $this->account()->getId()
+        ]);
+
+        return $this->render('projects/templates/templates_list.html.twig', [
+            'themes' => $themes
+        ]);
     }
 
     /**
@@ -221,5 +271,29 @@ class TemplateController extends AbstractController
             $this->templateProcessor
                 ->setValue('valor#'.($i + 1), self::formatCurrency($project->getAccumulatedCash()[$i]));
         }
+    }
+
+    private function saveTheme($filename, $originalName)
+    {
+        $manager = $this->manager('theme');
+
+        /** @var ThemeInterface $theme */
+        $theme = $manager->create();
+        $theme
+            ->setAccountId($this->account()->getId())
+            ->setTheme(1)
+            ->setContent('')
+            ->setName($originalName)
+            ->setFilename($filename);
+
+        $manager->save($theme);
+    }
+
+    /**
+     * @return \AppBundle\Entity\BusinessInterface|\AppBundle\Entity\AccountInterface
+     */
+    protected function account()
+    {
+        return $this->member()->getAccount();
     }
 }
