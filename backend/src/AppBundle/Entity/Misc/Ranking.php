a36a5588c7045e8bde69729cba3e9bf63e2243ad
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-01-15 15:23:22 -0200

    criação da entidade e interface de Ranking

---
+++ backend/src/AppBundle/Entity/Misc/Ranking.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace AppBundle\Entity\Misc;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Ranking
+ *
+ * @ORM\Table(name="app_ranking")
+ * @ORM\Entity
+ */
+class Ranking implements RankingInterface
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="target", type="string", length=255)
+     */
+    private $target;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createdAt", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="amount", type="integer")
+     */
+    private $amount;
+
+
+    /**
+     * @inheritdoc
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setTarget($target)
+    {
+        $this->target = $target;
+
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getTarget()
+    {
+        return $this->target;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+}
+
--- backend/src/AppBundle/Entity/Misc/RankingInterface.php
+++ backend/src/AppBundle/Entity/Misc/RankingInterface.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 15/01/18
- * Time: 14:38
- */
 
 namespace AppBundle\Entity\Misc;
 
@@ -12,4 +6,52 @@ namespace AppBundle\Entity\Misc;
 interface RankingInterface
 {
 
+    /**
+     * @return int
+     */
+    public function getId();
+
+    /**
+     * @param $target
+     * @return RankingInterface
+     */
+    public function setTarget($target);
+
+    /**
+     * @return string
+     */
+    public function getTarget();
+
+    /**
+     * @param $description
+     * @return RankingInterface
+     */
+    public function setDescription($description);
+
+    /**
+     * @return string
+     */
+    public function getDescription();
+
+    /**
+     * @param $createdAt
+     * @return RankingInterface
+     */
+    public function setCreatedAt($createdAt);
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt();
+
+    /**
+     * @param $amount
+     * @return RankingInterface
+     */
+    public function setAmount($amount);
+
+    /**
+     * @return int
+     */
+    public function getAmount();
 }
