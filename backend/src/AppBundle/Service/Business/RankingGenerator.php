e76ef63f826affa11cac9b91621f1f936582f6a7
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-01-15 17:37:23 -0200

    Implementação do Serviço RankingGenerator

--- backend/src/AppBundle/Service/Business/RankingGenerator.php
+++ backend/src/AppBundle/Service/Business/RankingGenerator.php
@@ -1,15 +1,78 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 15/01/18
- * Time: 15:55
- */
 
 namespace AppBundle\Service\Business;
 
+use AppBundle\Entity\Customer;
+use AppBundle\Entity\Misc\Ranking;
+use AppBundle\Entity\Misc\RankingInterface;
+use AppBundle\Service\Stock\Identity;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class RankingGenerator
 {
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
 
+    /**
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param $target
+     * @param $description
+     * @param $amount
+     * @return RankingInterface
+     */
+    public function create($target, $description, $amount)
+    {
+        $manager = $this->container->get('ranking_manager');
+
+        $target = $this->resolveTarget($target);
+
+        /** @var RankingInterface $ranking */
+        $ranking = $manager->create();
+        $ranking
+            ->setTarget($target)
+            ->setDescription($description)
+            ->setAmount($amount)
+        ;
+
+        $manager->save($ranking);
+
+        return $ranking;
+    }
+
+    /**
+     * @param $target
+     * @return mixed
+     */
+    public function load($target)
+    {
+        $manager = $this->container->get('ranking_manager');
+
+        $target = $this->resolveTarget($target);
+
+        $ranking = $manager->findBy([
+            'target' => $target
+        ]);
+
+        return $ranking;
+    }
+
+    /**
+     * @param $target
+     * @return array|string
+     */
+    private function resolveTarget($target)
+    {
+        if (is_object($target)) {
+            return $target = Identity::create($target);
+        }
+    }
 }
