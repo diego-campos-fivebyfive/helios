dce60b771e7707b392af51f456ee7fd1d3e2fe59
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-01-15 15:24:44 -0200

    TestCase da Entidade de Ranking

--- backend/tests/AppBundle/Entity/RankingManagerTest.php
+++ backend/tests/AppBundle/Entity/RankingManagerTest.php
@@ -1,15 +1,45 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 15/01/18
- * Time: 15:02
- */
 
 namespace Tests\AppBundle\Entity;
 
+use AppBundle\Entity\Misc\Ranking;
+use AppBundle\Entity\Misc\RankingInterface;
+use Tests\AppBundle\AppTestCase;
+use Tests\AppBundle\Helpers\ObjectHelperTest;
 
-class RankingManagerTest
+/**
+ * @group ranking
+ */
+class RankingManagerTest extends AppTestCase
 {
+    use ObjectHelperTest;
+
+    public function testDefault()
+    {
+        $ranking = $this->createRanking();
+        $this->assertInstanceOf(Ranking::class, $ranking);
+        $this->assertNotNull($ranking);
+    }
+
+    private function createRanking()
+    {
+        $manager = $this->getContainer()->get('ranking_manager');
+
+        /** @var RankingInterface $ranking */
+        $ranking = $manager->create();
+
+        $date = new \DateTime('now');
+
+        $ranking
+            ->setTarget('Teste um')
+            ->setDescription('Descrição do Teste de entidade Ranking')
+            ->setCreatedAt($date)
+            ->setAmount(5)
+        ;
+
+        $manager->save($ranking);
+
+        return $ranking;
+    }
 
 }
