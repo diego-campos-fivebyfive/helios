3ccc1659c3c33bde484c8631521d6570b6e9ddbe
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-01-15 15:24:22 -0200

    criação do Manager de Ranking

--- backend/src/AppBundle/Manager/RankingManager.php
+++ backend/src/AppBundle/Manager/RankingManager.php
@@ -1,15 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 15/01/18
- * Time: 14:58
- */
 
 namespace AppBundle\Manager;
 
 
-class RankingManager
+class RankingManager extends AbstractManager
 {
 
 }
