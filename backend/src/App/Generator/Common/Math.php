aaa244513999ab72548dc4228689d77a0828b332
Claudinei M <cjchamado@gmail.com>
2018-01-23 15:43:35 -0200

    initializados testes do novo gerador

---
+++ backend/src/App/Generator/Common/Math.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudinei
+ * Date: 23/01/18
+ * Time: 14:58
+ */
+
+namespace App\Generator\Common;
+
+
+class Math
+{
+
+}
---
+++ backend/src/App/Generator/Inverter/Helper.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudinei
+ * Date: 23/01/18
+ * Time: 15:26
+ */
+
+namespace App\Generator\Inverter;
+
+
+class Helper
+{
+
+}
