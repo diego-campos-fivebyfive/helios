7a4c5c293cc7a155910b7a412855cc460369f43e
Claudinei M <cjchamado@gmail.com>
2018-01-23 16:24:37 -0200

    migrado método de exemplo

--- backend/src/App/Generator/Inverter/Helper.php
+++ backend/src/App/Generator/Inverter/Helper.php
@@ -25,4 +25,29 @@ class Helper
 
         return $newPower;
     }
+
+    /**
+     * @param array $inverters
+     * @return array
+     */
+    public static function mpptOperations(array $inverters)
+    {
+        $mppts = array();
+
+        for ($i=0; $i<count($inverters); $i++){
+            $parallel = $inverters[$i]["mppt_parallel"];
+             $number = $inverters[$i]["mppt_number"];
+
+            if ($parallel == 1){
+                $mppts[$i][0] =  $number;
+            }else{
+                for ($m=0; $m < $number; $m++){
+                    $mppts[$i][$m] = 1;
+                }
+            }
+
+        }
+
+        return $mppts;
+    }
 }
