892e634013217e9795f297eabf4dd02f037ed233
kolina-pc2 <fabiojd47@gmail.com>
2018-01-24 18:20:46 -0200

    novas funções convertidas

--- backend/src/App/Generator/Inverter/Helper.php
+++ backend/src/App/Generator/Inverter/Helper.php
@@ -34,7 +34,7 @@ class Helper
      */
     public static function mpptOperations(array $inverters)
     {
-        $mppts = array();
+        $mppts = [];
 
         for ($i=0; $i<count($inverters); $i++){
             $parallel = $inverters[$i]["mppt_parallel"];
@@ -91,7 +91,7 @@ class Helper
     public static function allCombinationsOptimized($numberOfElements, $maxOfElements)
     {
         $possibilities = Math::combinations($numberOfElements, $maxOfElements);
-        $combination = array();
+        $combination = [];
 
         for ($i = 0; $i < $numberOfElements; $i++) {
             $counter = array_fill(0, $maxOfElements, $i);
@@ -122,5 +122,167 @@ class Helper
         }
 
         return $combination;
+    }
+
+    /**
+     * @param array $data
+     * @param array $alternatives
+     * @return array
+     */
+    public static function filterActives(array $data, array $alternatives = []){
+
+        foreach ($data as $key => $item){
+            if(null != $item = self::filterAlternativeItem($item, $alternatives)){
+                $data[$key] = $item;
+            }else{
+                unset($data[$key]);
+            }
+        }
+
+        uasort($data, function ($a, $b){
+            return $a["nominal_power"] > $b["nominal_power"];
+        });
+
+        return array_values($data);
+    }
+
+    /**
+     * @param $inverters
+     * @param $phaseVoltage
+     * @param $phaseNumber
+     * @return array
+     */
+    public static function filterPhases($inverters, $phaseVoltage, $phaseNumber)
+    {
+        $net = [$phaseVoltage, $phaseNumber];
+
+        if ($net == [220, 1] or $net == [220, 2]) {
+            $count = count($inverters);
+            for ($i = 0; $i < $count; $i++) {
+                if ($inverters[$i]["phase_number"] > $phaseNumber) {
+                    unset($inverters[$i]);
+                }
+            }
+
+            $inverters = array_values($inverters);
+        }
+
+        if ($net == [380, 3] or $net == [220, 3]) {
+            $count = count($inverters);
+            for ($i = 0; $i < $count; $i++) {
+                $inverterPhaseVoltage = $inverters[$i]["phase_voltage"];
+                $inverterCompatibility = $inverters[$i]["compatibility"];
+
+                if ($inverterPhaseVoltage == $phaseVoltage)
+                    $inverterCompatibility = 0;
+                if ($inverterPhaseVoltage > 380 or $inverterCompatibility > 0)
+                    unset($inverters[$i]);
+            }
+
+            $inverters = array_values($inverters);
+        }
+
+        if ($phaseVoltage > 380) {
+            $count = count($inverters);
+            for ($i = 0; $i < $count; $i++)
+                if ($inverters[$i]["phase_voltage"] != $phaseVoltage)
+                    unset($inverters[$i]);
+            $inverters = array_values($inverters);
+        }
+
+        return $inverters;
+    }
+
+    public static function filterPower($inverters, $desiredPower)
+    {
+        $count = count($inverters);
+        for ($i = 0; $i < $count; $i++) {
+            $maxShow = $inverters[$i]["pow_max_show"];
+            $minShow = $inverters[$i]["pow_min_show"];
+            if ($maxShow > 0) {
+                if ($desiredPower < $minShow or $desiredPower > $maxShow) {
+                    unset($inverters[$i]);
+                }
+            }
+        }
+        $inverters = array_values($inverters);
+
+        if ($desiredPower >= 75) {
+            $count = count($inverters);
+            for ($i = 0; $i < $count; $i++) {
+                if ($inverters[$i]["phase_number"] < 3) {
+                    unset($inverters[$i]);
+                }
+            }
+
+            $inverters = array_values($inverters);
+        }
+
+        if ($desiredPower >= 500) {
+            $count = count($inverters);
+            for ($i = 0; $i < $count - 1; $i++) {
+                unset($inverters[$i]);
+            }
+            $inverters = array_values($inverters);
+        }
+
+        return $inverters;
+    }
+
+    public static function inverterChoices($inverters, $desiredPower, $fdiMin, $fdiMax)
+    {
+        $inferiorLimit = $desiredPower * $fdiMin;
+        $upperLimit = $desiredPower * $fdiMax;
+
+        $selected = [];
+
+        for ($i = 0; $i <= 30; $i++) {
+            $combinations = self::allCombinationsOptimized(count($inverters), $i + 1);
+            for ($k = 0; $k < count($combinations); $k++) {
+                $accumulatedPower = 0;
+
+                for ($j = 0; $j < count($combinations[$k]); $j++) {
+                    $index = $combinations[$k][$j];
+                    $accumulatedPower += $inverters[$index]["nominal_power"];
+
+                    $selected[$j] = $inverters[$index];
+                }
+
+                if ($accumulatedPower >= $inferiorLimit and $accumulatedPower <= $upperLimit)
+                    break 2;
+            }
+            $selected = [];
+        }
+
+        return $selected;
+    }
+
+    /**
+     * @param $item
+     * @param array $alternatives
+     * @return null
+     */
+    private static function filterAlternativeItem($item, array &$alternatives = []){
+
+        if(!is_null($item)){
+            if($item['active']){
+                return $item;
+            }elseif(array_key_exists('alternative', $item)){
+
+                $key = array_search($item['alternative'], array_column($alternatives, 'id'));
+
+                if(is_numeric($key)) {
+
+                    $next = &$alternatives[$key];
+
+                    if (!array_key_exists('visited', $next) || !$next['visited']) {
+                        $next['visited'] = true;
+                        return self::filterAlternativeItem($next, $alternatives);
+                    }
+                }
+            }
+        }
+
+        return null;
     }
 }
