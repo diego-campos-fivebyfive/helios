b0674467755e44ecd556d0d69b865d36969a743c
kolina-pc2 <fabiojd47@gmail.com>
2018-01-25 10:56:31 -0200

    teste unitario em novos metodos

--- backend/tests/AppBundle/Entity/OrderEntityTest.php
+++ backend/tests/AppBundle/Entity/OrderEntityTest.php
@@ -2,25 +2,76 @@
 
 namespace Tests\AppBundle\Entity;
 
-use Tests\AppBundle\AppTestCase;
+use AppBundle\Entity\Order\Element;
+use AppBundle\Entity\Order\OrderInterface;
+use Tests\App\Generator\GeneratorTest;
 
 /**
  * Class OrderEntityTest
- * @group order_entity_a
+ * @group order_entity
  */
-class OrderEntityTest extends AppTestCase
+class OrderEntityTest extends GeneratorTest
 {
     public function testDiscountConfig()
     {
-       // $orderManager = $this->getContainer()->get('order_manager');
+        $discountConfigFixed = [
+            'target' => OrderInterface::DISCOUNT_FIXED,
+            'value' => 250
+        ];
+        $discountConfigPercent = [
+            'target' => OrderInterface::DISCOUNT_PERCENT,
+            'value' => 10.5
+        ];
 
-     //   $order = $orderManager->create();
-        //$orderManager->save($order);
+        $order = $this->createOrder();
+        self::assertEquals(1000, $order->getSubTotal());
 
-        $this->assertNotNull('kfgk');
+        $order->setDiscount(200);
+        self::assertEquals(200,$order->getDiscount());
+        self::assertEquals(800, $order->getTotal());
 
+        $order->setDiscountConfig($discountConfigFixed);
+        self::assertEquals(250,$order->getDiscount());
+        self::assertEquals(750, $order->getTotal());
 
+        $order->setDiscountConfig($discountConfigPercent);
+        self::assertEquals(105,$order->getDiscount());
+        self::assertEquals(895, $order->getTotal());
 
+        self::assertEquals(1, $order->getDiscountConfig()['target']);
+        self::assertEquals(10.5, $order->getDiscountConfig()['value']);
 
+        $discountConfig = [
+            'target' => OrderInterface::DISCOUNT_FIXED,
+            'value' => 10
+        ];
+
+        $order->setDiscountConfig($discountConfig);
+
+        self::assertEquals(0, $order->getDiscountConfig()['target']);
+        self::assertEquals(10, $order->getDiscountConfig()['value']);
+    }
+
+    private function createOrder()
+    {
+        $manager = $this->getContainer()->get('order_manager');
+
+        /** @var OrderInterface $parent */
+        $parent = $manager->create();
+
+        for($i= 0; $i < 2; $i++){
+            /** @var OrderInterface $order */
+            $order = $manager->create();
+
+            $element = new Element();
+            $element->setUnitPrice(100);
+            $element->setQuantity(5);
+
+            $order->addElement($element);
+
+            $parent->addChildren($order);
+        }
+
+        return $parent;
     }
 }
