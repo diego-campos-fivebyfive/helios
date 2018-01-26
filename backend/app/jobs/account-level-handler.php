81fb0d6c8c93a98aa70beeb6535ce1c8d87d14d2
Claudinei M <cjchamado@gmail.com>
2018-01-26 16:37:31 -0200

    adicionado cliente cron para atualização automática de níveis

--- backend/app/jobs/account-level-handler.php
+++ backend/app/jobs/account-level-handler.php
@@ -1,93 +1,56 @@
 <?php
 
-use AppBundle\Service\Order\OrderRanking;
-
 require_once dirname(__FILE__) . '/db/connection.php';
-require_once dirname(__DIR__) . '/../vendor/autoload.php';
 
-$mapping = OrderRanking::getMapping();
+$data = R::findOne('app_parameter', 'id = ?', ['platform_settings']);
 
-$sql = <<<SQL
-SELECT 
-o.id,
-o.reference, 
-o.level,
-o.power,
-o.account_id, 
-o.delivery_at 
-FROM app_order o 
-WHERE o.parent_id IS NULL
-AND o.status >= 7 
-AND o.power > 0
-SQL;
+if (!$data)  return;
 
-// Clear all ranking transactions
-R::wipe('app_ranking');
+$arrayParameters = json_decode($data->parameters, true);
 
-$orders = R::getAll($sql);
-$date = (new \DateTime())->format('Y-m-d H:i:s');
-$rankings = [];
+if (!array_key_exists('account_level_handler', $arrayParameters)) return;
 
-foreach ($orders as $order){
+$config = $arrayParameters['account_level_handler'];
 
-    if(!array_key_exists($order['level'], $mapping)) continue;
-
-    $accountId = $order['account_id'];
-
-    $target = sprintf('%s::%s', \AppBundle\Entity\Customer::class, $order['account_id']);
-
-    $deliveryInfo = $order['delivery_at'] ? sprintf(' - Disp: %s', (new \DateTime($order['delivery_at']))->format('d/m/Y')) : '';
-
-    $description = sprintf(
-        '%s - %s%s',
-        $order['reference'],
-        (new \DateTime())->format('d/m/Y H:i'),
-        $deliveryInfo
-    );
-
-    $amount = ceil($order['power'] * $mapping[$order['level']]);
-
-    // create ranking
-    $ranking = R::dispense('app_ranking');
-
-    $ranking->target = $target;
-    $ranking->description = $description;
-    $ranking->amount = $amount;
-    $ranking->created_at = $date;
-    $ranking->updated_at = $date;
-
-    $rankings[] = $ranking;
+/**
+ * @param array $config
+ */
+function normalizeConfig(array &$config)
+{
+    uasort($config['levels'], function ($a, $b) {
+        return $a['amount'] > $b['amount'];
+    });
 }
 
-// Store rankings
-R::storeAll($rankings);
+/**
+ * @param array $config
+ */
+function normalizeLevels(array $config)
+{
+    normalizeConfig($config);
 
-/*** TRIGGERS
+    foreach ($config['levels'] as $level => $parameter) {
 
-DELIMITER //
+        $days = (int)$parameter['days'];
+        $amount = (float)$parameter['amount'];
 
-DROP TRIGGER IF EXISTS onInsertRanking;
-CREATE TRIGGER onInsertRanking
-AFTER INSERT ON app_ranking FOR EACH ROW
-BEGIN
-UPDATE app_customer c SET c.ranking = (SELECT sum(r.amount) FROM app_ranking r WHERE r.target = NEW.target);
-END//
+        $createdAt = (new \DateTime(sprintf('%d days ago', $days)))->format('Y-m-d');
 
+        $updateSQL = sprintf(<<<SQL
+UPDATE app_customer c
+SET c.level = '%s'
+WHERE (
+  SELECT SUM(o.total) total
+  FROM app_order o
+  WHERE o.parent_id IS NULL
+  AND o.status >= 7
+  AND DATE(o.created_at) >= '%s' 
+  AND o.account_id = c.id) > ROUND(%f, 2)
+SQL
+, $level, $createdAt, $amount);
 
-DROP TRIGGER IF EXISTS onUpdateRanking;
-CREATE TRIGGER onUpdateRanking
-AFTER UPDATE ON app_ranking FOR EACH ROW
-BEGIN
-UPDATE app_customer c SET c.ranking = (SELECT sum(r.amount) FROM app_ranking r WHERE r.target = NEW.target);
-END//
+        R::exec($updateSQL);
+    }
+}
 
-DROP TRIGGER IF EXISTS onDeleteRanking;
-CREATE TRIGGER onDeleteRanking
-AFTER DELETE ON app_ranking FOR EACH ROW
-BEGIN
-UPDATE app_customer c SET c.ranking = (SELECT sum(r.amount) FROM app_ranking r WHERE r.target = OLD.target);
-END//
-
-DELIMITER ;
-
-***/
+normalizeLevels($config);
