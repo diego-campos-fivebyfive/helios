ba4a0f33082710672bcc82447a8073109ee8b403
Claudinei M <cjchamado@gmail.com>
2018-01-26 14:03:11 -0200

    inserido normalizador de ranking de contas com redbean-php

--- backend/app/jobs/db/connection.php
+++ backend/app/jobs/db/connection.php
@@ -1,7 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudinei
- * Date: 26/01/18
- * Time: 10:31
- */
+
+require_once 'rb.php';
+
+$parameters = explode("\n", file_get_contents(dirname(__DIR__) . '/../config/parameters.yml'));
+
+$config = [];
+foreach ($parameters as $parameter){
+    $parameter = trim($parameter);
+    if(0 === strpos($parameter, 'database')){
+        list($key, $value) = explode(':', $parameter);
+        $config[trim($key)] = trim($value);
+    }
+}
+
+$dsn = sprintf('mysql:host=%s;port=%s;dbname=%s', $config['database_host'], $config['database_port'], $config['database_name']);
+
+R::setup($dsn, $config['database_user'], $config['database_password']);
