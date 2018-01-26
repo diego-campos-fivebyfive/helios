fa0e2777314f1ba40044631b383dca4af23f127d
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-01-26 17:12:13 -0200

    implementação do FormType para descontos comerciais

--- backend/src/AppBundle/Form/Order/DiscountType.php
+++ backend/src/AppBundle/Form/Order/DiscountType.php
@@ -1,15 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 26/01/18
- * Time: 11:27
- */
 
 namespace AppBundle\Form\Order;
 
+use AppBundle\Entity\Order\Order;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class DiscountType
+class DiscountType extends AbstractType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $member = $options['member'];
 
+        $choices = [
+            Order::DISCOUNT_PERCENT => 'Desconto Percentual',
+            Order::DISCOUNT_FIXED => 'Desconto Fixo'
+        ];
+
+        if ($member->isPlatformExpanse())
+            unset($choices[Order::DISCOUNT_FIXED]);
+
+        $builder->add('target', ChoiceType::class, [
+            'choices' => $choices
+            ]);
+        $builder->add('value', TextType::class);
+
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'member' => null,
+            'discount' => null
+        ));
+    }
 }
