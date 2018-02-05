4ad32964aaffaf8072a20d9698c15aba9048529c
kolina-pc2 <fabiojd47@gmail.com>
2018-02-05 12:03:40 -0200

    manager de Coupon

--- backend/src/AppBundle/Manager/CouponManager.php
+++ backend/src/AppBundle/Manager/CouponManager.php
@@ -3,7 +3,37 @@
 namespace AppBundle\Manager;
 
 
-class RankingManager extends AbstractManager
-{
+use AppBundle\Entity\AccountInterface;
 
+class CouponManager extends AbstractManager
+{
+    /**
+     * @param AccountInterface $account
+     * @return array
+     */
+    public function findByAccount(AccountInterface $account)
+    {
+        $qb = $this->createQueryBuilder();
+
+        $qb->select('c')
+            ->where('c.account = :account')
+            ->setParameter('account', $account);
+
+        return $qb->getQuery()->getResult();
+    }
+
+    /**
+     * @param $target
+     * @return array
+     */
+    public function findByTarget($target)
+    {
+        $qb = $this->createQueryBuilder();
+
+        $qb->select('c')
+            ->where('c.target = :target')
+            ->setParameter('target', $target);
+
+        return $qb->getQuery()->getResult();
+    }
 }
