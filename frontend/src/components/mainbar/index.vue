bfa95862bbd91cc6c93101ea1400d68c73427434
Rafael Kendrik <rafamikovski@hotmail.com>
2018-02-08 22:33:25 -0200

    dinamic date render

--- frontend/src/components/mainbar/index.vue
+++ frontend/src/components/mainbar/index.vue
@@ -1,8 +1,7 @@
 <template lang="pug">
   header.bar
     h1.title Inversores
-    h2.info
-      | quinta-feira, 8 de fevereiro de 2018
+    h2.info {{ date }}
     nav.menu
       Button(
         type='default-common',
@@ -14,7 +13,54 @@
 </template>
 
 <script>
-  /* Mainbar Script */
+  const getDate = (vm) => {
+
+    const months = [
+      'janeiro',
+      'fevereiro',
+      'março',
+      'abril',
+      'maio',
+      'junho',
+      'julho',
+      'agosto',
+      'setembro',
+      'outubro',
+      'novembro',
+      'dezembro'
+    ]
+
+    const daysInTheWeek = [
+      'domingo',
+      'segunda-feira',
+      'terça-feira',
+      'quarta-feira',
+      'quinta-feira',
+      'sexta-feira',
+      'sábado',
+    ]
+
+    const date = new Date()
+    const year = date.getFullYear()
+    const month = months[date.getMonth()]
+    const day = date.getDate()
+    const dayInTheWeek = daysInTheWeek[date.getDay()]
+
+    vm.date = `${dayInTheWeek}, ${day} de ${month} de ${year}`
+
+    setTimeout(() => {
+      getDate(vm)
+    }, 500)
+  }
+
+  export default {
+    data: () => ({
+      date: ''
+    }),
+    mounted() {
+      getDate(this)
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
