b4d86be232f804579156b557677396fb156fb563
Claudinei M <cjchamado@gmail.com>
2018-02-09 16:17:39 -0200

    adicionado paser util para utilização mista

--- backend/src/App/Sices/Utils/Parser.php
+++ backend/src/App/Sices/Utils/Parser.php
@@ -1,15 +1,52 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudinei
- * Date: 09/02/18
- * Time: 16:06
- */
 
-namespace App\Sices;
-
+namespace App\Sices\Utils;
 
 class Parser
 {
+    /**
+     * @var array
+     */
+    private $mapping;
 
+    /**
+     * Parser constructor.
+     * @param array $mapping
+     */
+    private function __construct(array $mapping)
+    {
+        $this->mapping = $mapping;
+    }
+
+    /**
+     * @param string $base
+     * @return array
+     */
+    public function parse(string $base)
+    {
+        $data = [];
+        $offset = 0;
+        foreach ($this->mapping as $key => $size){
+            $data[$key] = substr($base, $offset, $size);
+            $offset += $size;
+        }
+
+        return $data;
+    }
+
+    /**
+     * @param array $mapping
+     * @param string|null $base
+     * @return Parser|array
+     */
+    public static function from(array $mapping, string $base = null)
+    {
+        $parser = new self($mapping);
+
+        if(!is_null($base)){
+            return $parser->parse($base);
+        }
+
+        return $parser;
+    }
 }
