cb2fc455b87904077859bb3c49f606661e97a425
Rafael Kendrik <rafamikovski@hotmail.com>
2018-02-20 16:00:56 -0300

    paginator component bases

---
+++ frontend/src/components/collection/Paginator.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  nav
+    span(v-if='pagination.links.prev') Anterior
+    ul
+      li(v-for='pageNumber in pagination.total')
+        button(v-on:click='paginate(pageNumber)')
+          | {{ pageNumber }}
+    span(v-if='pagination.links.next') Próxima
+</template>
+
+<script>
+  export default {
+    props: [
+      'pagination'
+    ],
+    methods: {
+      paginate(pageNumber) {
+        this.$emit('paginate', pageNumber)
+      }
+    }
+  }
+</script>
+
+<style lag="scss">
+
+</style>
