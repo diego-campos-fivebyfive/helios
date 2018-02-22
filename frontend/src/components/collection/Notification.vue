711b55f551d7dcd18cd39660265b319a4936c039
Rafael Kendrik <rafamikovski@hotmail.com>
2018-02-22 15:08:03 -0300

    collection notification basic implements

---
+++ frontend/src/components/collection/Notification.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .collection-notification(v-show='open')
+    | {{ message }}
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      open: false,
+      message: ''
+    }),
+    methods: {
+      notify(message) {
+        this.open = true
+        this.message = message
+
+        setTimeout(() => {
+          this.open = false
+        }, 5000)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .collection-notification {
+    position: fixed;
+  }
+</style>
