2ca4513c73e6d6dc3a98b1c7984232b3b33c64b2
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-02-22 16:24:23 -0300

    ajuste na nomenclatura de variavel

--- backend/tests/App/Sices/Nfe/ProcessorTest.php
+++ backend/tests/App/Sices/Nfe/ProcessorTest.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Sices\Nfe\Parser;
 use App\Sices\Nfe\Processor;
 use Tests\AppBundle\AppTestCase;
 
@@ -32,33 +31,5 @@ class ProcessorNfeTest extends AppTestCase
             $filesIndexed
         );
         self::assertArrayNotHasKey('xml', $filesIndexed);
-    }
-
-    public function testMatchReference()
-    {
-        $manager = $this->manager('order');
-
-        $files = [
-            "3517111777450100012855001000010212100531131017110900920171123S",
-            "3517111777450100012855001000010213100466462417111308520171123S",
-            "3517111777450100012855001000010214100223243017111300320171123S",
-            "3517111777450100012855001000010215100535935817110801220171123S",
-            "3517111777450100012855001000010216100417411417103005420171123S"
-        ];
-
-        $parse = new Parser();
-        $filter = new Processor($manager);
-
-        foreach ($files as $file) {
-            $danfe = $parse::extract($file);
-            $order = $filter->matchReference($danfe);
-
-            if (!$order) {
-                continue;
-            }
-            var_dump($order);
-        }
-
-        die;
     }
 }
