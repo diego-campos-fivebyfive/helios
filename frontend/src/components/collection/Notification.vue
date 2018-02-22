f4d55d086275ab3cde579b4465366e1cba85e99d
Rafael Kendrik <rafamikovski@hotmail.com>
2018-02-22 17:37:52 -0300

    collection notitification basic style

--- frontend/src/components/collection/Notification.vue
+++ frontend/src/components/collection/Notification.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
   .collection-notification(v-show='open')
-    | {{ message }}
+    span.collection-notification-message
+      Icon(name='check', scale='1.5')
+      | {{ message }}
 </template>
 
 <script>
@@ -24,6 +26,25 @@
 
 <style lang="scss">
   .collection-notification {
+    left: 0;
+    padding-top: $ui-space-y/2;
     position: fixed;
+    right: 0;
+    text-align: center;
+    top: 0;
+  }
+
+  .collection-notification-message {
+    background-color: $ui-blue-light;
+    border-radius: $ui-corner;
+    box-shadow: 0 0 2px rgba(0, 0, 0, 0.25);
+    color: $ui-white-regular;
+    display: inline-block;
+    padding: $ui-space-y/1.5 $ui-space-x;
+
+    svg {
+      margin-right: $ui-space-x/2;
+      vertical-align: middle;
+    }
   }
 </style>
