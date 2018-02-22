4768280706856820fb7a4a57c2b850f21499190a
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-02-22 11:29:01 -0300

    transferência dos códigos de teste da DebugController para a processorTest

--- backend/tests/App/Sices/Nfe/ProcessorTest.php
+++ backend/tests/App/Sices/Nfe/ProcessorTest.php
@@ -1,12 +1,35 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 2/22/18
- * Time: 10:56 AM
- */
 
-class ProcessorTest
+use App\Sices\Nfe\Processor;
+use Tests\App\Sices\SicesTest;
+
+/**
+ * Class FileReaderTest
+ * @group sices_nfe_processor
+ */
+class ProcessorTest extends SicesTest
 {
 
+    public function testIndexer()
+    {
+        $filesList = [
+            "PR180212345678000112AA123123456789C12345678B171010000013201802051",
+            "PR180212345678000112AA123123456789C12345678B171010000013201802051.xml",
+            "PR180212345678000112AA123123456789C12345678B171010000013201802052.pdf",
+            "PR180212345678000112AA123123456789C12345678B171010000013201802052.xml",
+            "PR180212345678000112AA123123456789C12345678B171010000013201802053.pdf",
+            "PR180212345678000112AA123123456789C12345678B171010000013201802053.xml",
+        ];
+
+        $processor = new Processor();
+
+        $arrayIndexed = $processor->indexer($filesList);
+
+        self::assertCount(3, $arrayIndexed);
+        self::assertArrayHasKey(
+            'PR180212345678000112AA123123456789C12345678B171010000013201802051',
+            $arrayIndexed
+        );
+        self::assertArrayNotHasKey('xml', $arrayIndexed);
+    }
 }
