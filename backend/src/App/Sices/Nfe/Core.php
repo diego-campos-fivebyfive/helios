0659029da008288d93b59987f13acfab8418a8e9
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-02-23 14:53:21 -0300

    implementação da classe responsavel por interligar todos os serviços de processamento do Danfe

--- backend/src/App/Sices/Nfe/Core.php
+++ backend/src/App/Sices/Nfe/Core.php
@@ -8,8 +8,51 @@
 
 namespace App\Sices\Nfe;
 
+use App\Sices\Ftp\FileReader;
+use App\Sices\Ftp\FileSystemFactory;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class Core
 {
 
+    /**
+     * @var
+     */
+    private $container;
+
+    /**
+     * Core constructor.
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function core()
+    {
+        $parse = new Parser();
+        $processor = $this->container->get('nfe_processor');
+
+        $fileSystem = FileSystemFactory::create([
+            'host' => getenv('CES_SICES_FTP_HOST'),
+            'username' => getenv('CES_SICES_FTP_USER'),
+            'password' => getenv('CES_SICES_FTP_PASS'),
+            'directory' => '/DANFE'
+        ]);
+
+        $fileReader = new FileReader($fileSystem, $this->container);
+
+        $files = $processor->indexer($fileReader->files());
+
+        foreach ($files as $filename => $extensions) {
+            $danfe = $parse::extract($filename);
+            $order = $processor->matchReference($danfe);
+
+            if (!$order) {
+                continue;
+            }
+
+            $processor->setOrderDanfe($order, $danfe, $filename, $extensions);
+        }
+    }
 }
