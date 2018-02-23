bc58b41cc05f6799d02292594b02697addb55286
kolina-pc2 <fabiojd47@gmail.com>
2018-02-23 19:44:21 -0300

    integração de serviços para processamento de eventos

--- backend/src/App/Proceda/Processor.php
+++ backend/src/App/Proceda/Processor.php
@@ -1,11 +1,23 @@
 <?php
 
+/*
+ * This file is part of the SicesSolar package.
+ *
+ * (c) SicesSolar <http://sicesbrasil.com.br/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace App\Proceda;
 
 use App\Sices\Cache\JsonCache;
+use App\Sices\Ftp\FileReader;
+use App\Sices\Ftp\FileSystemFactory;
 use AppBundle\Entity\Order\Order;
 use AppBundle\Entity\Order\OrderInterface;
 use AppBundle\Manager\OrderManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class Processor
@@ -13,12 +25,20 @@ use AppBundle\Manager\OrderManager;
  */
 class Processor
 {
-    /**
-     * @var OrderManager
-     */
+    /** @var OrderManager */
     private $manager;
 
+    /** @var JsonCache */
     private $cache;
+
+    /** @var ContainerInterface */
+    private $container;
+
+    /** @var \Gaufrette\Filesystem */
+    private $fileSystem;
+
+    /** @var FileReader */
+    private $fileReader;
 
     const DELIVERING = ['000'];
 
@@ -26,21 +46,56 @@ class Processor
 
     const PROCEDA_CACHE = 'OCOREN';
 
+    const SEARCH_PREFIX = 'OCOREN';
+
     /**
      * Processor constructor.
-     * @param OrderManager $manager
+     * @param ContainerInterface $container
      */
-    function __construct(OrderManager $manager)
+    function __construct(ContainerInterface $container)
     {
-        $this->manager = $manager;
+        $this->container = $container;
+
+        $this->manager = $this->container->get('order_manager');
+
+        $this->fileSystem = $this->connect();
+
+        $this->fileReader = $this->container->get('file_reader');
+
+        $this->fileReader->init($this->fileSystem);
 
         $this->cache = JsonCache::create(self::PROCEDA_CACHE);
     }
 
-    public function processEvents($eventGroups)
+    /**
+     * event resolver
+     */
+    public function resolve()
+    {
+        $files = $this->fileReader->files(self::SEARCH_PREFIX);
+
+        $date = (new \DateTime())->format('Ymd');
+        $prefix = "PROCESSED-${date}-";
+
+        foreach ($files as $filename) {
+            $content = $this->fileSystem->read($filename);
+
+            $events = Parser::fromContent($content);
+
+            $this->mergeEventsAndCache($events);
+
+            $this->processEvents($this->cache->all());
+
+            $this->fileReader->prefixer($filename, $prefix);
+        }
+    }
+
+    /**
+     * @param $eventGroups
+     */
+    private function processEvents($eventGroups)
     {
         foreach ($eventGroups as $invoice => $group) {
-
             /** @var Order $order */
             $order = $this->manager->findOneBy([
                 'invoiceNumber' => $invoice
@@ -51,10 +106,12 @@ class Processor
                     $this->changeStatusByEvent($order, $event['event']);
                     // TODO: chamar Timeline
                 }
-                // TODO: remover do cache
+
+                $this->cache->remove($invoice);
             }
         }
-        // TODO: salvar cache
+
+        $this->cache->store();
     }
 
     /**
@@ -65,21 +122,37 @@ class Processor
     {
         $status = $order->getStatus();
 
-        if (in_array($event, self::DELIVERING)) {
-            $status = OrderInterface::STATUS_DELIVERING;
-        } elseif (in_array($event, self::DELIVERED)) {
-            $status = OrderInterface::STATUS_DELIVERED;
+        if ($status != OrderInterface::STATUS_DELIVERED
+            && in_array($event, self::DELIVERING)) {
+            $order->setStatus(OrderInterface::STATUS_DELIVERING);
+            $this->manager->save($order);
+        } elseif ($status != OrderInterface::STATUS_DELIVERED
+            && in_array($event, self::DELIVERED)) {
+            $order->setStatus(OrderInterface::STATUS_DELIVERED);
+            $this->manager->save($order);
         }
-
-        $order->setStatus($status);
-
-        $this->manager->save($order);
     }
 
-    public function mergeEventsAndCache($events)
+    /**
+     * @param $events
+     */
+    private function mergeEventsAndCache($events)
     {
         foreach ($events as $event) {
             $this->cache->incrementInArrayPosition($event['invoice'], $event);
         }
+    }
+
+    /**
+     * @return \Gaufrette\Filesystem
+     */
+    private function connect()
+    {
+        return FileSystemFactory::create([
+            'host' => getenv('CES_SICES_FTP_HOST'),
+            'username' => getenv('CES_SICES_FTP_USER'),
+            'password' => getenv('CES_SICES_FTP_PASS'),
+            'directory' => '/PROCEDA-SICESSOLAR'
+        ]);
     }
 }
