2ec7ff9cb302d0348b6cea6eec1cdd4c3e37b075
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-02-27 18:17:00 -0300

    implementação da classe de OrderCoupon e metodo para geração de opções do valor de coupon

--- backend/src/AppBundle/Service/Order/OrderCoupon.php
+++ backend/src/AppBundle/Service/Order/OrderCoupon.php
@@ -1,15 +1,55 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 2/27/18
- * Time: 4:59 PM
- */
 
 namespace AppBundle\Service\Order;
 
+use AppBundle\Entity\Order\Order;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class OrderCoupon
 {
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
 
+    /**
+     * OrderCoupon constructor.
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param Order $order
+     * @return array
+     */
+    public function generateOptions(Order $order)
+    {
+        $account = $order->getAccount();
+
+        $options = [];
+
+        $params = [
+            'percent' => 0.5,
+            'step' => 500
+        ];
+
+        if ($account->getRanking() < $params['step']) {
+            return $options;
+        }
+
+        $limit = $order->getTotal() * $params['percent'];
+
+        $stepLimit = $limit < $account->getRanking() ? $limit : $account->getRanking();
+
+        $steps = floor($stepLimit / $params['step']);
+
+        for ($i = 0; $i < $steps; $i++) {
+            $options[] = $params['step'] * ($i + 1);
+        }
+
+        return $options;
+    }
 }
