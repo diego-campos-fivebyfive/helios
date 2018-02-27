ca1168d7ef3e379e3c3b5ede04a0db621916ea37
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-02-27 16:35:45 -0300

    implementação do serviço transformer, com metodos para criação de coupon para conta

--- backend/src/AppBundle/Service/Coupon/Transformer.php
+++ backend/src/AppBundle/Service/Coupon/Transformer.php
@@ -1,15 +1,65 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 2/27/18
- * Time: 8:43 AM
- */
 
 namespace AppBundle\Service\Coupon;
 
+use AppBundle\Entity\AccountInterface;
+use AppBundle\Entity\Misc\Coupon;
+use AppBundle\Manager\CouponManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class Transformer
 {
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * @var CouponManager
+     */
+    private $manager;
+
+    /**
+     * Transformer constructor.
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+        $this->manager = $this->container->get('coupon_manager');
+    }
+
+    /**
+     * @param AccountInterface $account
+     */
+    public function fromAccount(AccountInterface $account, $amount)
+    {
+        if ($account->getRanking() >= $amount) {
+
+            $coupon = $this->manager->create();
+            $name = "Cupom de desconto";
+
+            $coupon
+                ->setAmount($amount)
+                ->setName($name)
+                ->setAccount($account);
+            $this->manager->save($coupon);
+
+            $this->addCodeCoupon($coupon);
+        }
+    }
+
+    /**
+     * @param $coupon
+     */
+    private function addCodeCoupon(Coupon $coupon)
+    {
+        $code = substr(strtoupper(md5(uniqid()) . $coupon->getId()), -6);
+
+        $coupon->setCode($code);
+
+        $this->manager->save($coupon);
+    }
 
 }
