e8cd37cb4ca5f091356124aa843427a85d5f16d6
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-02-28 10:25:10 -0300

    implementação da função responsavel por gerar um cupom e criar uma transação negativa no ranking

--- backend/src/AppBundle/Service/Order/OrderCoupon.php
+++ backend/src/AppBundle/Service/Order/OrderCoupon.php
@@ -4,6 +4,8 @@ namespace AppBundle\Service\Order;
 
 use AppBundle\Entity\Order\Order;
 use AppBundle\Manager\ParameterManager;
+use AppBundle\Service\Business\RankingGenerator;
+use AppBundle\Service\Coupon\Transformer;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class OrderCoupon
@@ -47,5 +49,34 @@ class OrderCoupon
         $ranges = range($step, intval($discountLimit), $step);
 
         return $ranges;
+    }
+
+    public function generateCoupon(Order $order, $amount)
+    {
+        $ranges = $this->generateOptions($order);
+
+        if (!in_array($amount, $ranges)) {
+            return [];
+        }
+
+        $account = $order->getAccount();
+
+        /** @var Transformer $transformer */
+        $transformer = $this->container->get('coupon_transformer');
+        $coupon = $transformer->fromAccount($account, $amount);
+
+        if (!$coupon) {
+            return [];
+        }
+
+        $this->debitRanking($account,$coupon, $amount);
+    }
+
+    private function debitRanking($account, $coupon, $amount)
+    {
+        $debitAmount = - $amount;
+        /** @var RankingGenerator $rankingGenerator */
+        $rankingGenerator = $this->container->get('ranking_generator');
+        $rankingGenerator->create($account, $coupon->getName(), $debitAmount);
     }
 }
