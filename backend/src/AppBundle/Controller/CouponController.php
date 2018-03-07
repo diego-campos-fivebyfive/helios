609fb23812f378740deb909d0ae8ee13347c353e
kolina-pc2 <fabiojd47@gmail.com>
2018-03-07 17:18:46 -0300

    ajuste em orderBy

--- backend/src/AppBundle/Controller/CouponController.php
+++ backend/src/AppBundle/Controller/CouponController.php
@@ -31,7 +31,7 @@ class CouponController extends AbstractController
     {
         $qb = $this->manager('coupon')->createQueryBuilder();
         $qb->orderBy('c.appliedAt', 'ASC');
-        $qb->addorderBy('c.id', 'DESC');
+        $qb->addOrderBy('c.id', 'DESC');
 
         $accounts = $request->get('account');
         $name = $request->get('name');
