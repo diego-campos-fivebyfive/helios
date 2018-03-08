5429201e939e6badea47518d726a6ef5b0525f7e
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-03-08 10:30:57 -0300

    exibir notificação a frente da modal

---
+++ frontend/src/components/collection/Money.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  Money(v-model='price', v-bind='money')
+  span {{price}}
+</template>
+
+<script>
+  import { Money } from 'v-money'
+
+  export default {
+    components: {
+      Money
+    },
+    props: [
+      'price'
+    ],
+    data: () => ({
+      money: {
+        decimal: ',',
+        thousands: '.',
+        prefix: 'R$ ',
+        suffix: ' #',
+        precision: 2,
+        masked: false
+      }
+    })
+  }
+</script>
+
+<style lang="scss">
+  /* Money Style */
+</style>
