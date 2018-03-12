f50e6805b951700218ade19dc8f202b238a14b33
Claudinei M <cjchamado@gmail.com>
2018-03-12 11:35:56 -0300

    refatorado método de agrupamento

--- backend/src/AppBundle/Service/Order/StockChecker.php
+++ backend/src/AppBundle/Service/Order/StockChecker.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Service\Order;
 
+use AppBundle\Entity\Order\Element;
 use AppBundle\Entity\Order\Order;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -27,34 +28,50 @@ class StockChecker
      */
     public function groupComponents(Order $order)
     {
-        $groups = [];
+        $group = [];
 
         if($order->isMaster()) {
 
             /** @var Order $suborder */
             foreach ($order->getChildrens() as $suborder) {
-
-                foreach ($suborder->getElements() as $element) {
-
-                    $family = $element->getFamily();
-                    $code = $element->getCode();
-
-                    if (!array_key_exists($family, $groups)) {
-                        $groups[$family] = [];
-                    }
-
-                    if (!array_key_exists($code, $groups[$family])) {
-                        $groups[$family][$code] = [
-                            'description' => $element->getDescription(),
-                            'quantity' => 0
-                        ];
-                    }
-
-                    $groups[$family][$code]['quantity'] += $element->getQuantity();
-                }
+                $this->addSuborderElementsOnGroup($suborder, $group);
             }
         }
 
-        return $groups;
+        return $group;
+    }
+
+    /**
+     * @param Order $suborder
+     * @param array $group
+     */
+    private function addSuborderElementsOnGroup(Order $suborder, array &$group = [])
+    {
+        foreach ($suborder->getElements() as $element) {
+            $this->addSuborderElementOnGroup($element, $group);
+        }
+    }
+
+    /**
+     * @param Element $element
+     * @param array $group
+     */
+    private function addSuborderElementOnGroup(Element $element, array &$group = [])
+    {
+        $family = $element->getFamily();
+        $code = $element->getCode();
+
+        if (!array_key_exists($family, $group)) {
+            $group[$family] = [];
+        }
+
+        if (!array_key_exists($code, $group[$family])) {
+            $group[$family][$code] = [
+                'description' => $element->getDescription(),
+                'quantity' => 0
+            ];
+        }
+
+        $group[$family][$code]['quantity'] += $element->getQuantity();
     }
 }
