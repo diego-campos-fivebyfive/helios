aba13d3b75df699994a5aecd1d2f823a1c846130
Rafael Kendrik <rafamikovski@hotmail.com>
2018-03-13 10:34:41 -0300

    (Imp - #1743) Spliting ContentForm actions to ActionForm pattern

--- frontend/src/components/pages/coupon/ContentForm.vue
+++ frontend/src/components/pages/coupon/ContentForm.vue
@@ -19,11 +19,9 @@
             :currentAccount='form.payload.account')
     ActionForm(
       slot='buttons',
-      icon='save',
-      type='primary-strong',
-      label='Salvar',
-      pos='single',
-      v-on:click.native='send')
+      :action='form.action',
+      :disabled='form.disabled',
+      :payload='form.payload')
 </template>
 
 <script>
@@ -37,6 +35,8 @@
     },
     data: () => ({
       form: {
+        action: '',
+        disabled: false,
         default: {
           name: '',
           amount: null,
@@ -46,57 +46,38 @@
       }
     }),
     methods: {
-      updateAccount(account) {
-        this.form.payload.account = {
-          id: account.value,
-          name: account.text
-        }
-      },
-      createCoupon() {
-        this.axios.post('api/v1/coupon/', this.form.payload)
-          .then(() => {
-            this.$emit('getCoupons')
-            this.$refs.modalForm.notify('Cupom cadastrado com sucesso')
-          })
-          .catch(() => {
-            this.$refs.modalForm.notify('Não foi possível cadastrar cupom')
-          })
-      },
-      editCoupon() {
-        this.axios.put(`api/v1/coupon/${this.form.id}`, this.form.payload)
-          .then(() => {
-            this.$emit('getCoupons')
-            this.$refs.modalForm.notify('Cupom editado com sucesso')
-          })
-          .catch(() => {
-            this.$refs.modalForm.notify('Não foi possível editar cupom')
-          })
-      },
-      send() {
-        if (!/^(\d{1,3}(\.\d{3})*|\d+)(\,\d{2})?$/.test(this.form.payload.amount)) {
-          this.$refs.modalForm.notify('Formato de moeda em Real invalido')
+      isValidAmount() {
+        if (/^(\d{1,3}(\.\d{3})*|\d+)(\,\d{2})?$/.test(this.form.payload.amount)) {
+          this.disabled = false
           return
         }
 
+        this.$refs.modalForm.notify('Formato de moeda em Real invalido')
+        this.disabled = true
+      },
+      closeModalForm(response) {
         this.$refs.modalForm.hide()
 
-        if (this.modal.action === 'create') {
-          this.createCoupon()
+        response
+          .then(message => {
+            this.$emit('getCoupons')
+            this.$refs.modalForm.notify(message)
+          })
+          .catch(message => {
+            this.$refs.modalForm.notify(message)
+          })
+      },
+      showModalForm(coupon) {
+        this.$refs.modalForm.show()
+
+        if (coupon) {
+          this.form.action = 'edit'
+          this.form.payload = Object.assign({}, coupon)
           return
         }
 
-        this.editCoupon()
-      },
-      showModalForm(coupon) {
-        if (coupon) {
-          this.modal.action = 'edit'
-          this.form.payload = Object.assign({}, coupon)
-        } else {
-          this.modal.action = 'create'
-          this.from.payload = this.from.default
-        }
-
-        this.$refs.modalForm.show()
+        this.form.action = 'create'
+        this.form.payload = Object.assign({}, this.form.default)
       }
     }
   }
