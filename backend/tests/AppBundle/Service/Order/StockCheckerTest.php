ecd3061306961906b83a609c800d5f0313a3afb5
Gianluca Bine <gian_bine@hotmail.com>
2018-03-13 17:09:37 -0300

    Refatoração de código

--- backend/tests/AppBundle/Service/Order/StockCheckerTest.php
+++ backend/tests/AppBundle/Service/Order/StockCheckerTest.php
@@ -62,7 +62,7 @@ class StockCheckerTest extends AppTestCase
 
         // Código que não pode estar no componentsOutOfStock porque familia variety não está
         // nos params
-        $code = $components['variety'][0]->getCode();
+        $code = $components['module'][2]->getCode();
 
         foreach ($componentsOutOfStock as $componentOutOfStock) {
             $this->assertLessThan($componentOutOfStock['quantity'], $componentOutOfStock['stock']);
