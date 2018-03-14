30034d5282b8f95408e4205a424115e4f1930545
Gianluca Bine <gian_bine@hotmail.com>
2018-03-14 13:52:42 -0300

    Teste para verificar o result do core com os logs de processamento

--- backend/tests/App/Sices/Nfe/CoreTest.php
+++ backend/tests/App/Sices/Nfe/CoreTest.php
@@ -1,7 +1,42 @@
 <?php
+
+use App\Sices\Nfe\Core;
+use Tests\AppBundle\AppTestCase;
+
 /**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 3/14/18
- * Time: 11:38 AM
+ * Class CoreNfeTest
+ * @group sices_nfe_core
  */
+class CoreNfeTest extends AppTestCase
+{
+
+    public function testCore()
+    {
+        $orderManager = $this->getContainer()->get('order_manager');
+
+        $order = $orderManager->create();
+
+        $order->setReference('171128008');
+
+        $orderManager->save($order);
+
+        $core = new Core($this->getContainer());
+
+        $result = $core->core();
+
+        $this->assertEquals($result['loaded_files'], 1);
+        $this->assertEquals($result['processed_files'], 1);
+
+//        $files = [
+//            "3517111777450100012855001000012345100466462417112800820171120S.XML"
+//        ];
+//        $filesIndexed = $processor->indexer($files);
+//
+//        self::assertCount(1, $filesIndexed);
+//        self::assertArrayHasKey(
+//            '3517111777450100012855001000012345100466462417112800820171120S',
+//            $filesIndexed
+//        );
+//        self::assertArrayNotHasKey('xml', $filesIndexed);
+    }
+}
