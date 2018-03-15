ddc962fa1b4335e2c26f1c1300868960a704587a
Gianluca Bine <gian_bine@hotmail.com>
2018-03-15 08:29:40 -0300

    Adicionado método de formatação de order master para extrair dados

--- backend/src/AppBundle/Service/Order/OrderExporter.php
+++ backend/src/AppBundle/Service/Order/OrderExporter.php
@@ -14,6 +14,7 @@ namespace AppBundle\Service\Order;
 use AppBundle\Entity\Component\ComponentInterface;
 use AppBundle\Entity\Component\MakerInterface;
 use AppBundle\Entity\Order\Element;
+use AppBundle\Entity\Order\Order;
 use AppBundle\Entity\Order\OrderInterface;
 use Exporter\Writer\CsvWriter;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -55,6 +56,36 @@ class OrderExporter
         $this->collector = $container->get('component_collector');
         $this->storage = $container->get('app_storage');
         $this->manager = $container->get('order_manager');
+    }
+
+    /**
+     * @param Order $order
+     */
+    public function extractOrderData(Order $order)
+    {
+        $orderData = [
+            'reference' => $order->getReference(),
+            'status' => $this->getStatusNameInPortuguese()[$order->getStatus()],
+            'send_at' => $this->formatDate($order->getSendAt()),
+            'account' => $order->getAccount()->getName(),
+            'cnpj' => $order->getCnpj(),
+            'level' => $order->getLevel(),
+            //'agent' => $order->getAgent()->getName(),
+            'sub_orders' => count($order->getChildrens()),
+            'power' => $order->getPower() . " kWp",
+            'total' => $this->formatMoney($order->getTotal()),
+            'shipping_type' => $order->getShippingRules()['type'],
+            'shipping_price' => $this->formatMoney($order->getShipping()),
+            'payment_method' => $order->getPaymentMethod('array')['name'],
+            'delivery_at' => $this->formatDate($order->getDeliveryAt()),
+            'note' => $order->getNote(),
+            'billing_name' => $order->getBillingFirstname(),
+            'billing_cnpj' => $order->getBillingCnpj(),
+            'invoices' => implode(", ", $order->getInvoices()),
+            'billed_at' => $this->formatDate($order->getBilledAt())
+        ];
+
+        return $orderData;
     }
 
     /**
@@ -136,6 +167,43 @@ class OrderExporter
         ];
 
         return $data;
+    }
+
+    private function getStatusNameInPortuguese()
+    {
+        return [
+            'Editando',
+            'Pendente',
+            'Validado',
+            'Aprovado',
+            'Cancelado',
+            'Confirmado',
+            'Em produção',
+            'Coleta disponível',
+            'Coletado',
+            'Em trânsito',
+            'Entregue'
+        ];
+    }
+
+    /**
+     * @param $money
+     * @return mixed
+     */
+    private function formatMoney($money) {
+
+        $formatedMoney = 'R$ '. number_format($money, 2);
+
+        return str_replace('.',',', $formatedMoney);
+    }
+
+    /**
+     * @param $date
+     * @return mixed
+     */
+    private function formatDate($date)
+    {
+        return $date->format('d/m/Y');
     }
 
     /**
