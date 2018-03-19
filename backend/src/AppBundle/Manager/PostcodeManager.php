518953f28a92f1ecb0b1078a737bd33dcf56bfde
Fabio Dukievicz <fabiojd47@gmail.com>
2018-03-19 09:32:08 -0300

    manager de post code

--- backend/src/AppBundle/Manager/PostcodeManager.php
+++ backend/src/AppBundle/Manager/PostcodeManager.php
@@ -1,15 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 16/03/18
- * Time: 18:30
- */
 
 namespace AppBundle\Manager;
 
-
-class PostcodeManager
+class PostcodeManager extends AbstractManager
 {
 
 }
