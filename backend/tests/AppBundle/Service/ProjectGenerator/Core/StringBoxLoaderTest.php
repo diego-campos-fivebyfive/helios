6db9ef5942f28501f7cd23ebe33d24578359bdb9
Gianluca Bine <gian_bine@hotmail.com>
2018-04-03 14:02:20 -0300

    Implementado nova classe de carregamento de string box

---
+++ backend/tests/AppBundle/Service/ProjectGenerator/Core/StringBoxLoaderTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kolinalabs
+ * Date: 4/3/18
+ * Time: 11:11 AM
+ */
+
+namespace Tests\AppBundle\Service\ProjectGenerator\Core;
+
+use StringBoxLoaderTest;
+
+class StringBoxLoaderTest extends \PHPUnit_Framework_TestCase
+{
+
+}
