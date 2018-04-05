0b732c79431215613b3e4eab5dd70058b2f3215d
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-04-05 11:53:41 -0300

    Tranferido as classes de input e select do Form para seus respectivos componentes

--- frontend/src/theme/collection/Input.vue
+++ frontend/src/theme/collection/Input.vue
@@ -22,6 +22,33 @@
 </script>
 
 <style lang="scss" scoped>
+  label {
+    float: left;
+    font-weight: 600;
+    padding: $ui-space-y/2 $ui-space-x/2;
+  }
+
+  input {
+    background-color: $ui-white-regular;
+    border: 1px solid $ui-gray-light;
+    border-radius: 1px;
+    color: $ui-gray-dark;
+    display: block;
+    height: $ui-action-y;
+    padding: 0 $ui-space-x/2;
+    margin-top: $ui-space-y/2;
+    width: 100%;
+    transition:
+      border-color 150ms ease-in-out 0s,
+      box-shadow 150ms ease-in-out 0s;
+
+    @include placeholder-color($ui-gray-regular);
+
+    &:focus {
+      border-color: $ui-blue-light;
+    }
+  }
+
   .danger-common {
     color: $ui-red-dark;
     opacity: 0.75;
