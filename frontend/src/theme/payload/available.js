aab5a81ea5ef75423df20a504fb1d6dd58dcb3d8
Rafael Kendrik <rafamikovski@hotmail.com>
2018-04-10 18:03:02 -0300

    (Pro - #2200) Implement available method to check if the payload is ready to go

--- frontend/src/theme/payload/available.js
+++ frontend/src/theme/payload/available.js
@@ -1,29 +1,6 @@
-const checkPayload = payload => {
-  /* eslint-disable no-use-before-define, no-restricted-syntax */
-  const isResolved = (obj, key) => {
-    const val = obj[key]
+const available = payload =>
+  payload.reduce((acc, item) => (
+    acc && !item.rejected
+  ), true)
 
-    if (val === Object(val)) {
-      return isValid(val)
-    }
-
-    return (key === 'rejected' && val)
-      ? !isInvalidField(obj)
-      : true
-  }
-
-  const isValid = obj => {
-    for (const key in obj) {
-      if (!isResolved(obj, key)) return false
-    }
-
-    return true
-  }
-
-  return isValid(payload)
-  /* eslint-enable no-use-before-define, no-restricted-syntax */
-}
-
-const check = payload => true
-
-export default check
+export default available
