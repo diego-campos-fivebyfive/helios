2ed1681b27f9df0b9cf94cbbb31ec04c7176857e
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-04-16 11:41:34 -0300

    implementação do component Chechebox e definição de layout

--- frontend/src/theme/collection/Checkbox.vue
+++ frontend/src/theme/collection/Checkbox.vue
@@ -1,13 +1,57 @@
-<template>
-  $END$
+<template lang="pug">
+  label.collection-checkbox
+    | {{ field.label }}
+    input.collection-checkbox-field(
+      type='checkbox',
+      :value='field.value',
+      v-on:input='$set(field, "value", $event.target.value)')
+    Icon.collection-checkbox-icon(name='check')
 </template>
 
 <script>
   export default {
-    name: "checkbox"
+    props: [
+      'field'
+    ]
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .collection-checkbox {
+    float: left;
+    font-weight: 600;
+    padding: $ui-space-y/2 $ui-space-x/2;
+    position: relative;
+    max-width: 1.75rem;
+    width: 100%;
+    height: 1.75rem;
+    border: 1px solid $ui-gray-light;
 
+    &:hover {
+      border-color: $ui-blue-dark;
+    }
+  }
+
+  .collection-checkbox-field {
+    display: none;
+    transition: opacity 0.1s ease-in;
+
+    &:checked {
+      + .collection-checkbox-icon {
+        background-color: $ui-blue-dark;
+        color: $ui-white-regular;
+        top: 0;
+        left: 0;
+        opacity: 1;
+        position: absolute;
+        padding: 0.25rem;
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+
+  .collection-checkbox-icon {
+    opacity: 0;
+  }
 </style>
