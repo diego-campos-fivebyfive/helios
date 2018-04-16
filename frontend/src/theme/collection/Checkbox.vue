62d1f0673058eba29b6540b6090e014e20bf8c09
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-04-16 14:19:05 -0300

    estilização do componente checkbox

--- frontend/src/theme/collection/Checkbox.vue
+++ frontend/src/theme/collection/Checkbox.vue
@@ -1,11 +1,15 @@
 <template lang="pug">
   label.collection-checkbox
     | {{ field.label }}
-    input.collection-checkbox-field(
-      type='checkbox',
-      :value='field.value',
-      v-on:input='$set(field, "value", $event.target.value)')
-    Icon.collection-checkbox-icon(name='check')
+    .collection-checkbox-wrapper
+      .collection-checkbox-wrapper-checker
+        input.collection-checkbox-wrapper-checker-field(
+          type='checkbox',
+          :value='field.value',
+          v-on:input='$set(field, "value", $event.target.value)')
+        Icon.collection-checkbox-wrapper-checker-icon(name='check')
+      .collection-checkbox-wrapper-description
+        | {{ field.description }}
 </template>
 
 <script>
@@ -19,25 +23,33 @@
 <style lang="scss" scoped>
   .collection-checkbox {
     float: left;
-    font-weight: 600;
     padding: $ui-space-y/2 $ui-space-x/2;
-    position: relative;
-    max-width: 1.75rem;
-    width: 100%;
-    height: 1.75rem;
-    border: 1px solid $ui-gray-light;
 
     &:hover {
       border-color: $ui-blue-dark;
     }
   }
 
-  .collection-checkbox-field {
+  .collection-checkbox-wrapper {
+    display: flex;
+    margin-top: $ui-space-y/2;
+  }
+
+  .collection-checkbox-wrapper-checker {
+    border: 1px solid $ui-gray-light;
+    height: 1.75rem;
+    width: 100%;
+    max-width: 1.75rem;
+    margin-top: 0.25rem;
+    position: relative;
+  }
+
+  .collection-checkbox-wrapper-checker-field {
     display: none;
     transition: opacity 0.1s ease-in;
 
     &:checked {
-      + .collection-checkbox-icon {
+      + .collection-checkbox-wrapper-checker-icon {
         background-color: $ui-blue-dark;
         color: $ui-white-regular;
         top: 0;
@@ -51,7 +63,12 @@
     }
   }
 
-  .collection-checkbox-icon {
+  .collection-checkbox-wrapper-checker-icon {
     opacity: 0;
+  }
+
+  .collection-checkbox-wrapper-description {
+    font-weight: 700;
+    margin-left: $ui-space-y/2;
   }
 </style>
