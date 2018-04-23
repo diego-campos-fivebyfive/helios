014dc14563f8bb3dfe62de73f687624e37d2bbef
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-04-23 16:40:15 -0300

    implementação do Panel para listagem de mensagens

---
+++ frontend/src/pages/messenger/index.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  .wrapper
+    Panel.panel
+      div(slot='header')
+        slot(name='head')
+          h1.title
+            | Mensagens ({{ messages }})
+          Search
+        slot(name='actions')
+          ActionBar.action-bar
+      List(
+        slot='section')
+</template>
+
+<script>
+  import List from './list'
+  import Search from '@/theme/collection/Search'
+
+  export default {
+    components: {
+      List,
+      Search
+    },
+    data: () => ({
+      messages: 11
+    })
+  }
+</script>
+
+<style lang="scss" scoped>
+  .action-bar {
+    margin-top: 1.5rem;
+  }
+</style>
