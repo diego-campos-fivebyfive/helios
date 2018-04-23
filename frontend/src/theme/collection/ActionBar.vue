841cd52f590c03700ecf37adc7d58645146ccad5
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-04-23 11:14:24 -0300

    implementação e estilização do componente ActionBar

--- frontend/src/theme/collection/ActionBar.vue
+++ frontend/src/theme/collection/ActionBar.vue
@@ -1,5 +1,30 @@
-<template>
-  $END$
+<template lang="pug">
+  .collection-action-bar
+    Button.collection-action-bar-left(
+      type='default-bordered',
+      icon='refresh',
+      label='Atualizar',
+      pos='single')
+    Button.collection-action-bar-left(
+      type='default-bordered',
+      icon='eye',
+      pos='single')
+    Button.collection-action-bar-left(
+      type='default-bordered',
+      icon='exclamation',
+      pos='single')
+    Button.collection-action-bar-left(
+      type='default-bordered',
+      icon='trash',
+      pos='single')
+    Button.collection-action-bar-right(
+      type='default-bordered',
+      icon='arrow-left',
+      pos='last')
+    Button.collection-action-bar-right(
+      type='default-bordered',
+      icon='arrow-right',
+      pos='first')
 </template>
 
 <script>
@@ -8,6 +33,17 @@
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .collection-action-bar {
+    width: 100%;
 
+    .collection-action-bar-left {
+      float: left;
+      margin-right: 0.25rem;
+    }
+
+    .collection-action-bar-right {
+      float: right;
+    }
+  }
 </style>
