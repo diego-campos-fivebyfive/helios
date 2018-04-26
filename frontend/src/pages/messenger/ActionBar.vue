aab0737cc7e0be594caa1b35ad92232c86e2bd33
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-04-26 09:40:05 -0300

    implementação de promise no metodo markIsRead, e correções de lint

--- frontend/src/pages/messenger/ActionBar.vue
+++ frontend/src/pages/messenger/ActionBar.vue
@@ -23,6 +23,7 @@
     props: [
       'getMessages',
       'checkedMessagesClear',
+      'incrementCheckedMessages',
       'messages',
       'pagination'
     ],
@@ -70,18 +71,23 @@
         }
       },
       markIsRead() {
-        const messagesIds = this.messages
-          .filter(message => message.value && !message.isRead)
-          .map(message => message.id)
+        this.incrementCheckedMessages().then(messages => {
+          const messagesIds = messages
+            .filter(message => !message.isRead)
+            .map(message => message.id)
 
-        if (messagesIds.length > 0) {
-          const data = { messagesIds }
+          if (messagesIds.length > 0) {
+            const data = { messagesIds }
 
-          const uri = 'admin/api/v1/orders/messages/mark_as_read'
+            const uri = 'admin/api/v1/orders/messages/mark_as_read'
 
-          this.axios.post(uri, data)
-          this.checkedMessagesClear()
-        }
+            this.axios.post(uri, data)
+              .then(() => {
+                this.checkedMessagesClear()
+              })
+              .catch(() => 'Não foi possível marcar mensagens como lidas')
+          }
+        })
       }
     }
   }
