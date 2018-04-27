08abec3dac1e9a445d874e769a1a88d992119898
Fabio Dukievicz <fabiojd47@gmail.com>
2018-04-27 14:18:37 -0300

    testes em serviço de Termos de uso

--- backend/tests/AppBundle/Service/Business/TermsCheckerTest.php
+++ backend/tests/AppBundle/Service/Business/TermsCheckerTest.php
@@ -1,39 +1,83 @@
 <?php
 
-namespace Tests\AppBundle\Service\Additive;
+namespace Tests\AppBundle\Service\Business;
 
-use AppBundle\Entity\Misc\Additive;
-use AppBundle\Entity\Order\Order;
-use AppBundle\Entity\Order\OrderAdditive;
-use AppBundle\Entity\Pricing\Memorial;
-use Tests\AppBundle\AppTestCase;
+use AppBundle\Entity\TermInterface;
+use AppBundle\Manager\TermManager;
+use AppBundle\Service\Business\TermsChecker;
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+
 
 /**
- * Class TermsTest
- * @group terms_synchronize
+ * Class TermsCheckerTest
+ * @group terms_checker
  */
-class TermsTest extends AppTestCase
+class TermsCheckerTest extends WebTestCase
 {
 
     public function testSynchronize()
     {
-        $manager = $this->manager('order');
-        $synchronizer = $this->getSynchronizer();
+        /** @var TermManager $manager */
+        $manager = $this->getContainer()->get('term_manager');
 
-        $partnerAdditives = $this->createAdditives(5, [
-            'requiredLevels' => ['partner']
-        ]);
+        foreach ($manager->findAll() as $item) {
+            $manager->delete($item);
+        }
 
-        $order = $manager->create();
-        $order->setLevel(Memorial::LEVEL_PARTNER);
+        /** @var TermInterface $term */
+        $term = $this->createTerm('2017-01-01');
 
-        $addPartnerAdditives = array_slice($partnerAdditives, 0, 3);
-        $this->assertCount(3, $addPartnerAdditives);
+        /** @var TermInterface $term1 */
+        $term1 = $this->createTerm('2018-01-01');
 
-        $this->associateAdditives($order, $addPartnerAdditives);
+        /** @var TermInterface $term2 */
+        $term2 = $this->createTerm('now');
 
-        $partnerAdditivesLoaded = $synchronizer->getUnlinkedAdditivesByRequiredLevels($order);
+        /** @var TermsChecker $checker */
+        $checker = $this->getContainer()->get('terms_checker');
 
-        $this->assertCount(2, $partnerAdditivesLoaded);
+        $terms = [
+            $term->getId() => ['checkedAt' => 1483236001], // aceito
+            $term1->getId() => ['checkedAt' => 1514771000], // precisa aceitar
+        ];
+
+        self::assertEquals(2, count($terms));
+        $all = $checker->synchronize($terms)->all();
+        self::assertEquals(3, count($all));
+        self::assertEquals(3, count($terms));
+
+        $terms = [
+            $term->getId() => ['checkedAt' => 1483236001], // aceito
+            $term1->getId() => ['checkedAt' => 1514771000], // precisa aceitar
+        ];
+        $checked = $checker->synchronize($terms)->checked();
+        self::assertEquals(1, count($checked));
+        self::assertEquals(3, count($terms));
+
+        $terms = [
+            $term->getId() => ['checkedAt' => 1483236001], // aceito
+            $term1->getId() => ['checkedAt' => 1514771000], // precisa aceitar
+        ];
+        $unchecked = $checker->synchronize($terms)->unchecked();
+        self::assertEquals(2, count($unchecked));
+        self::assertEquals(3, count($terms));
+    }
+
+    private function createTerm($date)
+    {
+        /** @var TermManager $manager */
+        $manager = $this->getContainer()->get('term_manager');
+
+        /** @var TermInterface $term */
+        $term = $manager->create();
+
+        $term->setTitle('termo x');
+        $term->setUrl('www');
+        $term->setUpdatedAt(new \DateTime($date));
+        $term->setCreatedAt(new \DateTime());
+
+        $manager->save($term);
+
+        return $term;
     }
 }
