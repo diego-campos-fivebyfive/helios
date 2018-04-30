6540c9ed47d000d670587449d868cf38f60c6338
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-04-30 16:14:39 -0300

    implementação da tabela de listagem dos termos de uso

--- frontend/src/pages/termsofuse/list/index.vue
+++ frontend/src/pages/termsofuse/list/index.vue
@@ -1,30 +1,38 @@
 <template lang="pug">
-  Table.table(type='stripped')
-    tr(slot='head')
-      th.col-name Título
-      th.col-code Url
-      th.col-account Publicação
-      th.col-action Ações
-    tr.rows(slot='rows', v-for='term in terms')
-      td.col-name {{ term.title }}
-      td.col-code {{ term.url }}
-      td.col-account {{ term.publishedAt }}
-      td.col-action
-        Button(
-          type='primary-common',
-          icon='pencil',
-          pos='first')
-        Button(
-          type='danger-common',
-          icon='trash',
-          pos='last')
+  div
+    Table.table(type='stripped')
+      tr(slot='head')
+        th.col-title Título
+        th.col-url Url
+        th.col-published Publicação
+        th.col-action Ações
+      tr.rows(slot='rows', v-for='term in terms')
+        td.col-title {{ term.title }}
+        td.col-url
+          a(:href='term.url') {{ term.url }}
+        td.col-published {{ formatDate(term.publishedAt) }}
+        td.col-action
+          Button(
+            type='primary-common',
+            icon='pencil',
+            pos='first')
+          Button(
+            type='danger-common',
+            icon='trash',
+            pos='last')
 </template>
 
 <script>
   export default {
     props: [
       'terms'
-    ]
+    ],
+    methods: {
+      formatDate(date) {
+        const moment = this.$moment(date, 'YYYY-MM-DD, hh:mm a')
+        return moment.format('DD/MM/YYYY, hh:mm a')
+      }
+    }
   }
 </script>
 
@@ -33,29 +41,31 @@
     cursor: pointer;
   }
 
-  .col-name {
-    min-width: 20%;
+  .col-title {
+    min-width: 30%;
     text-align: left;
   }
 
-  .col-code {
+  .col-url {
+    min-width: 30%;
     text-align: center;
   }
 
-  .col-account {
-    min-width: 20%;
+  .col-published {
+    min-width: 30%;
     text-align: center;
-  }
-
-  .col-status {
-    text-align: center;
-  }
-
-  .col-amount {
-    text-align: right;
   }
 
   .col-action {
+    min-width: 10%;
     text-align: right;
+  }
+
+  a {
+    color: $ui-blue-dark;
+
+    &:hover {
+      color: $ui-blue-darken;
+    }
   }
 </style>
