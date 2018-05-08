e5f333bbf26b57efd37527cfb9adad9519fba01b
Gianluca Bine <gian_bine@hotmail.com>
2018-05-08 14:56:01 -0300

    Adicionado propriedade nullable em tag

--- backend/src/AppBundle/Entity/Pricing/Range.php
+++ backend/src/AppBundle/Entity/Pricing/Range.php
@@ -91,7 +91,7 @@ class Range implements RangeInterface
     /**
      * @var string
      *
-     * @ORM\Column(name="tag", type="string")
+     * @ORM\Column(name="tag", type="string", nullable=true)
      */
     private $tag;
 
