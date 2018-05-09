f27b59b450915738e1e645a72bda7308395511f5
Rafael Kendrik <rafamikovski@hotmail.com>
2018-05-09 11:29:28 -0300

    (Imp - #2283) Implement select search component

---
+++ frontend/src/theme/collection/SelectSearch.vue
@@ -0,0 +1,78 @@
+<template lang="pug">
+  Field(:field='field')
+    input.field(
+      v-on:input='requestOptions($event.target.value)',
+      :value='selected.text')
+    div.options(v-if='this.show')
+      ul
+        li(
+          v-on:click='updateOption($event.target.value)',
+          v-for='option in options',
+          :value='option.value')
+          | {{ option.text }}
+</template>
+
+<script>
+  import Field from '@/theme/collection/Field'
+
+  export default {
+    components: {
+      Field
+    },
+    props: [
+      'field',
+      'disabled',
+      'options',
+      'selected'
+    ],
+    data: () => ({
+      show: false
+    }),
+    methods: {
+      requestOptions(param) {
+        this.$emit("request", param)
+        this.show = true
+      },
+      updateOption(selectedOption) {
+        this.show = false
+
+        const currentOption = this.options.find(eachOption => (
+          eachOption.value === selectedOption
+        ))
+
+        this.$emit('update', currentOption)
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .options {
+    position: relative;
+    width: 100%;
+
+    ul {
+      background-color: $ui-white-regular;
+      border: 1px solid $ui-gray-light;
+      border-radius: 1px;
+      border-top: 0;
+      left: 0;
+      list-style: none;
+      // position: absolute;
+      width: 100%;
+      top: 0;
+      z-index: 5;
+    }
+
+    li {
+      padding: $ui-space-y/3 $ui-space-x/2;
+      width: 100%;
+
+      &:hover {
+        background-color: $ui-blue-light;
+        color: $ui-white-regular;
+        cursor: pointer;
+      }
+    }
+  }
+</style>
