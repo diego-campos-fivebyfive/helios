eb04430b79d1bf3851409250837bf257d675f411
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-05-11 10:16:45 -0300

    replace do 'http://' do dominio para track do woopra

--- frontend/src/widgets/woopra.js
+++ frontend/src/widgets/woopra.js
@@ -2,11 +2,13 @@ import { axios } from '@/router'
 
 export function woopra() {
   /* eslint-disable */
-  if (process.env.AMBIENCE !== 'development') {
+  if (process.env.AMBIENCE !== 'localhost') {
     ;!function(){var a,b,c,d=window,e=document,f=arguments,g="script",h=["config","track","trackForm","trackClick","identify","visit","push","call"],i=function(){var a,b=this,c=function(a){b[a]=function(){return b._e.push([a].concat(Array.prototype.slice.call(arguments,0))),b}};for(b._e=[],a=0;a<h.length;a++)c(h[a])};for(d.__woo=d.__woo||{},a=0;a<f.length;a++)d.__woo[f[a]]=d[f[a]]=d[f[a]]||new i;b=e.createElement(g),b.async=1,b.src="//static.woopra.com/js/w.js",c=e.getElementsByTagName(g)[0],c.parentNode.insertBefore(b,c)}("wo");
 
+    const domain = process.env.API_URL.split('://')
+
     wo.config({
-      domain: process.env.API_URL
+      domain: domain[1]
     })
 
     axios.get('api/v1/user/track-account')
