b016cef7e13dbbbf9584472ad689789cfce198b5
Fabio Dukievicz <fabiojd47@gmail.com>
2018-05-11 16:52:54 -0300

    testes na classe Schema

--- backend/tests/modules/Precifier/SchemaTest.php
+++ backend/tests/modules/Precifier/SchemaTest.php
@@ -1,15 +1,38 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 5/11/18
- * Time: 11:11 AM
- */
 
 namespace Tests\modules\Precifier;
 
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+use Precifier\Schema;
 
-class SchemaTest
+/**
+ * Class SchemaTest
+ * @group dynamo_schema
+ */
+class SchemaTest extends WebTestCase
 {
+    //Test
+    //Movies
+    public function testSchema()
+    {
+        $params = [
+            [
+                'attribute' => 'year',
+                'keyType' => 'HASH',
+                'type' => 'N'
+            ],
+            [
+                'attribute' => 'title',
+                'keyType' => 'RANGE',
+                'type' => 'S'
+            ],
 
+        ];
+
+        // $status = Schema::createTable('Movies2', $params);
+        $status = Schema::updateTable('Movies', $params);
+        //$status = Schema::deleteTable('Movies2');
+
+        print_r($status);die;
+    }
 }
