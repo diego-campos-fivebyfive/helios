9dc99edf42b39a4edf03466a7656dc46e5ccd96b
Gianluca Bine <gian_bine@hotmail.com>
2018-05-14 11:45:55 -0300

    Implementados serviços de carregar range e memorial

--- backend/src/AppBundle/Service/Precifier/MemorialLoader.php
+++ backend/src/AppBundle/Service/Precifier/MemorialLoader.php
@@ -1,15 +1,48 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 5/14/18
- * Time: 10:18 AM
- */
 
 namespace AppBundle\Service\Precifier;
 
+use AppBundle\Entity\Precifier\Memorial;
+use AppBundle\Manager\Precifier\MemorialManager;
 
+/**
+ * Class MemorialLoader
+ * @package AppBundle\Service\Precifier
+ * @author Gianluca Bine <gian_bine@hotmail.com>
+ */
 class MemorialLoader
 {
+    /**
+     * @var MemorialManager
+     */
+    private $manager;
 
+    /**
+     * @param MemorialManager $manager
+     */
+    function __construct(MemorialManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    /**
+     * @return mixed
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function load()
+    {
+        $qb = $this->manager->createQueryBuilder();
+
+        $qb->where('m.status = :status')
+            ->andWhere('m.publishedAt is not null')
+            ->andWhere('m.expiredAt is null')
+            ->setMaxResults(1)
+            ->setParameters([
+                'status' => Memorial::STATUS_PUBLISHED
+            ]);
+
+        $memorial = $qb->getQuery()->getOneOrNullResult();
+
+        return $memorial;
+    }
 }
--- backend/src/AppBundle/Service/Precifier/RangeLoader.php
+++ backend/src/AppBundle/Service/Precifier/RangeLoader.php
@@ -1,15 +1,53 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 5/14/18
- * Time: 10:25 AM
- */
 
 namespace AppBundle\Service\Precifier;
+use AppBundle\Entity\Precifier\Memorial;
+use AppBundle\Entity\Precifier\Range;
+use AppBundle\Manager\Precifier\RangeManager;
 
 
+/**
+ * Class RangeLoader
+ * @package AppBundle\Service\Precifier
+ * @author Gianluca Bine <gian_bine@hotmail.com>
+ */
 class RangeLoader
 {
+    /** @var RangeManager */
+    private $manager;
 
+    /**
+     * @param RangeManager $manager
+     */
+    function __construct(RangeManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    /**
+     * @param $memorial
+     * @param $power
+     * @param $level
+     * @param $family
+     * @param $componentId
+     * @return mixed
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function load(Memorial $memorial, $power, $level, $family, $componentId)
+    {
+        $qb = $this->manager->createQueryBuilder();
+
+        $qb->where($qb->expr()->andX(
+            $qb->expr()->eq('r.memorial', $memorial->getId()),
+            $qb->expr()->eq('r.family', $qb->expr()->literal($family)),
+            $qb->expr()->eq('r.componentId', $componentId)
+        ));
+
+        /** @var Range $result */
+        $result = $qb->getQuery()->getOneOrNullResult();
+
+        $range = Calculator::identifyRange($power);
+
+        return $result->getMetadata()[$level][$range];
+    }
 }
