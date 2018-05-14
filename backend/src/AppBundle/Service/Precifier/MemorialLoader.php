06968eea5f054267c268b5e6e598d89f588d8418
Gianluca Bine <gian_bine@hotmail.com>
2018-05-14 11:43:55 -0300

    Adicionado novos serviços de carregar range e memorial

---
+++ backend/src/AppBundle/Service/Precifier/MemorialLoader.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kolinalabs
+ * Date: 5/14/18
+ * Time: 10:18 AM
+ */
+
+namespace AppBundle\Service\Precifier;
+
+
+class MemorialLoader
+{
+
+}
---
+++ backend/src/AppBundle/Service/Precifier/RangeLoader.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kolinalabs
+ * Date: 5/14/18
+ * Time: 10:25 AM
+ */
+
+namespace AppBundle\Service\Precifier;
+
+
+class RangeLoader
+{
+
+}
