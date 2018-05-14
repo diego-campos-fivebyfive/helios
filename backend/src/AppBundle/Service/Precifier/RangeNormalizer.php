aa0a3c39fff81399a7d1b20653286884d4d79409
Fabio Dukievicz <fabiojd47@gmail.com>
2018-05-14 15:16:40 -0300

    RangeNormalizer

--- backend/src/AppBundle/Service/Precifier/RangeNormalizer.php
+++ backend/src/AppBundle/Service/Precifier/RangeNormalizer.php
@@ -9,54 +9,53 @@
  * file that was distributed with this source code.
  */
 
-namespace AppBundle\Service\Pricing;
+namespace AppBundle\Service\Precifier;
 
-use AppBundle\Entity\Pricing\Memorial;
-use AppBundle\Entity\Pricing\MemorialInterface;
-use AppBundle\Entity\Pricing\Range;
-use AppBundle\Manager\Pricing\RangeManager;
+use AppBundle\Entity\Precifier\Memorial;
+use AppBundle\Entity\Precifier\Range;
+use AppBundle\Manager\Precifier\RangeManager;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 
 /**
  * Class RangeNormalizer
- * This class resolves and normalizes ranges from memorial based on codes and levels
+ * This class resolves and normalizes ranges from memorial
  *
- * @author Claudinei Machado <claudinei@kolinalabs.com>
+ * @author Fabio Dukievicz <fabiojd47@gmail.com>
  */
 class RangeNormalizer
 {
-    const FILTER_CACHE = 0;
-    const FILTER_DATABASE = 1;
-    const FILTER_COLLECTION = 2;
+//    const FILTER_CACHE = 0;
+//    const FILTER_DATABASE = 1;
+//    const FILTER_COLLECTION = 2;
 
     /**
      * @var RangeManager
      */
     private $manager;
 
-    /**
-     * @var int
-     */
-    private $strategy;
+//    /**
+//     * @var int
+//     */
+//    private $strategy;
 
-    /**
-     * @var array
-     */
-    private $cache = [];
+//    /**
+//     * @var array
+//     */
+//    private $cache = [];
 
-    /**
-     * @var array
-     */
-    private $powers = [
-        [0, 10], [10, 20], [20, 30], [30, 40], [40, 50], [50, 60], [60, 70], [70, 80], [80, 90], [90, 100],
-        [100, 200], [200, 300], [300, 400], [400, 500], [500, 600], [600, 700], [700, 800], [800, 900], [900, 1000],
-        [1000, 999000]
-    ];
+//    /**
+//     * @var array
+//     */
+//    private $powers = [
+//        [0, 10], [10, 20], [20, 30], [30, 40], [40, 50], [50, 60], [60, 70], [70, 80], [80, 90], [90, 100],
+//        [100, 200], [200, 300], [300, 400], [400, 500], [500, 600], [600, 700], [700, 800], [800, 900], [900, 1000],
+//        [1000, 999000]
+//    ];
 
-    /**
-     * @var array
-     */
-    private $definitions = [];
+//    /**
+//     * @var array
+//     */
+//    private $definitions = [];
 
     /**
      * @var PropertyAccess
@@ -74,75 +73,84 @@ class RangeNormalizer
      */
     function __construct(RangeManager $manager)
     {
-        ini_set('memory_limit', $this->memory);
+       // ini_set('memory_limit', $this->memory);
 
         $this->manager = $manager;
 
-        $this->strategy = self::FILTER_CACHE;
+       // $this->strategy = self::FILTER_CACHE;
     }
 
-    /**
-     * @param int $strategy
-     * @return RangeNormalizer
-     */
-    public function setStrategy($strategy)
+    /**/
+    public function normalize(Memorial $memorial, $groups)
     {
-        $this->strategy = $strategy;
 
-        return $this;
-    }
+        $defautMetadata = [];
 
-    /**
-     * @return array
-     */
-    public function getPowers()
-    {
-        return $this->powers;
-    }
 
-    /**
-     * @return array
-     */
-    public function getCache()
-    {
-        return $this->cache;
-    }
+        foreach ($groups as $family => $components) {
+            $qb = $this->manager->createQueryBuilder();
 
-    /**
-     * @param Memorial $memorial
-     * @param array $codes
-     * @param array $levels
-     * @param array $definitions
-     */
-    public function normalize(Memorial $memorial, array $codes, array $levels, array $definitions = [])
-    {
-        $this->definitions = $definitions;
+            $ids = array_keys($components);
+            //print_r(array_keys($ids));die;
 
-        $this->cache($memorial, $codes, $levels);
+            $qb->select('r.id')
+            ->where('r.family = :family')
+            ->andWhere('r.memorial = :memorial')
+            ->andWhere($qb->expr()->in('r.componentId', $ids))
+            ->setParameters([
+                'family' => $family,
+                'memorial' => $memorial->getId()
+            ]);
 
-        foreach ($this->powers as $config) {
+            $has = $qb->getQuery()->getResult();
 
-            list($initialPower, $finalPower) = $config;
+            $without = array_diff($ids, $has);
 
-            foreach($levels as $level) {
+            foreach ($without as $componentId) {
+                $range = new Range();
 
-                foreach ($codes as $code){
+                $range->setFamily($family);
+                $range->setComponentId($componentId);
+                $range->setCode($components[$componentId]);
+                $range->setCostPrice(0);
+                $range->setMemorial($memorial);
+                $range->setMetadata($defautMetadata);
 
-                    $range = $this->filter($code, $level, $initialPower, $finalPower);
 
-                    if (!$range instanceof Range) {
-
-                        $range = $this->create($memorial, $code, $level, $initialPower, $finalPower);
-
-                        $cacheKey = $this->createCacheKey($initialPower, $finalPower);
-
-                        $this->cache[$cacheKey][$level][$code] = $range;
-                    }
-
-                    $this->checkDefinitions($range);
-                }
+                dump($range);
             }
+            die;
+
         }
+
+
+        //$this->definitions = $definitions;
+
+        //$this->cache($memorial, $codes, $levels);
+
+//        foreach ($this->powers as $config) {
+//
+//            list($initialPower, $finalPower) = $config;
+//
+//            foreach($levels as $level) {
+//
+//                foreach ($codes as $code){
+//
+//                    $range = $this->filter($code, $level, $initialPower, $finalPower);
+//
+//                    if (!$range instanceof Range) {
+//
+//                        $range = $this->create($memorial, $code, $level, $initialPower, $finalPower);
+//
+//                        $cacheKey = $this->createCacheKey($initialPower, $finalPower);
+//
+//                        $this->cache[$cacheKey][$level][$code] = $range;
+//                    }
+//
+//                    $this->checkDefinitions($range);
+//                }
+//            }
+//        }
 
         $this->manager->flush();
     }
@@ -174,85 +182,8 @@ class RangeNormalizer
         return $range;
     }
 
-    /**
-     * @param $code
-     * @param $level
-     * @param $initialPower
-     * @param $finalPower
-     * @param Memorial|null $memorial
-     * @return bool|mixed|null|object|Range
-     */
-    public function filter($code, $level, $initialPower, $finalPower, Memorial $memorial = null)
-    {
-        switch ($this->strategy){
-            case self::FILTER_CACHE:
 
-                $cacheKey = $this->createCacheKey($initialPower ,$finalPower);
 
-                return array_key_exists($code, $this->cache[$cacheKey][$level]) ? $this->cache[$cacheKey][$level][$code] : false ;
-
-                break;
-
-            case self::FILTER_DATABASE:
-
-                return $this->manager->findOneBy([
-                    'memorial' => $memorial,
-                    'code' => $code,
-                    'level' => $level,
-                    'initialPower' => $initialPower,
-                    'finalPower' => $finalPower
-                ]);
-
-                break;
-
-            case self::FILTER_COLLECTION:
-
-                return $memorial->getRanges()->filter(function (Range $range) use ($code, $level, $initialPower, $finalPower) {
-                    return $range->hasConfig($code, $level, $initialPower, $finalPower);
-                })->last();
-
-                break;
-        }
-
-        return null;
-    }
-
-    /**
-     * @param $initialPower
-     * @param $finalPower
-     * @return string
-     */
-    public function createCacheKey($initialPower, $finalPower)
-    {
-        return sprintf('%s_%s', $initialPower, $finalPower);
-    }
-
-    /**
-     * @param Range $range
-     */
-    private function checkDefinitions(Range $range)
-    {
-        if(array_key_exists($range->getCode(), $this->definitions)){
-
-            $updatePrice = false;
-            $accessor = $this->getAccessor();
-
-            foreach ($this->definitions[$range->getCode()] as $property => $value){
-
-                if($accessor->getValue($range, $property) != $value){
-                    $accessor->setValue($range, $property, $value);
-
-                    if('costPrice' == $property){
-                        $updatePrice = true;
-                    }
-                }
-            }
-
-            if($updatePrice) {
-                $range->updatePrice();
-            }
-        }
-    }
 
     /**
      * @param Memorial $memorial
@@ -307,14 +238,5 @@ class RangeNormalizer
         $this->cache = $cache;
 
         return $this;
-    }
-
-    private function getAccessor()
-    {
-        if(!$this->accessor){
-            $this->accessor = PropertyAccess::createPropertyAccessor();
-        }
-
-        return $this->accessor;
     }
 }
