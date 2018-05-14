4f335206c3be2ae6a383c8e1aa4a7f892c1f218f
Gianluca Bine <gian_bine@hotmail.com>
2018-05-14 14:48:57 -0300

    Adicionado novo serviço de clonagem de memorial e range das novas entidades

---
+++ backend/src/AppBundle/Service/Precifier/MemorialCloner.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace AppBundle\Service\Pricing;
+
+use AppBundle\Entity\Pricing\Memorial;
+use AppBundle\Entity\Pricing\MemorialInterface;
+use AppBundle\Entity\Pricing\Range;
+use AppBundle\Entity\Pricing\RangeInterface;
+use AppBundle\Manager\Pricing\MemorialManager;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
+class MemorialCloner
+{
+    /**
+     * @var MemorialManager
+     */
+    private $manager;
+
+    /**
+     * @var \Symfony\Component\PropertyAccess\PropertyAccessor
+     */
+    private $accessor;
+
+    /**
+     * @var array
+     */
+    private $rangeProperties = [
+        'initialPower',
+        'finalPower',
+        'costPrice',
+        'markup',
+        'level',
+        'price',
+        'code',
+        'tax'
+    ];
+
+    /**
+     * Adjust memory limit
+     * @var string
+     */
+    private $memory = '512M';
+
+    /**
+     * MemorialCloner constructor.
+     */
+    function __construct(MemorialManager $manager)
+    {
+        ini_set('memory_limit', $this->memory);
+
+        $this->accessor = PropertyAccess::createPropertyAccessor();
+        $this->manager = $manager;
+    }
+
+    /**
+     * @param MemorialInterface $source
+     * @return Memorial
+     */
+    public function execute(MemorialInterface $source)
+    {
+        $memorial = new Memorial();
+
+        $memorial
+            ->setStatus(Memorial::STATUS_PENDING)
+            ->setName(sprintf('%s [clone:%s]', $source->getName(), $source->getId()))
+        ;
+
+        /** @var Range $sourceRange */
+        foreach ($source->getRanges() as $sourceRange){
+            $this->cloneRange($sourceRange, [
+                'memorial' => $memorial
+            ]);
+        }
+
+        $this->manager->save($memorial);
+
+        return $memorial;
+    }
+
+    /**
+     * @param MemorialInterface $memorial
+     * @param $source
+     * @param $target
+     */
+    public function convertLevel(MemorialInterface $memorial, $source, $target)
+    {
+        $sources = $this->filterRangesByLevel($memorial, $source);
+
+        if(!$sources->isEmpty()) {
+
+            $targets = $this->filterRangesByLevel($memorial, $target);
+
+            $em = $this->manager->getEntityManager();
+            foreach ($targets as $range){
+                $memorial->removeRange($range);
+                $em->remove($range);
+            }
+
+            foreach ($sources as $source) {
+                $this->cloneRange($source, [
+                    'memorial' => $memorial,
+                    'level' => $target
+                ]);
+            }
+
+            $this->manager->save($memorial);
+        }
+    }
+
+    /**
+     * @param MemorialInterface $memorial
+     * @param $level
+     * @return \Doctrine\Common\Collections\ArrayCollection|\Doctrine\Common\Collections\Collection
+     */
+    private function filterRangesByLevel(MemorialInterface $memorial, $level)
+    {
+        return $memorial->getRanges()->filter(function (RangeInterface $range) use($level){
+            return $level === $range->getLevel();
+        });
+    }
+
+    /**
+     * @param RangeInterface $source
+     * @param array $definitions
+     * @return Range
+     */
+    private function cloneRange(RangeInterface $source, array $definitions = [])
+    {
+        $range = new Range();
+
+        foreach ($this->rangeProperties as $property){
+
+            $value = $this->accessor->getValue($source, $property);
+
+            $this->accessor->setValue($range, $property, $value);
+        }
+
+        foreach ($definitions as $property => $value){
+            $this->accessor->setValue($range, $property, $value);
+        }
+
+        return $range;
+    }
+}
---
+++ backend/tests/AppBundle/Service/Precifier/MemorialClonerTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\AppBundle\Service\Precifier;
+
+use AppBundle\Service\Precifier\Calculator;
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+
+/**
+ * Class CalculatorTest
+ * @group precifier_calculator
+ */
+class CalculatorTest extends WebTestCase
+{
+    private $ranges = [
+        [
+            'id' => 1,
+            'memorial_id' => 258,
+            'component_id' => 123,
+            'family' => 'inverter',
+            'code' => 1,
+            'cost_price' => 150,
+            'metadata' => [
+                'partner' => [
+                    60 => [
+                        'markup' => 0.1,
+                        'price' => 100
+                    ],
+                    70 => [
+                        'price' => 200
+                    ],
+                    80 => [
+                        'price' => 300
+                    ]
+                ]
+            ]
+        ],
+        [
+            'id' => 2,
+            'memorial_id' => 258,
+            'component_id' => 345,
+            'family' => 'module',
+            'code' => 2,
+            'cost_price' => 250,
+            'metadata' => [
+                'partner' => [
+                    60 => [
+                        'markup' => 0.2,
+                        'price' => 130
+                    ],
+                    70 => [
+                        'price' => 230
+                    ],
+                    80 => [
+                        'price' => 330
+                    ]
+                ]
+            ]
+        ]
+    ];
+
+    public function testPrecify()
+    {
+        $data = [
+            "level" => 'partner',
+            "power" => 78,
+            "groups" => [
+                'inverter' => [
+                    123
+                ],
+                'module' => [
+                    345
+                ]
+            ]
+        ];
+
+        $precifiedComponents = Calculator::precify($data, $this->ranges);
+
+        self::assertEquals(2, count($precifiedComponents));
+        self::assertEquals(200, $precifiedComponents['inverter'][123]);
+        self::assertEquals(230, $precifiedComponents['module'][345]);
+    }
+}
