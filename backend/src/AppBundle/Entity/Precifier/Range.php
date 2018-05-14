f7633a667a4970785a9e6dfe11c45a91b0e7ba92
Fabio Dukievicz <fabiojd47@gmail.com>
2018-05-14 15:14:51 -0300

    powerRanges na entidade

--- backend/src/AppBundle/Entity/Precifier/Range.php
+++ backend/src/AppBundle/Entity/Precifier/Range.php
@@ -12,6 +12,11 @@ use Doctrine\ORM\Mapping as ORM;
 class Range
 {
     /**
+     * @var array
+     */
+    public static $powerRanges = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+
+    /**
      * @var int
      *
      * @ORM\Column(name="id", type="integer")
