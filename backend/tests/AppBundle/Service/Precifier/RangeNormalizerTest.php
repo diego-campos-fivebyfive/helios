9f944eece7820ecfb2bd181dcd04cbd3e6011663
Fabio Dukievicz <fabiojd47@gmail.com>
2018-05-14 16:12:19 -0300

    testes em normalizador de ranges

--- backend/tests/AppBundle/Service/Precifier/RangeNormalizerTest.php
+++ backend/tests/AppBundle/Service/Precifier/RangeNormalizerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\AppBundle\Service\Precifier;
 
 use AppBundle\Entity\Precifier\Memorial;
+use AppBundle\Manager\Precifier\RangeManager;
 use AppBundle\Service\Precifier\RangeNormalizer;
 use Liip\FunctionalTestBundle\Test\WebTestCase;
 use Tests\AppBundle\Helpers\ObjectHelperTest;
@@ -21,15 +22,21 @@ class RangeNormalizerTest extends WebTestCase
         $memorial = $this->createMemorial();
 
         $groups = [
-            'module' => [1 => '123',2 => 'abc',3 => '541'],
-            'inverter' => [3=>'158',4=>'875',5=>'fnh'],
+            'module' => [1 => '123',2 => 'abc',3 => '541',4 => '111'],
+            'inverter' => [3=>'158',4=>'875',5=>'fnh',6=>'222'],
         ];
 
         /** @var RangeNormalizer $normalizer */
         $normalizer = $this->getContainer()->get('precifier_range_normalizer');
 
         $normalizer->normalize($memorial, $groups);
-        //$this->assertEquals(count($powers), $memorial->getRanges()->count());
+
+        /** @var RangeManager $manager */
+        $manager = $this->getContainer()->get('precifier_range_manager');
+
+        $all = $manager->findAll();
+
+        $this->assertEquals(count($all), 8);
     }
 
     /**
