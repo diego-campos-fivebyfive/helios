85c96043822d8f86e84d57703919a56366eeb54d
Gianluca Bine <gian_bine@hotmail.com>
2018-05-14 09:18:23 -0300

    Remoração de use não utilizado

--- backend/src/AppBundle/Entity/Precifier/Memorial.php
+++ backend/src/AppBundle/Entity/Precifier/Memorial.php
@@ -4,7 +4,6 @@ namespace AppBundle\Entity\Precifier;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
-use Knp\DoctrineBehaviors\Model as ORMBehaviors;
 
 /**
  * Memorial
