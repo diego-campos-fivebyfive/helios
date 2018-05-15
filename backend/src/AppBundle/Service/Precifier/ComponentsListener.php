0f048e5fd2bd859df30dd87fdc27619493c43f17
Gianluca Bine <gian_bine@hotmail.com>
2018-05-15 14:37:56 -0300

    Implementado serviço de checagem de adição/remoção de componentes

--- backend/src/AppBundle/Service/Precifier/ComponentsListener.php
+++ backend/src/AppBundle/Service/Precifier/ComponentsListener.php
@@ -1,15 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kolinalabs
- * Date: 5/15/18
- * Time: 2:16 PM
- */
 
 namespace AppBundle\Service\Precifier;
 
+use AppBundle\Entity\Precifier\Memorial;
+use AppBundle\Manager\Precifier\MemorialManager;
 
+
+/**
+ * Class ComponentsListener
+ * @package AppBundle\Service\Precifier
+ * @author Gianluca Bine <gian_bine@hotmail.com>
+ */
 class ComponentsListener
 {
+    /** @var MemorialManager */
+    private $manager;
 
+    /**
+     * @param MemorialManager $manager
+     */
+    function __construct(MemorialManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    public function action($type, $family)
+    {
+        $memorials = $this->manager->findAll();
+
+        /** @var Memorial $memorial */
+        foreach($memorials as $memorial) {
+            $memorial->addFamilyMetadata($type, $family);
+
+            $this->manager->save($memorial, false);
+        }
+
+        $this->manager->flush();
+    }
 }
