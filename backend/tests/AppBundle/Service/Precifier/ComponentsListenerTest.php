a4b18fc2287601da2e9fa1c0a91f2ca273f89607
Gianluca Bine <gian_bine@hotmail.com>
2018-05-15 14:38:15 -0300

    Implementado caso de teste do serviço de checagem de adição/remoção de componentes

--- backend/tests/AppBundle/Service/Precifier/ComponentsListenerTest.php
+++ backend/tests/AppBundle/Service/Precifier/ComponentsListenerTest.php
@@ -2,81 +2,37 @@
 
 namespace Tests\AppBundle\Service\Precifier;
 
+use AppBundle\Entity\Precifier\Memorial;
+use AppBundle\Manager\Precifier\MemorialManager;
 use AppBundle\Service\Precifier\Calculator;
+use AppBundle\Service\Precifier\ComponentsListener;
 use Liip\FunctionalTestBundle\Test\WebTestCase;
 
 /**
- * Class CalculatorTest
- * @group precifier_calculator
+ * Class ComponentsListenerTest
+ * @group precifier_components_listener
  */
-class CalculatorTest extends WebTestCase
+class ComponentsListenerTest extends WebTestCase
 {
-    private $ranges = [
-        [
-            'id' => 1,
-            'memorial_id' => 258,
-            'component_id' => 123,
-            'family' => 'inverter',
-            'code' => 1,
-            'cost_price' => 150,
-            'metadata' => [
-                'partner' => [
-                    60 => [
-                        'markup' => 0.1,
-                        'price' => 100
-                    ],
-                    70 => [
-                        'price' => 200
-                    ],
-                    80 => [
-                        'price' => 300
-                    ]
-                ]
-            ]
-        ],
-        [
-            'id' => 2,
-            'memorial_id' => 258,
-            'component_id' => 345,
-            'family' => 'module',
-            'code' => 2,
-            'cost_price' => 250,
-            'metadata' => [
-                'partner' => [
-                    60 => [
-                        'markup' => 0.2,
-                        'price' => 130
-                    ],
-                    70 => [
-                        'price' => 230
-                    ],
-                    80 => [
-                        'price' => 330
-                    ]
-                ]
-            ]
-        ]
-    ];
-
-    public function testPrecify()
+    public function testAction()
     {
-        $data = [
-            "level" => 'partner',
-            "power" => 78,
-            "groups" => [
-                'inverter' => [
-                    123
-                ],
-                'module' => [
-                    345
-                ]
-            ]
-        ];
+        /** @var ComponentsListener $componentsLister */
+        $componentsLister = $this->getContainer()->get('precifier_components_listener');
 
-        $precifiedComponents = Calculator::precify($data, $this->ranges);
+        $componentsLister->action(Memorial::ACTION_TYPE_ADD_COMPONENT, 'module');
+        $componentsLister->action(Memorial::ACTION_TYPE_REMOVE_COMPONENT, 'variety');
 
-        self::assertEquals(2, count($precifiedComponents));
-        self::assertEquals(200, $precifiedComponents['inverter'][123]);
-        self::assertEquals(230, $precifiedComponents['module'][345]);
+        /** @var MemorialManager $memorialManager */
+        $memorialManager = $this->getContainer()->get('precifier_memorial_manager');
+
+        $memorials = $memorialManager->findAll();
+
+        /** @var Memorial $memorial */
+        foreach ($memorials as $memorial) {
+            self::assertArrayHasKey(Memorial::ACTION_TYPE_ADD_COMPONENT, $memorial->getMetadata());
+            self::assertArrayHasKey(Memorial::ACTION_TYPE_REMOVE_COMPONENT, $memorial->getMetadata());
+            self::assertArrayHasKey('module', $memorial->getMetadata()[Memorial::ACTION_TYPE_ADD_COMPONENT]);
+            self::assertArrayHasKey('variety', $memorial->getMetadata()[Memorial::ACTION_TYPE_REMOVE_COMPONENT]);
+        }
     }
 }
