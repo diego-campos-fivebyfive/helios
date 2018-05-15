965055c7b9ea550e3f49b9bc25e903df7dc3d9ef
Gianluca Bine <gian_bine@hotmail.com>
2018-05-15 18:03:08 -0300

    Refatoração do caso de teste com o teste do método loadByIds

--- backend/tests/AppBundle/Service/Precifier/ComponentsLoaderTest.php
+++ backend/tests/AppBundle/Service/Precifier/ComponentsLoaderTest.php
@@ -11,17 +11,36 @@ use Liip\FunctionalTestBundle\Test\WebTestCase;
  */
 class ComponentsLoaderTest extends WebTestCase
 {
-    public function testLoad()
+    public function testLoadAll()
     {
         /** @var ComponentsLoader $componentsLoader */
         $componentsLoader = $this->getContainer()->get('precifier_components_loader');
 
-        $components = $componentsLoader->load();
+        $components = $componentsLoader->loadAll();
 
         self::assertArrayHasKey('module', $components);
         self::assertArrayHasKey('inverter', $components);
         self::assertArrayHasKey('string_box', $components);
         self::assertArrayHasKey('variety', $components);
         self::assertArrayHasKey('structure', $components);
+
+        $components = $componentsLoader->loadAll(['module', 'structure']);
+
+        self::assertEquals(2, count($components));
+        self::assertArrayHasKey('module', $components);
+        self::assertArrayHasKey('structure', $components);
+    }
+
+    public function testLoadByIds()
+    {
+        /** @var ComponentsLoader $componentsLoader */
+        $componentsLoader = $this->getContainer()->get('precifier_components_loader');
+
+        $components = $componentsLoader->loadByIds([
+            'inverter' => [6418]
+        ]);
+
+        self::assertArrayHasKey('inverter', $components);
+        self::assertArrayHasKey(6418, $components['inverter']);
     }
 }
