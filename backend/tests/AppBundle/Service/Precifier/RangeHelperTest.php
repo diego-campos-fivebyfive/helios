e49bb322a270187701731417055976d6710f8db5
Fabio Dukievicz <fabiojd47@gmail.com>
2018-05-16 16:39:19 -0300

    testes em novo metodo componentsIds

--- backend/tests/AppBundle/Service/Precifier/RangeHelperTest.php
+++ backend/tests/AppBundle/Service/Precifier/RangeHelperTest.php
@@ -3,31 +3,48 @@
 namespace Tests\AppBundle\Service\Precifier;
 
 use AppBundle\Entity\Precifier\Range;
-use AppBundle\Service\Precifier\RangeLoader;
+use AppBundle\Service\Precifier\RangeHelper;
 use Liip\FunctionalTestBundle\Test\WebTestCase;
 
 /**
  * Class RangeLoaderTest
- * @group precifier_range_loader
+ * @group precifier_range_helper
  */
-class RangeLoaderTest extends WebTestCase
+class RangeHelperTest extends WebTestCase
 {
     /**
      * @throws \Doctrine\ORM\NonUniqueResultException
      */
     public function testLoad()
     {
-        /** @var RangeLoader $rangeLoader */
-        $rangeLoader = $this->getContainer()->get('precifier_range_loader');
+        /** @var RangeHelper $rangeHelper */
+        $rangeHelper = $this->getContainer()->get('precifier_range_helper');
 
         /** @var Range $range */
-        $range = $rangeLoader->load(7);
+        $range = $rangeHelper->load(7);
 
         self::assertEquals(true, $range instanceof Range);
 
-        $ranges = $rangeLoader->load([7,8,9]);
+        $ranges = $rangeHelper->load([7,8,9]);
 
         self::assertEquals(3, count($ranges));
         self::assertEquals(true, $ranges[0] instanceof Range);
+    }
+
+    /**
+     * test ComponentsIds
+     */
+    public function testComponentsIds()
+    {
+        /** @var RangeHelper $rangeHelper */
+        $rangeHelper = $this->getContainer()->get('precifier_range_helper');
+
+        $groups = $rangeHelper->componentsIds(20);
+
+        self::assertEquals(5, count($groups));
+
+        foreach ($groups as $family) {
+            self::assertTrue(is_array($family));
+        }
     }
 }
