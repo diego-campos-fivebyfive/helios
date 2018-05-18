6fea69d855e40b671a2e775c45a6ae4f36fa43ef
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-05-18 15:38:25 -0300

    Imporatação e utirlização do componente Cells

---
+++ frontend/src/pages/memorial/form/Cells.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+  export default {
+    name: "cells"
+  }
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/pages/memorial/form/index.vue
+++ frontend/src/pages/memorial/form/index.vue
@@ -1,13 +1,16 @@
 <template lang="pug">
   Panel.panel
     Filters(slot='header')
+    Cells(slot='section')
 </template>
 
 <script>
+  import Cells from './Cells'
   import Filters from './filters'
 
   export default {
     components: {
+      Cells,
       Filters
     }
   }
