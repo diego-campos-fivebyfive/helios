b7d2bb5610adfad40ce701e29de7f4672c892644
Fabio Dukievicz <fabiojd47@gmail.com>
2018-05-22 10:07:02 -0300

    normalizador de ranges

--- backend/bin/app/memorial_transfer.php
+++ backend/bin/app/memorial_transfer.php
@@ -7,112 +7,99 @@ getAutoload();
  * Este script servirá para transferir os dados do antigo memorial para o novo modelo.
  */
 
-
 connectDatabase();
 
-$levels = \AppBundle\Entity\Precifier\Memorial::getDefaultLevels(true);
+$sourceMemorialId = 301;
+$targetMemorialId = 20;
 
-print_r($levels);die;
+$levelsArray = \AppBundle\Entity\Precifier\Memorial::getDefaultLevels(true);
 
+$levels = '(';
+$c = 0;
 
-$families = "SELECT DISTINCT r.family as families FROM app_precifier_range r WHERE r.memorial_id = 20";
-
-$result = R::getAll($families);
-
-print_r($result);die;
-
-
-$sql = "SELECT COUNT(c.code) as qtt FROM app_component_inverter c";
-
-$result = R::getAll($sql);
-
-print_r($result);die;
-
-
-// Clear all ranking transactions
-//R::wipe('app_ranking');
-
-
-
-$rankings = [];
-
-foreach ($orders as $order){
-
-    if(!array_key_exists($order['level'], $mapping)) continue;
-
-    $accountId = $order['account_id'];
-
-    $target = sprintf('%s::%s', \AppBundle\Entity\Customer::class, $order['account_id']);
-
-    $deliveryInfo = $order['delivery_at'] ? sprintf(' - Disp: %s', (new \DateTime($order['delivery_at']))->format('d/m/Y')) : '';
-
-    $description = sprintf(
-        '%s - %s%s',
-        $order['reference'],
-        (new \DateTime())->format('d/m/Y H:i'),
-        $deliveryInfo
-    );
-
-    $amount = ceil($order['power'] * $mapping[$order['level']]);
-
-    // create ranking
-    $ranking = R::dispense('app_ranking');
-
-    $ranking->target = $target;
-    $ranking->description = $description;
-    $ranking->amount = $amount;
-    $ranking->created_at = $date;
-    $ranking->updated_at = $date;
-
-    $rankings[] = $ranking;
+foreach ($levelsArray as $level) {
+    $c++;
+    $levels .= "'".$level."'";
+    if (count($levelsArray) != $c) {
+        $levels .= ', ';
+    }
 }
 
-// Store rankings
-R::storeAll($rankings);
+$levels .= ')';
 
-echo sprintf("%s rankings normalized.", count($rankings));
-echo "\n";
+$sqlFamilies = "SELECT DISTINCT r.family as families 
+                 FROM app_precifier_range r
+                 WHERE r.memorial_id = {$targetMemorialId}";
 
-/*** TRIGGERS QUE DEVEM SER ARMAZENADAS NA BASE DE DADOS (tabela app_ranking)
+$families = R::getCol($sqlFamilies);
 
-DELIMITER //
+$countComponents = 0;
+$countNormalized = 0;
 
-DROP TRIGGER IF EXISTS onInsertRanking;
-CREATE TRIGGER onInsertRanking
-AFTER INSERT ON app_ranking
-FOR EACH ROW
-BEGIN
-UPDATE app_customer c
-SET c.ranking = (SELECT sum(r.amount)
-FROM app_ranking r
-WHERE r.target = NEW.target)
-WHERE c.id = REPLACE(NEW.target, 'AppBundle\\Entity\\Customer::', '');
-END//
+foreach ($families as $family) {
 
-DROP TRIGGER IF EXISTS onUpdateRanking;
-CREATE TRIGGER onUpdateRanking
-AFTER UPDATE ON app_ranking
-FOR EACH ROW
-BEGIN
-UPDATE app_customer c
-SET c.ranking = (SELECT sum(r.amount)
-FROM app_ranking r
-WHERE r.target = NEW.target)
-WHERE c.id = REPLACE(NEW.target, 'AppBundle\\Entity\\Customer::', '');
-END//
+    $sqlComponents = "SELECT c.id, c.code FROM app_component_" . $family . " c";
 
-DROP TRIGGER IF EXISTS onDeleteRanking;
-CREATE TRIGGER onDeleteRanking
-AFTER DELETE ON app_ranking
-FOR EACH ROW
-BEGIN
-UPDATE app_customer c
-SET c.ranking = (SELECT sum(r.amount)
-FROM app_ranking r
-WHERE r.target = OLD.target)
-WHERE c.id = REPLACE(OLD.target, 'AppBundle\\Entity\\Customer::', '');
-END//
+    $components = R::getAll($sqlComponents);
 
-DELIMITER ;
+    foreach ($components as $component) {
 
- ***/
+        $countComponents++;
+
+        $componentId = $component['id'];
+        $componentCode = $component['code'];
+
+        $sqlRangePrecifier = "SELECT r.metadata 
+                              FROM app_precifier_range r 
+                              WHERE r.family = '{$family}' 
+                              AND r.component_id = '{$componentId}'";
+
+        $range = R::getRow($sqlRangePrecifier);
+
+        $metadata = json_decode($range['metadata'], true);
+
+        $sqlRangePricing =
+            "SELECT r.initialPower as powerRange, r.level, r.cost_price, r.price, r.markup 
+              FROM app_pricing_range r 
+              WHERE r.memorial_id = {$sourceMemorialId} 
+              AND r.code = '{$componentCode}' 
+              AND r.level IN {$levels}";
+
+        $baseRanges = R::getAll($sqlRangePricing);
+
+        $costPrice = null;
+
+        foreach ($baseRanges as $baseRange) {
+            if (is_null($costPrice)) {
+                $costPrice = (float) $baseRange['cost_price'];
+            }
+
+            $baseLevel = $baseRange['level'];
+            $basePowerRange = $baseRange['powerRange'];
+            $basePrice = $baseRange['price'];
+            $baseMarkup = $baseRange['markup'];
+
+            $metadata[$baseLevel][$basePowerRange]['price'] = (float) $basePrice;
+            $metadata[$baseLevel][$basePowerRange]['markup'] = (float) $baseMarkup;
+        }
+
+        if ($baseRanges) {
+            $metadata = json_encode($metadata);
+
+            $sqlUpdate =
+                "UPDATE app_precifier_range 
+              SET cost_price = {$costPrice}, metadata = '{$metadata}'
+              WHERE component_id = {$componentId} AND family = '{$family}'";
+
+            $countNormalized++;
+
+            R::exec($sqlUpdate);
+        }
+
+        $costPrice = null;
+    }
+}
+
+print_r("Number of components processed: ".$countComponents."\n");
+print_r("Number of ranges normalized: ".$countNormalized."\n");
+print_r('finish'."\n");
