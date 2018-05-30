0e824edb5496bf9058952e7c00c4a685949a87f1
Fabio Dukievicz <fabiojd47@gmail.com>
2018-05-30 15:29:32 -0300

    formType de kit

--- backend/src/AppBundle/Form/Kit/KitType.php
+++ backend/src/AppBundle/Form/Kit/KitType.php
@@ -1,11 +1,9 @@
 <?php
 
-namespace AppBundle\Form\Component;
+namespace AppBundle\Form\Kit;
 
-use AppBundle\Entity\Component\Catalog;
-use AppBundle\Entity\Component\Kit;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,41 +16,33 @@ class KitType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        if (!$options['final']) {
-            
-            $builder
-                ->add('identifier', TextType::class, [
-                    'attr' => [
-                        'maxlength' => 30
-                    ]
-                ])
-                ->add('invoicePriceStrategy', ChoiceType::class, [
-                    'choices' => Kit::getInvoicePriceStrategies(),
-                    'expanded' => true
-                ]);
-
-        } else {
-
-            /** @var Kit $kit */
-            $kit = $options['data'];
-
-            if (Kit::PRICE_STRATEGY_ABS == $kit->getInvoicePriceStrategy()) {
-
-                $builder->add('invoiceBasePrice', MoneyType::class, [
-                    'currency' => false,
-                    'scale' => 2
-                ]);
-            }
-
-            $builder
-                ->add('deliveryPriceStrategy', ChoiceType::class, [
-                    'choices' => Kit::getDeliveryPriceStrategies(),
-                ])
-                ->add('deliveryBasePrice', MoneyType::class, [
-                    'currency' => false,
-                    'scale' => 2
-                ]);
-        }
+        $builder
+            ->add('code', TextType::class, [
+                'required' => true
+            ])
+            ->add('description', TextType::class, [
+                'required' => true
+            ])
+            ->add('power', TextType::class, [
+                'required' => true
+            ])
+            ->add('price', MoneyType::class, [
+                'currency' => false,
+                'scale' => 2,
+                'required' => true
+            ])
+            ->add('stock', TextType::class, [
+                'required' => true
+            ])
+            ->add('image', TextType::class, [
+                'required' => false
+            ])
+            ->add('position', TextType::class, [
+                'required' => true
+            ])
+            ->add('available', CheckboxType::class, [
+                'required' => false
+            ]);
     }
 
     /**
@@ -60,9 +50,5 @@ class KitType extends AbstractType
      */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults(array(
-            'data_class' => 'AppBundle\Entity\Component\Kit',
-            'final' => false
-        ));
     }
 }
