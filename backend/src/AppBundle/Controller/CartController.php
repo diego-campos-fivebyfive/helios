dbec6693d707663228bfefc5775797576a7769a7
Fabio Dukievicz <fabiojd47@gmail.com>
2018-06-01 10:56:35 -0300

    rota para adição de kit ao carrinho

--- backend/src/AppBundle/Controller/CartController.php
+++ backend/src/AppBundle/Controller/CartController.php
@@ -2,56 +2,74 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Kit\Cart;
+use AppBundle\Entity\Kit\CartHasKit;
 use AppBundle\Entity\Kit\Kit;
-use AppBundle\Form\Kit\KitType;
+use AppBundle\Manager\CartManager;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-USE Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use APY\BreadcrumbTrailBundle\Annotation\Breadcrumb;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
- * @Route("kit")
+ * @Route("cart")
  *
- * @Breadcrumb("Kits Fixos")
+ * @Breadcrumb("Carrinho de compras")
  */
-class KitsController extends AbstractController
+class CartController extends AbstractController
 {
     /**
-     * @Route("/", name="index_kit")
-     * @Method("get")
+     * @Route("/{id}/add_kit", name="cart_add_kit")
+     * @Method("post")
      */
-    public function indexAction(Request $request)
+    public function addKitAction(Request $request, Kit $kit)
     {
-        $manager = $this->manager('kit');
+        /** @var CartManager $cartManager */
+        $cartManager = $this->manager('cart');
 
-        $qb = $manager->createQueryBuilder();
+        /** @var Cart $cart */
+        $cart = $cartManager->findOneBy([
+            'account' => $this->account()
+        ]);
 
-        $qb
-            ->orderBy('k.position', 'asc')
-            ->where('k.available = true')
-            ->andWhere('k.stock > 0');
+        if (!$cart) {
+            $cart = $cartManager->create();
 
-        $this->overrideGetFilters();
+            $cart->setAccount($this->account());
 
-        $pagination = $this->getPaginator()->paginate(
-            $qb->getQuery(),
-            $request->query->getInt('page', 1), 8
-        );
+            $cartManager->save($cart);
+        }
 
-        return $this->render('kits.index', array(
-            'pagination' => $pagination
-        ));
-    }
+        $cartHasKitManager = $this->manager('cart_has_kit');
 
-    /**
-     * @Route("/{id}", name="kit_show_detail")
-     * @Method("GET")
-     */
-    public function showAction(Kit $kit)
-    {
-        return $this->render('kit/show.html.twig', array(
-            'kit' => $kit
-        ));
+        /** @var CartHasKit $cartHasKit */
+        $cartHasKit = $cartHasKitManager->create();
+
+        $quantity = $request->get('quantity');
+
+        $cartHasKit->setKit($kit);
+        $cartHasKit->setCart($cart);
+        $cartHasKit->setQuantity($quantity);
+
+        try {
+            $cartHasKitManager->save($cartHasKit);
+
+            return $this->json([], Response::HTTP_OK);
+        } catch (\Exception $exception) {
+            $message = 'Não foi possível adicionar o kit';
+
+            if ($cartHasKit->getKit() === null) {
+                $message = 'O kit não está disponível';
+            }
+
+            if ($cartHasKit->getQuantity() === null) {
+                $message = 'Quantidade indisponível';
+            }
+
+            return $this->json([
+                'message' => $message
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
     }
 }
