862d342c5a024509da3ec8ce7370b7a21428e61a
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-06-01 08:44:39 -0300

    Implementação da rota de listagem e visualização de detalhes de kit

--- backend/src/AppBundle/Controller/KitsController.php
+++ backend/src/AppBundle/Controller/KitsController.php
@@ -27,25 +27,10 @@ class KitsController extends AbstractController
 
         $qb = $manager->createQueryBuilder();
 
-        $qb->orderBy('k.position', 'asc');
-
-        if ($actives = $request->get('actives')) {
-            if ((int) $actives == 1) {
-                $expression  =
-                    $qb->expr()->eq(
-                        'k.available',
-                        $qb->expr()->literal(1));
-            } else {
-                $expression  =
-                    $qb->expr()->eq(
-                        'k.available',
-                        $qb->expr()->literal(0));
-            }
-
-            $qb->andWhere(
-                $expression
-            );
-        }
+        $qb
+            ->orderBy('k.position', 'asc')
+            ->where('k.available = true')
+            ->andWhere('k.stock > 0');
 
         $this->overrideGetFilters();
 
@@ -54,81 +39,13 @@ class KitsController extends AbstractController
             $request->query->getInt('page', 1), 8
         );
 
-        return $this->render('kit/index.html.twig', array(
-            'pagination' => $pagination,
-            'kits_active_val' => $actives
+        return $this->render('kits.index', array(
+            'pagination' => $pagination
         ));
     }
 
     /**
-     * @Route("/create", name="create_kit")
-     */
-    public function createAction(Request $request)
-    {
-        $manager = $this->manager('kit');
-
-        /** @var Kit $kit */
-        $kit = $manager->create();
-
-        $form = $this->createForm(KitType::class, $kit);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $components = $request->get('components') ?? [];
-
-            $kit->setComponents($components);
-
-            $manager->save($kit);
-
-            $this->get('component_file_handler')->upload($kit, $request->files);
-
-            $manager->save($kit);
-
-            $this->setNotice('Kit cadastrado com sucesso!');
-
-            return $this->redirectToRoute('kits_index');
-        }
-
-        return $this->render("kit/config.html.twig", [
-            'form' => $form->createView(),
-            'kit' => $kit
-        ]);
-    }
-
-    /**
-     * @Route("/{id}/update", name="update_kit")
-     */
-    public function updateAction(Request $request, Kit $kit)
-    {
-        $form = $this->createForm(KitType::class, $kit);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $manager = $this->manager('kit');
-
-            // TODO: revisar salvamento de imagem
-            $this->get('component_file_handler')->upload($kit, $request->files);
-
-            $components = $request->get('components') ?? [];
-
-            $kit->setComponents($components);
-
-            $manager->save($kit);
-
-            $this->setNotice('Kit cadastrado com sucesso!');
-
-            return $this->redirectToRoute('kits_index');
-        }
-
-        return $this->render("kit/config.html.twig", [
-            'form' => $form->createView(),
-            'kit' => $kit
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="kit_show")
+     * @Route("/{id}", name="kit_show_detail")
      * @Method("GET")
      */
     public function showAction(Kit $kit)
@@ -136,65 +53,5 @@ class KitsController extends AbstractController
         return $this->render('kit/show.html.twig', array(
             'kit' => $kit
         ));
-    }
-
-    /**
-     *
-     * @Security("has_role('ROLE_PLATFORM_ADMIN')")
-     *
-     * @Route("/{id}/delete/", name="delete_kit")
-     * @Method("delete")
-     */
-    public function deleteAction(Kit $kit)
-    {
-        try {
-            $this->manager('kit')->delete($kit);
-            $message = 'Kit excluído com sucesso';
-            $status = Response::HTTP_OK;
-        } catch (\Exception $exception) {
-            $message = 'Falha ao excluir este Kit';
-            $status = Response::HTTP_CONFLICT;
-        }
-
-        return $this->json(['message' => $message], $status);
-    }
-
-    /**
-     * @Route("/components/{family}", name="kit_components_by_family")
-     * @Method("get")
-     */
-    public function getKitComponentsByFamilyAction($family)
-    {
-        if (!in_array($family, $this->getComponentFamilies())) {
-            return JsonResponse::create([], 404);
-        }
-
-        $manager = $this->container->get("{$family}_manager");
-
-        $field = in_array($family, ['module',  'inverter']) ? 'model' : 'description';
-
-        /** @var QueryBuilder $qb */
-        $qb = $manager->createQueryBuilder();
-        $alias = $qb->getRootAlias();
-
-        $qb->select("{$alias}.id, {$alias}.code, {$alias}.{$field} as description");
-
-        $results = $qb->getQuery()->getResult();
-
-        return JsonResponse::create($results, 200);
-    }
-
-    /**
-     * @return array
-     */
-    private function getComponentFamilies()
-    {
-        return $families = [
-            ComponentInterface::FAMILY_MODULE => ComponentInterface::FAMILY_MODULE,
-            ComponentInterface::FAMILY_INVERTER => ComponentInterface::FAMILY_INVERTER,
-            ComponentInterface::FAMILY_STRING_BOX => ComponentInterface::FAMILY_STRING_BOX,
-            ComponentInterface::FAMILY_STRUCTURE => ComponentInterface::FAMILY_STRUCTURE,
-            ComponentInterface::FAMILY_VARIETY => ComponentInterface::FAMILY_VARIETY
-        ];
     }
 }
