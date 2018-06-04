9b1a2d2080247d47be096c454ffc196b07996823
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-06-04 14:19:13 -0300

    Implementação da Entidade e do Manager de CartPool

--- backend/src/AppBundle/Entity/Kit/CartPool.php
+++ backend/src/AppBundle/Entity/Kit/CartPool.php
@@ -1,15 +1,114 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kolina-pc2
- * Date: 04/06/18
- * Time: 11:12
- */
 
 namespace AppBundle\Entity\Kit;
 
+use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * Cart Pool
+ * @ORM\Entity
+ * @ORM\Table(name="app_cart_pool")
+ */
 class CartPool
 {
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $code;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(type="json", nullable=true)
+     */
+    private $metadata;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(type="json", nullable=true)
+     */
+    private $callbacks;
+
+    /**
+     * @inheritdoc
+     */
+    public function __construct()
+    {
+        $this->metadata = [];
+        $this->callbacks = [];
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setMetadata(array $metadata = [])
+    {
+        $this->metadata = $metadata;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getMetadata()
+    {
+        return $this->metadata;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setCallbacks(array $callbacks = [])
+    {
+        $this->callbacks = $callbacks;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCallbacks()
+    {
+        return $this->callbacks;
+    }
 }
--- backend/src/AppBundle/Manager/CartPoolManager.php
+++ backend/src/AppBundle/Manager/CartPoolManager.php
@@ -1,15 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kolina-pc2
- * Date: 04/06/18
- * Time: 11:13
- */
 
 namespace AppBundle\Manager;
 
-
-class CartPoolManager
+class CartPoolManager extends AbstractManager
 {
 
 }
