7a6ff35e696b9362d08520d47ca3a42abe277f07
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-06-04 14:18:22 -0300

    Definição do Service de CartPool

---
+++ backend/src/AppBundle/Entity/Kit/CartPool.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kolina-pc2
+ * Date: 04/06/18
+ * Time: 11:12
+ */
+
+namespace AppBundle\Entity\Kit;
+
+
+class CartPool
+{
+
+}
---
+++ backend/tests/AppBundle/Entity/CartPoolManagerTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kolina-pc2
+ * Date: 04/06/18
+ * Time: 11:44
+ */
+
+namespace Tests\AppBundle\Entity;
+
+
+class CartPoolManagerTest
+{
+
+}
