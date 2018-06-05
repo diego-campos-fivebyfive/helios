550eb62c8ac6c618dbdf2c5a12717b8b57450a27
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-06-05 14:49:29 -0300

    Implemetado serviço CartPoolHelper, com metodos de formatação de dados

--- backend/src/AppBundle/Service/Cart/CartPoolHelper.php
+++ backend/src/AppBundle/Service/Cart/CartPoolHelper.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Service\Cart;
 
+use AppBundle\Entity\AccountInterface;
+use AppBundle\Entity\Kit\CartHasKit;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class CartPoolCreate
+class CartPoolHelper
 {
     /**
      * @var ContainerInterface
@@ -19,8 +21,77 @@ class CartPoolCreate
         $this->container = $container;
     }
 
-    public function create($code, $account, $kits)
+    /**
+     * @param AccountInterface $account
+     * @return array
+     */
+    private function formatAccount(AccountInterface $account)
     {
+        return [
+            'id' => $account->getId(),
+            'firstname' => $account->getFirstname(),
+            'lastname' => $account->getLastname(),
+            'document' => $account->getDocument(),
+            'extraDocument' => $account->getExtraDocument(),
+            'email' => $account->getEmail(),
+            'phone' => $account->getPhone(),
+            'level' => $account->getLevel()
+        ];
+    }
 
+    /**
+     * @param array $items
+     * @return array
+     */
+    public function formatItems(array $items, $checkout = true)
+    {
+        $formatedItems = [];
+
+        /** @var CartHasKit $item */
+        foreach ($items as $item) {
+            $formatedItem = [
+                'name' => $item->getKit()->getCode(),
+                'description' => $item->getKit()->getDescription(),
+                'value' => round($item->getKit()->getPrice(), 2),
+                'quantity' => $item->getQuantity(),
+                'sku' => $item->getKit()->getId()
+            ];
+
+            if (!$checkout) {
+                $formatedItem['power'] = $item->getKit()->getPower();
+                $formatedItem['components'] = $item->getKit()->getComponents();
+            }
+
+            $formatedItems[] = $formatedItem;
+        }
+
+        return $formatedItems;
+    }
+
+    /**
+     * @param array $checkout
+     * @return array
+     */
+    public function formatCheckout(array $checkout)
+    {
+        return [
+            "first_name" => $checkout['firstName'],
+            "name" => $checkout['firstName'] . " " . $checkout['lastName'],
+            "email" => $checkout['email'],
+            "phone_number" => $checkout['phone'],
+            "shipping_amount" => 10,
+            "address" => [
+                "street" => $checkout['differentDelivery'] ? $checkout['shippingStreet'] : $checkout['street'],
+                "complement" => $checkout['differentDelivery'] ? $checkout['shippingComplement'] : $checkout['complement'],
+                "number" => $checkout['differentDelivery'] ? $checkout['shippingNumber'] : $checkout['number'],
+                "district" => $checkout['differentDelivery'] ? $checkout['shippingNeighborhood'] : $checkout['neighborhood'],
+                "city" => $checkout['differentDelivery'] ? $checkout['shippingCity'] : $checkout['city'],
+                "state" => $checkout['differentDelivery'] ? $checkout['shippingState'] : $checkout['state'],
+                "country" => "Brasil",
+                "postal_code" => $checkout['differentDelivery']
+                    ? str_replace("-", "", $checkout['shippingPostcode'])
+                    : str_replace("-", "", $checkout['postcode'])
+            ]
+        ];
     }
 }
