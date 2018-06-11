84bb38a77784d0c9f5c25412619a668232f030da
Gianluca Bine <gian_bine@hotmail.com>
2018-06-11 16:49:28 -0300

    Implementado colunas createdAt e updatedAt com LifeCycleCallbacks

--- backend/src/AppBundle/Entity/Kit/CartPool.php
+++ backend/src/AppBundle/Entity/Kit/CartPool.php
@@ -10,6 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
  * Cart Pool
  * @ORM\Entity
  * @ORM\Table(name="app_cart_pool")
+ * @ORM\HasLifecycleCallbacks()
  */
 class CartPool
 {
@@ -55,6 +56,20 @@ class CartPool
      * @ORM\Column(type="json", nullable=true)
      */
     private $checkout;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime", nullable=true)
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime", nullable=true)
+     */
+    private $updatedAt;
 
     /**
      * @inheritdoc
@@ -177,4 +192,60 @@ class CartPool
     {
         $this->callbacks[] = $callback;
     }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     * @return CartPool
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTime $updatedAt
+     * @return CartPool
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function prePersist()
+    {
+        $this->createdAt = new \DateTime;
+        $this->updatedAt = new \DateTime;
+    }
+
+    /**
+     * @ORM\PreUpdate
+     */
+    public function preUpdate()
+    {
+        $this->updatedAt = new \DateTime;
+    }
+
 }
