156a2185cbef152e64739c22ee78b00c64ba806d
Gianluca Bine <gian_bine@hotmail.com>
2018-06-12 13:57:56 -0300

    Refatoração de código e separação em três rotas distintas

--- backend/src/AppBundle/Controller/PublicAccess/PaymentController.php
+++ backend/src/AppBundle/Controller/PublicAccess/PaymentController.php
@@ -18,16 +18,14 @@ use Symfony\Component\HttpFoundation\Response;
 class PaymentController extends AbstractController
 {
     /**
-     * @Route("/", name="payment_callback")
+     * @Route("/card", name="payment_callback_card")
      * @Method("get")
      * @return JsonResponse
      * @throws \Doctrine\ORM\NoResultException
      * @throws \Doctrine\ORM\NonUniqueResultException
      */
-    public function getPaymentAction(Request $request)
+    public function getPaymentCardAction(Request $request)
     {
-        $paymentType = $request->query->get('payment_type');
-
         /** @var CartPoolManager $cartPoolManager */
         $cartPoolManager = $this->container->get('cart_pool_manager');
 
@@ -38,13 +36,46 @@ class PaymentController extends AbstractController
             'code' => $code
         ]);
 
-        if ($paymentType != 'boleto') {
-            $callback = $this->formatCallback($request->query->all(), 'card');
-            $result = $this->processCardCallback($callback, $pool);
-        } else {
-            $callback = $this->formatCallback($request->query->all(), 'billet');
-            $result = $this->processBilletCallback($callback, $pool);
+        $callback = $this->formatCallback($request->query->all(), 'card');
+        $result = $this->processCardCallback($callback, $pool);
+
+        if ($result['processed']) {
+            $cartPoolManager->save($pool);
+
+            if ($result['transform']) {
+                /** @var OrderTransformer $orderTransformer */
+                $orderTransformer = $this->container->get('order_transformer');
+
+                $orderTransformer->transformFromCartPool($pool);
+            }
+
+            return $this->json();
         }
+
+        return $this->json([], Response::HTTP_BAD_REQUEST);
+    }
+
+    /**
+     * @Route("/billet/register", name="payment_callback_billet_register")
+     * @Method("get")
+     * @return JsonResponse
+     * @throws \Doctrine\ORM\NoResultException
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function getPaymentBilletRegisterAction(Request $request)
+    {
+        /** @var CartPoolManager $cartPoolManager */
+        $cartPoolManager = $this->container->get('cart_pool_manager');
+
+        $code = $request->query->get('order_id');
+
+        /** @var CartPool $pool */
+        $pool = $cartPoolManager->findOneBy([
+            'code' => $code
+        ]);
+
+        $callback = $this->formatCallback($request->query->all(), 'billet');
+        $result = $this->processBilletCallback($callback, $pool);
 
         if ($result['processed']) {
             $cartPoolManager->save($pool);
@@ -216,37 +247,55 @@ class PaymentController extends AbstractController
             return null;
         }
 
-        if ($mode === 'card') {
-            return [
-                'acquirer_transaction_id' => $callback['acquirer_transaction_id'],
-                'amount' => $callback['amount'],
-                'authorization_timestamp' => $callback['authorization_timestamp'],
-                'customer_id' => $callback['customer_id'],
-                'number_installments' => $callback['number_installments'],
-                'order_id' => $callback['order_id'],
-                'payment_id' => $callback['payment_id'],
-                'payment_type' => $callback['payment_type'],
-                'status' => $callback['status']
-            ];
-        } else if ($mode === 'billet') {
-            return [
-                'payment_type' => $callback['payment_type'],
-                'order_id' => $callback['order_id'],
-                'id' => $callback['id'],
-                'amount' => $callback['amount'],
-                'status' => $callback['status'],
-                'bank' => $callback['bank'],
-                'our_number' => $callback['our_number'],
-                'typeful_line' => $callback['typeful_line'],
-                'issue_date' => $callback['issue_date']
-            ];
+        return $this->getFormat($mode, $callback);
+    }
+
+    /**
+     * @param $mode
+     * @param array $callback
+     * @return mixed
+     */
+    private function getFormat($mode, array $callback)
+    {
+        $format = [];
+
+        switch ($mode) {
+            case 'card':
+                $format = [
+                    'acquirerTransactionId' => $callback['acquirer_transaction_id'],
+                    'amount' => $callback['amount'],
+                    'authorizationTimestamp' => $callback['authorization_timestamp'],
+                    'customerId' => $callback['customer_id'],
+                    'numberInstallments' => $callback['number_installments'],
+                    'orderId' => $callback['order_id'],
+                    'paymentId' => $callback['payment_id'],
+                    'paymentType' => $callback['payment_type'],
+                    'status' => $callback['status']
+                ];
+                break;
+            case 'billet':
+                $format = [
+                    'paymentType' =>  $callback['payment_type'],
+                    'orderId' => $callback['order_id'],
+                    'id' => $callback['id'],
+                    'amount' => $callback['amount'],
+                    'status' => $callback['status'],
+                    'bank' => $callback['bank'],
+                    'ourNumber' => $callback['our_number'],
+                    'typefulLine' => $callback['typeful_line'],
+                    'issueDate' => $callback['issue_date']
+                ];
+                break;
+            case 'paymentBillet':
+                $format = [
+                    'id' => $callback['id'],
+                    'paymentDate' => $callback['payment_date'],
+                    'amount' => $callback['amount'],
+                    'status' => $callback['status']
+                ];
+                break;
         }
 
-        return [
-            'id' => $callback['id'],
-            'payment_date' => $callback['payment_date'],
-            'amount' => $callback['amount'],
-            'status' => $callback['status']
-        ];
+        return $format;
     }
 }
