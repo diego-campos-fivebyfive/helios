f450d3496c8728f29681ca2f1737086294030b11
Gianluca Bine <gian_bine@hotmail.com>
2018-06-12 11:31:52 -0300

    Adequação de rotas e implementação de rota de registro de boletos

--- backend/src/AppBundle/Controller/PublicAccess/PaymentController.php
+++ backend/src/AppBundle/Controller/PublicAccess/PaymentController.php
@@ -31,20 +31,58 @@ class PaymentController extends AbstractController
         /** @var CartPoolManager $cartPoolManager */
         $cartPoolManager = $this->container->get('cart_pool_manager');
 
-        $code = $paymentType ? $code = $request->query->get('order_id') : $request->query->get('id');
+        $code = $request->query->get('order_id');
 
         /** @var CartPool $pool */
         $pool = $cartPoolManager->findOneBy([
             'code' => $code
         ]);
 
-        if (isset($paymentType)) {
+        if ($paymentType != 'boleto') {
             $callback = $this->formatCallback($request->query->all(), 'card');
             $result = $this->processCardCallback($callback, $pool);
         } else {
             $callback = $this->formatCallback($request->query->all(), 'billet');
             $result = $this->processBilletCallback($callback, $pool);
         }
+
+        if ($result['processed']) {
+            $cartPoolManager->save($pool);
+
+            if ($result['transform']) {
+                /** @var OrderTransformer $orderTransformer */
+                $orderTransformer = $this->container->get('order_transformer');
+
+                $orderTransformer->transformFromCartPool($pool);
+            }
+
+            return $this->json();
+        }
+
+        return $this->json([], Response::HTTP_BAD_REQUEST);
+    }
+
+    /**
+     * @Route("/billet", name="payment_callback_billet")
+     * @Method("get")
+     * @return JsonResponse
+     * @throws \Doctrine\ORM\NoResultException
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function getPaymentBilletAction(Request $request)
+    {
+        /** @var CartPoolManager $cartPoolManager */
+        $cartPoolManager = $this->container->get('cart_pool_manager');
+
+        $billetId = $request->query->get('id');
+
+        /** @var CartPool $pool */
+        $pool = $cartPoolManager->findOneBy([
+            'billetId' => $billetId
+        ]);
+
+        $callback = $this->formatCallback($request->query->all(), 'paymentBillet');
+        $result = $this->processPaymentBilletCallback($callback, $pool);
 
         if ($result['processed']) {
             $cartPoolManager->save($pool);
@@ -72,10 +110,31 @@ class PaymentController extends AbstractController
         if ($callback && $cartPool) {
             $cartPool->addCallback($callback);
 
-            $transform = false;
-            if ($callback['status'] === 'APPROVED') {
-                $transform = true;
-            }
+            $transform = ($callback['status'] === 'APPROVED') ? true : false;
+
+            return [
+                'processed' => true,
+                'transform' => $transform
+            ];
+        }
+
+        return [
+            'processed' => false,
+            'transform' => false
+        ];
+    }
+
+    /**
+     * @param array $callback
+     * @param CartPool $cartPool
+     * @return array
+     */
+    private function processPaymentBilletCallback(array $callback, CartPool $cartPool)
+    {
+        if ($callback && $cartPool) {
+            $cartPool->addCallback($callback);
+
+            $transform = ($callback['status'] === 'PAID') ? true : false;
 
             return [
                 'processed' => true,
@@ -99,14 +158,11 @@ class PaymentController extends AbstractController
         if ($callback && $cartPool) {
             $cartPool->addCallback($callback);
 
-            $transform = false;
-            if ($callback['status'] === 'PAGO') {
-                $transform = true;
-            }
+            $cartPool->setBilletId($callback['id']);
 
             return [
                 'processed' => true,
-                'transform' => $transform
+                'transform' => false
             ];
         }
 
@@ -136,19 +192,31 @@ class PaymentController extends AbstractController
         ];
 
         $billetKeys = [
+            'payment_type',
+            'order_id',
+            'id',
+            'amount',
+            'status',
+            'bank',
+            'our_number',
+            'typeful_line',
+            'issue_date'
+        ];
+
+        $paymentBilletKeys = [
             'id',
             'payment_date',
             'amount',
             'status'
         ];
 
-        if ($mode === 'card') {
-            $keys = $cardKeys;
-        } else {
-            $keys = $billetKeys;
-        }
+        $keys = [
+            'card' => $cardKeys,
+            'billet' => $billetKeys,
+            'paymentBillet' => $paymentBilletKeys
+        ];
 
-        if (array_diff($keys, array_keys($callback))) {
+        if (array_diff($keys[$mode], array_keys($callback))) {
             return null;
         }
 
@@ -163,6 +231,18 @@ class PaymentController extends AbstractController
                 'payment_id' => $callback['payment_id'],
                 'payment_type' => $callback['payment_type'],
                 'status' => $callback['status']
+            ];
+        } else if ($mode === 'billet') {
+            return [
+                'payment_type' => $callback['payment_type'],
+                'order_id' => $callback['order_id'],
+                'id' => $callback['id'],
+                'amount' => $callback['amount'],
+                'status' => $callback['status'],
+                'bank' => $callback['bank'],
+                'our_number' => $callback['our_number'],
+                'typeful_line' => $callback['typeful_line'],
+                'issue_date' => $callback['issue_date']
             ];
         }
 
