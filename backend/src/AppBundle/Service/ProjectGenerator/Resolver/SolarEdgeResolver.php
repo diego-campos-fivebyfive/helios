2d0b487f9bedcd733e314d49b973d84e670ea9f9
Fabio Dukievicz <fabiojd47@gmail.com>
2018-06-15 11:42:12 -0300

    tipagem de retorno

---
+++ backend/src/AppBundle/Service/ProjectGenerator/Resolver/SolarEdgeResolver.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kolinalabs
+ * Date: 6/15/18
+ * Time: 10:32 AM
+ */
+
+namespace AppBundle\Service\ProjectGenerator\Resolver;
+
+
+class SolarEdgeResolver
+{
+
+}
