9979710c0be81dbe19aba6e0daff1065f018d496
Fabio Dukievicz <fabiojd47@gmail.com>
2018-06-25 16:03:15 -0300

    mudança em manager

--- backend/src/AppBundle/Controller/PublicAccess/PaymentController.php
+++ backend/src/AppBundle/Controller/PublicAccess/PaymentController.php
@@ -69,7 +69,7 @@ class PaymentController extends AbstractController
     public function getPaymentBilletRegisterAction(Request $request)
     {
         /** @var CartPoolManager $cartPoolManager */
-        $cartPoolManager = $this->container->get('cart_pool_manager');
+        $cartPoolManager = $this->manager('cart_pool');
 
         $code = $request->query->get('order_id');
 
