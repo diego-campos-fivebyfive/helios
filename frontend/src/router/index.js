633e41aa6ccf7983727c8889b9683cdf80590676
Rafael Kendrik <rafamikovski@hotmail.com>
2018-06-25 17:35:07 -0300

    Solving fucking marge problems part II

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -16,7 +16,6 @@ export const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
-<<<<<<< HEAD
   const path = {
     destiny: to.path,
     origin: from.path,
@@ -29,14 +28,15 @@ router.beforeEach((to, from, next) => {
   }
 
   if (path.destiny === path.notfound) {
-=======
-  if (to.path === '/terms') {
->>>>>>> b6f510a3d2ab7cbec1321a97de4d66594c9b07c8
     next()
     return
   }
 
-<<<<<<< HEAD
+  if (to.path === '/terms') {
+    next()
+    return
+  }
+
   axios.get('api/v1/application/menu')
 
     .then(({ data: menu }) => Object.values(menu)
@@ -51,12 +51,11 @@ router.beforeEach((to, from, next) => {
 
       next()
     })
-=======
+
   const uri = '/api/v1/terms/checker'
   axios.get(uri).catch(() => next('/terms'))
 
   next()
->>>>>>> b6f510a3d2ab7cbec1321a97de4d66594c9b07c8
 })
 
 export { axios }
