0adbd56a1a070060e8fcb24b25deb8f7c951ff23
Paula Daiane <pauladpm@outlook.com>
2018-08-09 15:33:47 -0300

    [alterações]

--- collection/index.js
+++ collection/index.js
@@ -13,7 +13,6 @@ import Paginator from './Paginator'
 import Panel from './Panel'
 import Progress from './Progress'
 import Search from './Search'
-import Sticker from './Sticker'
 import Table from './Table'
 
 export default {
@@ -30,6 +29,5 @@ export default {
   Panel,
   Progress,
   Search,
-  Sticker,
   Table
 }
