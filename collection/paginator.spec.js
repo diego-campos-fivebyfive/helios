1d1ab4ed662f282eba2b4c942fde2aa2bb164c1c
joaozaqueu <joaozaqueuchereta@gmail.com>
2018-08-09 09:49:45 -0300

    (Imp) Split tests in methods' describes

--- collection/paginator.spec.js
+++ collection/paginator.spec.js
@@ -27,51 +27,260 @@ describe('Paginator.vue', () => {
     expect(schema).toBeDefined()
   })
 
-  describe('showPagination()', () => {
-    it('should return false when total is 0 or undefined', () => {
-      const schemaOne = pipe(
-        mountPaginator
-      )({ pagination: {} }).showPagination()
+  it('should render the array returned by navigationItems()', () => {
+    const schemaOne = pipe(
+      mountPaginator,
+      getRenderedText
+    )({
+      pagination: {
+        total: 5
+      }
+    })
 
-      const schemaTwo = pipe(
-        mountPaginator
+    const schemaTwo = pipe(
+      mountPaginator,
+      getRenderedText
+    )({
+      pagination: {
+        total: 10,
+        current: 5
+      }
+    })
+
+    expect(schemaOne).toBe('12345Próximo')
+    expect(schemaTwo).toBe('Anterior1...34567...10Próximo')
+  })
+
+  describe('getCurrent()', () => {
+
+  })
+
+  describe('getInitialRangeIndex()', () => {
+
+  })
+
+  describe('getNavigationItems()', () => {
+  })
+
+  describe('getNextControls()', () => {
+    it('should exhibit neither the spread item nor the last index when last range item and index are the same', () => {
+      const schema = pipe(
+        mountPaginator,
+        Paginator => pipe(
+          Paginator.getRangeItems,
+          Paginator.getNextControls
+        )()
       )({
         pagination: {
-          total: 0
+          total: 9,
+          current: 9
         }
-      }).showPagination()
+      })
 
-      expect(schemaOne).not.toEqual(true)
-      expect(schemaTwo).not.toEqual(true)
+      const expected = expect.arrayContaining([
+        expect.objectContaining({
+          label: '...'
+        }),
+        expect.objectContaining({
+          label: 9,
+          value: 9
+        })
+      ])
+
+      expect(schema).not.toEqual(expected)
+    })
+
+    it('should exhibit the last index with spread item when there are more than 5 items', () => {
+      const schema = pipe(
+        mountPaginator,
+        Paginator => pipe(
+          Paginator.getRangeItems,
+          Paginator.getNextControls
+        )()
+      )({
+        pagination: {
+          total: 9,
+          current: 1
+        }
+      })
+
+      const expected = expect.arrayContaining([
+        expect.objectContaining({
+          label: '...'
+        }),
+        expect.objectContaining({
+          label: 9,
+          value: 9
+        })
+      ])
+
+      expect(schema).toEqual(expected)
+    })
+
+    it('should exhibit the next button when there is a next item', () => {
+      const schemaOne = pipe(
+        mountPaginator,
+        Paginator => pipe(
+          Paginator.getRangeItems,
+          Paginator.getNextControls
+        )()
+      )({
+        pagination: {
+          total: 9,
+          current: 1
+        }
+      })
+
+      const schemaTwo = pipe(
+        mountPaginator,
+        Paginator => pipe(
+          Paginator.getRangeItems,
+          Paginator.getNextControls
+        )()
+      )({
+        pagination: {
+          total: 9,
+          current: 9
+        }
+      })
+
+      const expected = expect.arrayContaining([
+        expect.objectContaining({
+          label: 'Próximo'
+        })
+      ])
+
+      expect(schemaOne).toEqual(expected)
+    })
+
+    it('should sum current page plus one and add it to next button as value', () => {
+      const schema = pipe(
+        mountPaginator,
+        Paginator => pipe(
+          Paginator.getRangeItems,
+          Paginator.getNextControls
+        )()
+      )({
+        pagination: {
+          total: 3,
+          current: 2
+        }
+      })
+
+      const expected = expect.arrayContaining([
+        expect.objectContaining({
+          label: 'Próximo',
+          value: 3
+        })
+      ])
+
+      expect(schema).toEqual(expected)
+    })
+  })
+
+  describe('getPrevControls()', () => {
+    it('should exhibit the first index with spread item when the first range item is not 1', () => {
+      const schema = pipe(
+        mountPaginator,
+        Paginator => pipe(
+          Paginator.getRangeItems,
+          Paginator.getPrevControls
+        )()
+      )({
+        pagination: {
+          total: 9,
+          current: 5
+        }
+      })
+
+      const expected = expect.arrayContaining([
+        expect.objectContaining({
+          label: 1,
+          value: 1
+        }),
+        expect.objectContaining({
+          label: '...'
+        })
+      ])
+
+      expect(schema).toEqual(expected)
+    })
+
+    it('should exhibit the prev button when there is a prev item', () => {
+      const schemaOne = pipe(
+        mountPaginator,
+        Paginator => pipe(
+          Paginator.getRangeItems,
+          Paginator.getPrevControls
+        )()
+      )({
+        pagination: {
+          total: 9,
+          current: 4
+        }
+      })
+
+      const expected = expect.arrayContaining([
+        expect.objectContaining({
+          label: 'Anterior'
+        })
+      ])
+
+      expect(schemaOne).toEqual(expected)
+    })
+
+    it('should decrease one and add it as prev button value', () => {
+      const schema = pipe(
+        mountPaginator,
+        Paginator => pipe(
+          Paginator.getRangeItems,
+          Paginator.getPrevControls
+        )()
+      )({
+        pagination: {
+          total: 9,
+          current: 4
+        }
+      })
+
+      const expected = expect.arrayContaining([
+        expect.objectContaining({
+          label: 'Anterior',
+          value: 3
+        })
+      ])
+
+      expect(schema).toEqual(expected)
     })
   })
 
   describe('getRangeItems()', () => {
     it('should return a maximum of 5 range items when there are 5 items or more', () => {
-      const schemaOne = pipe(
-        mountPaginator
-      )({
+      const schemaOne = mountPaginator({
         pagination: {
           total: 5
         }
       }).getRangeItems()
 
-      const schemaTwo = pipe(
-        mountPaginator
-      )({
+      const schemaTwo = mountPaginator({
         pagination: {
           total: 9
         }
       }).getRangeItems()
 
+      const schemaThree = mountPaginator({
+        pagination: {
+          total: 3
+        }
+      }).getRangeItems()
+
       expect(schemaOne).toHaveLength(5)
       expect(schemaTwo).toHaveLength(5)
+      expect(schemaThree).toHaveLength(3)
     })
 
     it('should keep current item in the middle when there are 2 prev items and 2 next items when there are 5 items or more', () => {
-      const schema = pipe(
-        mountPaginator
-      )({
+      const schema = mountPaginator({
         pagination: {
           total: 15,
           current: 10
@@ -105,19 +314,15 @@ describe('Paginator.vue', () => {
       expect(schema).toEqual(expected)
     })
 
-    it('should keep current item after the middle when there are no or 1 next item', () => {
-      const schemaOne = pipe(
-        mountPaginator
-      )({
+    it('should keep current item after the middle when there are any or 1 next item', () => {
+      const schemaOne = mountPaginator({
         pagination: {
           total: 15,
           current: 14
         }
       }).getRangeItems()
 
-      const schemaTwo = pipe(
-        mountPaginator
-      )({
+      const schemaTwo = mountPaginator({
         pagination: {
           total: 15,
           current: 15
@@ -175,179 +380,46 @@ describe('Paginator.vue', () => {
       expect(schemaOne).toEqual(expectedOne)
       expect(schemaTwo).toEqual(expectedTwo)
     })
-  })
 
-  describe('getNavigationItems()', () => {
-    it('should render the array returned by navigationItems()', () => {
-      const schema = pipe(
-        mountPaginator,
-        getRenderedText
-      )({
+    it('should contains at least one current item, setting first item as current (1) when there is no current arg', () => {
+      const schemaOne = mountPaginator({
         pagination: {
-          total: 5
+          total: 2
         }
-      })
+      }).getRangeItems()
 
-      expect(schema).toBe('12345')
-    })
-
-    it('should contains at least one current item', () => {
-      const schema = pipe(
-        mountPaginator
-      )({
+      const schemaTwo = mountPaginator({
         pagination: {
           total: 2,
           current: 1
         }
-      }).getNavigationItems()
-
-      const expected = expect.arrayContaining([
-        expect.objectContaining({ current: true }),
-        expect.objectContaining({ current: false })
-      ])
-
-      expect(schema).toEqual(expected)
-    })
-
-    it('should set first item as current when there is no current arg', () => {
-      const schema = pipe(
-        mountPaginator
-      )({
-        pagination: {
-          total: 2
-        }
-      }).getNavigationItems()
-
-      const expected = expect.arrayContaining([
-        expect.objectContaining({ current: true }),
-        expect.objectContaining({ current: false })
-      ])
-
-      expect(schema).toEqual(expected)
-    })
-
-    it('should exhibit the last index with spread item when there are more than 5 items', () => {
-      const schema = pipe(
-        mountPaginator
-      )({
-        pagination: {
-          total: 9,
-          current: 1
-        }
-      }).getNavigationItems()
-
-      const expected = expect.arrayContaining([
-        expect.objectContaining({ label: '...' }),
-        expect.objectContaining({ label: 9, value: 9 })
-      ])
-
-      expect(schema).toEqual(expected)
-    })
-
-    it('should exhibit the next button when there is a next item', () => {
-      const schemaOne = pipe(
-        mountPaginator
-      )({
-        pagination: {
-          total: 9,
-          current: 1
-        }
-      }).getNavigationItems()
-
-
-      const schemaTwo = pipe(
-        mountPaginator
-      )({
-        pagination: {
-          total: 9,
-          current: 9
-        }
-      }).getNavigationItems()
-
-      const expected = expect.arrayContaining([
-        expect.objectContaining({ label: 'Próximo' })
-      ])
-
-      expect(schemaOne).toEqual(expected)
-    })
-
-    it('should exhibit the prev button when there is a prev item', () => {
-      const schemaOne = pipe(
-        mountPaginator
-      )({
-        pagination: {
-          total: 9,
-          current: 4
-        }
-      }).getNavigationItems()
-
-      const expected = expect.arrayContaining([
-        expect.objectContaining({ label: 'Anterior' })
-      ])
-
-      expect(schemaOne).toEqual(expected)
-    })
-
-    it('should sum current page plus one and add it to next button as value', () => {
-      const schema = pipe(
-        mountPaginator
-      )({
-        pagination: {
-          total: 3,
-          current: 2
-        }
-      }).getNavigationItems()
+      }).getRangeItems()
 
       const expected = expect.arrayContaining([
         expect.objectContaining({
-          label: 'Próximo',
-          value: 3
+          current: true
         })
       ])
 
-      expect(schema).toEqual(expected)
-    })
-
-    it('should exhibit the first index with spread item when the first range item is not 1', () => {
-      const schema = pipe(
-        mountPaginator
-      )({
-        pagination: {
-          total: 9,
-          current: 5
-        }
-      }).getNavigationItems()
-
-      const expected = expect.arrayContaining([
-        expect.objectContaining({ label: 1, value: 1 }),
-        expect.objectContaining({ label: '...' })
-      ])
-
-      expect(schema).toEqual(expected)
+      expect(schemaOne).toEqual(expected)
+      expect(schemaTwo).toEqual(expected)
     })
   })
 
-  describe('getNextControls()', () => {
-    it('should exhibit neither the spread item nor the last index when last range item and index are the same', () => {
-      const schema = pipe(
-        mountPaginator,
-        Paginator => pipe(
-          Paginator.getRangeItems,
-          Paginator.getNextControls
-        )
-      )({
+  describe('showPagination()', () => {
+    it('should return false when total is 0 or there is no total arg', () => {
+      const schemaOne = mountPaginator({
+          pagination: {}
+      }).showPagination()
+
+      const schemaTwo = mountPaginator({
         pagination: {
-          total: 9,
-          current: 9
+          total: 0
         }
-      })
+      }).showPagination()
 
-      const expected = expect.arrayContaining([
-        expect.objectContaining({ label: 9, value: 9 }),
-        expect.objectContaining({ label: '...' })
-      ])
-
-      expect(schema).not.toEqual(expected)
+      expect(schemaOne).not.toEqual(true)
+      expect(schemaTwo).not.toEqual(true)
     })
   })
 })
