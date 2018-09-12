2e24706b7992f574eea41d64a8451bd1758669c4
Mauricio Paz Pacheco <mauriciopazdesign@gmail.com>
2018-09-12 15:32:35 -0300

    Modal story and readme

---
+++ collection/Modal.story.js
@@ -0,0 +1,38 @@
+import Modal from './Modal'
+import Button from './Button'
+
+const Simple = `
+<div>
+    <Button
+      :action='() => $refs.modal.show()'
+      label='Open modal'
+      class='default-bordered'>
+    </Button>
+
+    <Modal ref='modal'>
+      <div slot='header'>
+        Modal title
+      </div>
+      <div slot='section' :style='sectionMargin'>
+        <ul>
+          <li>lorem ipsum dolor sit amet consectetuer adipiscing elit</li>
+          <li>lorem ipsum dolor sit amet consectetuer adipiscing elit</li>
+          <li>lorem ipsum dolor sit amet consectetuer adipiscing elit</li>
+          <li>lorem ipsum dolor sit amet consectetuer adipiscing elit</li>
+        </ul>
+      </div>
+    </Modal>
+  </div>
+`
+
+export default {
+  components: { Modal, Button },
+  models: {
+    Simple
+  },
+  computed: {
+    sectionMargin() {
+      return `margin: 30px`
+    }
+  }
+}
